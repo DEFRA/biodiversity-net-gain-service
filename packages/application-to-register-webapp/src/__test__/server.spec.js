09e47b1e8d2744eede6be08d0bc9f80af032c7c8
Tedd Mason <teddmason@gmail.com>
2022-02-09 13:58:50 +0000

    extra helloworld test for coverage

--- packages/application-to-register-webapp/src/__test__/server.spec.js
+++ packages/application-to-register-webapp/src/__test__/server.spec.js
@@ -20,4 +20,18 @@ describe('The server', () => {
     expect(response.payload).toContain('Hello or World?')
     await server.stop()
   })
+
+  it('handles a request', async () => {
+    const server = await createServer(serverOptions)
+    await init(server)
+    const response = await server.inject({
+      method: 'POST',
+      url: '/',
+      payload: {
+        helloWorld: 'hello'
+      }
+    })
+    expect(response.statusCode).toEqual(302)
+    await server.stop()
+  })
 })
