c010dfcef94469c373b8a7ba9ddd193cec7fc6e8
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-04-22 09:24:29 +0100

    Add dummy settings for Azure storage accounts

--- packages/application-to-register-webapp/.jest/test.env.js
+++ packages/application-to-register-webapp/.jest/test.env.js
@@ -1,2 +1,4 @@
 process.env.SESSION_COOKIE_PASSWORD = 'the-password-must-be-at-least-32-characters-long'
 process.env.MAX_GEOSPATIAL_LAND_BOUNDARY_UPLOAD_MB=50
+process.env.AZURE_STORAGE_ACCOUNT='dummystorageaccount'
+process.env.AZURE_STORAGE_ACCESS_KEY='dummystorageaccountaccesskey=='
