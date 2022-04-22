cf15f779bf1b4d9d8c43f775aae2da2c78215b80
Tedd Mason <teddmason@gmail.com>
2022-04-22 13:38:39 +0100

    wip for azure deployment

--- packages/application-to-register-webapp/src/utils/config.js
+++ packages/application-to-register-webapp/src/utils/config.js
@@ -1,4 +1,4 @@
-export const SERVER_PORT = process.env.SERVER_PORT || 3000
+export const SERVER_PORT = process.env.PORT || 3000
 export const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
 export const REDIS_PORT = process.env.REDIS_PORT || '6379'
 export const SESSION_COOKIE_PASSWORD = process.env.SESSION_COOKIE_PASSWORD || 'the-password-must-be-at-least-32-characters-long'
