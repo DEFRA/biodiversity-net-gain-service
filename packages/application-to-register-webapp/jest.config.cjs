5ae352dca5013ec678e7b4382851a4f557923f71
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-05-27 18:59:59 +0100

    Add moduleNameMapper for import resolution

--- packages/application-to-register-webapp/jest.config.cjs
+++ packages/application-to-register-webapp/jest.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   coveragePathIgnorePatterns: ['__tests__/helpers'],
+  moduleNameMapper: {
+    '@defra/bng-connectors-lib/azure-storage': '<rootDir>/node_modules/@defra/bng-azure-storage-test-utils/node_modules/@defra/bng-connectors-lib/src/helpers/azure-storage.js'
+  },
   setupFiles: ['<rootDir>/.jest/test.env.js'],
   testPathIgnorePatterns: ['__tests__/helpers']
 }
