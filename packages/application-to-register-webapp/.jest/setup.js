32c30877d9116417c0f29454a82d6e0bf38a84a7
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-02 13:19:07 +0100

    Manage mocks and environment variables

--- packages/application-to-register-webapp/.jest/setup.js
+++ packages/application-to-register-webapp/.jest/setup.js
@@ -1,9 +1,11 @@
 import { createServer, init } from '../src/server.js'
 import serverOptions from '../src/__mocks__/server-options.js'
 
+const ORIGINAL_ENV = process.env
 let server
 
 beforeEach(async () => {
+  jest.resetAllMocks()
   if (!process.env.USE_MOCK_SERVER) {
     server = await createServer(serverOptions)
     await init(server)
@@ -11,8 +13,12 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-  if (server) {
-    await server.stop()
+  try {
+    if (server) {
+      await server.stop()
+    }
+  } finally {
+    process.env = ORIGINAL_ENV
   }
 })
 
