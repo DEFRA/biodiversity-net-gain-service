f4845295ad70fc7c75410fa66f6e9ab7924c4f10
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-06 20:37:24 +0100

    Increase code quality

--- packages/application-to-register-functions/SignalRNegotiate/index.mjs
+++ packages/application-to-register-functions/SignalRNegotiate/index.mjs
@@ -1,4 +1,4 @@
 // TO DO - Replace anonymous access to this endpoint.
-export default async function (context, req, connectionInfo) {
+export default async function (context, _req, connectionInfo) {
   context.res.body = connectionInfo
 }
