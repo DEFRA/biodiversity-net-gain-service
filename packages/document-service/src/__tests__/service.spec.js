4bfd7cf5e1a1275d4fcdbae2e66e3873514d1343
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-10 20:21:46 +0100

    Add document service unit tests

---
+++ packages/document-service/src/__tests__/service.spec.js
@@ -0,0 +1,20 @@
+import { Readable } from 'stream'
+import { upload } from '../service.js'
+
+describe('The document service', () => {
+  it('should delgate the upload of a document to a cofigured functon', async () => {
+    const logger = {
+      log: jest.fn()
+    }
+    const config = {
+      functionConfig: {
+        uploadFunction: jest.fn()
+      }
+    }
+
+    const documentStream = Readable.from('mock document')
+    await upload(logger, config, documentStream)
+    // The upload function should have been called after the upload function has been removed from the configuration.
+    expect(config.functionConfig.uploadFunction).toHaveBeenCalledWith(logger, {}, documentStream)
+  })
+})
