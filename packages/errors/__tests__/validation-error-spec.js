e40127e9c6c4f45cba4626792ab97c0c60563b78
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-10 20:22:29 +0100

    Add errors-lib unit tests

---
+++ packages/errors/__tests__/validation-error-spec.js
@@ -0,0 +1,15 @@
+import { ValidationError } from '../src/errors.js'
+
+const errorCode = 'mock error code'
+const errorMessage = 'mock error message'
+
+describe('Coordinate system validation errors', () => {
+  it('should supplement a standard Error with an error code', async () => {
+    try {
+      throw new ValidationError(errorCode, errorMessage)
+    } catch (err) {
+      expect(err.code).toBe(errorCode)
+      expect(err.message).toBe(errorMessage)
+    }
+  })
+})
