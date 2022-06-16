0dd0e59591b2b0ee6ccaba9107cbbf5a66fa0638
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-16 10:48:38 +0100

    Reset environment variables after each test

--- packages/application-to-register-functions/.jest/setup.js
+++ packages/application-to-register-functions/.jest/setup.js
@@ -1,6 +1,8 @@
 import Context from '../__mocks__/mock-context.js'
 import { recreateContainers, recreateQueues } from '@defra/bng-azure-storage-test-utils'
 
+const ORIGINAL_ENV = process.env
+
 let context
 
 beforeEach(async () => {
@@ -8,6 +10,10 @@ beforeEach(async () => {
   context = new Context()
   recreateContainers(),
   recreateQueues()
+})
+
+afterEach(async () => {
+  process.env = ORIGINAL_ENV
 })
 
 const getContext = () => {
