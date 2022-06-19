01543101e55a40f62cea6959b201fc1af0205476
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-19 18:24:19 +0100

    Add environment variables for Azurite

--- packages/azurite-bootstrap-service/bin/bootstrap.sh
+++ packages/azurite-bootstrap-service/bin/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export AZURE_STORAGE_ACCOUNT="devstoreaccount1"
+export AZURE_STORAGE_ACCESS_KEY="Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
+export AZURE_BLOB_SERVICE_URL="http://127.0.0.1:10000/devstoreaccount1"
+export AZURE_QUEUE_SERVICE_URL="http://127.0.0.1:10001/devstoreaccount1"
+
 if  `nc -z ${AZURITE_HOST:-localhost} ${AZURITE_BLOB_PORT:-10000}` ; then
   if  `nc -z ${AZURITE_HOST:-localhost} ${AZURITE_QUEUE_PORT:-10001}`; then
   node src/service.js
