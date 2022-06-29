5ef1b42daa478aa385d51de7590f6c8e78273b36
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-06-29 12:50:15 +0100

    Correct typos

--- bin/init-babel-config.sh
+++ bin/init-babel-config.sh
@@ -4,7 +4,7 @@
 # Add a symbolic link to the root Babel configuration file in the same directory as the Jest configuration file in each package.
 for package in `ls packages`
 do
-  if [[ ! -L "packages/${package}/babel.config.json" ]]; then
+  if [ ! -L "packages/${package}/babel.config.json" ]; then
     ln -s ../../babel.config.json  packages/"${package}"/babel.config.json
     echo "Created packages/"${package}"/babel.config.json symbolic link"
   fi
