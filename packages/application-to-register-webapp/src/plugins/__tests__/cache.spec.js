6d4a76ad39ff0d4833592f8982fdcd23aab4b190
Tedd Mason <teddmason@gmail.com>
2022-07-15 09:33:17 +0100

    adding coverage for cache tls check

---
+++ packages/application-to-register-webapp/src/plugins/__tests__/cache.spec.js
@@ -0,0 +1,9 @@
+import * as cache from '../cache.js'
+
+describe('cache', () => {
+  it('it is a cache plugin', () => {
+    expect(cache.default[0].name).toEqual('redis_cache')
+    expect(typeof cache.default[0].provider.options.tls.checkServerIdentity).toEqual('function')
+    expect(cache.default[0].provider.options.tls.checkServerIdentity()).toEqual(undefined)
+  })
+})
