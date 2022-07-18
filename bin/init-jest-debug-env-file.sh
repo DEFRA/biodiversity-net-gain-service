d46fb79e250b56803e0148f7c8c6068759e0371c
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-07-18 13:30:32 +0000

    Generate local debug environment variable file

---
+++ bin/init-jest-debug-env-file.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+JEST_DEBUG_DIR=.jest-with-debug
+JEST_DEBUG_ENV_FILE=${JEST_DEBUG_DIR}/.env
+mkdir -p ${JEST_DEBUG_DIR}
+echo NODE_DEV=test > ${JEST_DEBUG_ENV_FILE}
+grep "export AZURE" bin/run-unit-tests.sh | sed 's/^export //' >> ${JEST_DEBUG_ENV_FILE}
+grep "export AZURE\|export SIGNALR" docker/secrets/WEBAPP_ENV | sed 's/^export //' >> ${JEST_DEBUG_ENV_FILE}
+echo "(Re)created ${JEST_DEBUG_ENV_FILE}"
--- bin/local-install.sh
+++ bin/local-install.sh
@@ -2,3 +2,4 @@
 
 (cd packages/application-to-register-functions && bin/init-local-settings-if-needed.sh)
 (cd packages/application-to-register-webapp && bin/init-webapp-env-if-needed.sh)
+./bin/init-jest-debug-env-file.sh
