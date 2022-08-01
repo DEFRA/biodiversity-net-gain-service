57a6a2c88a3e85b875afe26994a1ca645ef8dd2e
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-08-01 12:17:31 +0100

    Revert "Revert "Preliminary threat processing integration""
    This reverts commit a9b59758c392d1ae686686867dcced6e260bbb9f.

--- packages/application-to-register-functions/ProcessUntrustedFile/index.mjs
+++ packages/application-to-register-functions/ProcessUntrustedFile/index.mjs
@@ -1,31 +1,67 @@
-// TO DO - Add security processing to this function.
 import { blobStorageConnector } from '@defra/bng-connectors-lib'
+import { screenDocumentForThreats } from '@defra/bng-document-service'
+
+const baseConfig = {
+  untrustedBlobStorageConfig: {
+    containerName: 'untrusted'
+  },
+  trustedBlobStorageConfig: {
+    containerName: 'trusted'
+  },
+  avConfig: {
+    baseUrl: process.env.AV_API_BASE_URL,
+    authenticationConfig: {
+      url: process.env.AV_API_TOKEN_URL,
+      clientId: process.env.AV_API_CLIENT_ID,
+      clientSecret: process.env.AV_API_CLIENT_SECRET,
+      scope: process.env.AV_API_SCOPE
+    }
+  }
+}
 
 export default async function (context, message) {
   context.log('Processing', JSON.stringify(message))
-  context.log.warn('Transferring untrusted file to trusted container WITHOUT SECURITY PROCESSING')
+  const config = JSON.parse(JSON.stringify(baseConfig))
+  config.untrustedBlobStorageConfig.blobName = message.location
+  config.trustedBlobStorageConfig.blobName = message.location
+
+  config.avConfig.fileConfig = {
+    location: message.location
+  }
+
   // Blob based triggering of Azure functions can be delayed due to its polling based implementation and event grid based
   // blob triggering of Azure functions is in preview.
   // Use Azure storage queues combined with blob storage to ensure responsive processing while event grid based blob triggering
   // is in preview - see https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-expressions-patterns.
   //
-  // Copy the untrusted file to the trusted file container manually before using a binding to place the trusted file location on a
+  // If threat screening succeeds, copy the untrusted file to the trusted file container manually before using a binding to place the trusted file location on a
   // storage queue. This ensures that the file will be present in the trusted file container BEFORE the queued message is processed.
 
-  const config = {
-    blobName: message.location,
-    containerName: 'untrusted'
-  }
-  const response = await blobStorageConnector.downloadStreamIfExists(context, config)
+  let screenedFileAsStream
+  const response = await blobStorageConnector.downloadStreamIfExists(context, config.untrustedBlobStorageConfig)
 
   if (response) {
-    const blobConfig = {
-      blobName: message.location,
-      containerName: 'trusted'
+    context.log('Sending file for security screening')
+    try {
+      // Interaction with the initial streaming based threat processing solution results in a stream being returned
+      // for upload to the trusted file container. At present, a stream is submitted for threat processing and then polling
+      // is used to retrieve the results.
+      //
+      // If asynchronous interaction with the threat processing solution is progressed, it will be benefical if:
+      // - this function submits a stream for threat processing.
+      // - the threat processing solution notifies another function when threat processing is complete
+      //   (either successfully or unsuccessfully).
+      //
+      // Synchronous threat processing is also an option for consideration.
+      screenedFileAsStream = await screenDocumentForThreats(context, config.avConfig, response.readableStreamBody)
+    } catch (err) {
+      context.log.error('Unable to screen file')
     }
-    await blobStorageConnector.uploadStream(blobConfig, response.readableStreamBody)
-    context.bindings.trustedFileQueue = message
+    if (screenedFileAsStream) {
+      await blobStorageConnector.uploadStream(config.trustedBlobStorageConfig, screenedFileAsStream)
+      context.bindings.trustedFileQueue = message
+    }
   } else {
-    context.log('Blob does not exist')
+    context.log.error('Unable to retrieve blob')
   }
 }
---
+++ packages/document-service/src/helpers/upload-document.js
@@ -0,0 +1,17 @@
+const upload = async (logger, config, document) => {
+  const uploadConfig = createUploadConfiguration(logger, config)
+  // Delegate upload processing to the function referenced in the configuration.
+  // The configured funcion encapsulates all implementation specific details.
+  return config.functionConfig.uploadFunction(logger, uploadConfig, document)
+}
+
+const createUploadConfiguration = (logger, config) => {
+  // Clone the original configuration.
+  const uploadConfig = JSON.parse(JSON.stringify(config))
+  // Delete all configuration that does not need to be passed to the configured functions.
+  delete uploadConfig.functionConfig
+  logger.log('Upload configuration has been created')
+  return uploadConfig
+}
+
+export default upload
