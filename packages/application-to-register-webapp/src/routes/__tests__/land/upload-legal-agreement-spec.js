d8eea67b8416f087c8fe5008c23bccb43c3e0166
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-08-14 21:37:00 +0100

    Added upload legal agreement tests

---
+++ packages/application-to-register-webapp/src/routes/__tests__/land/upload-legal-agreement-spec.js
@@ -0,0 +1,85 @@
+import { submitGetRequest, uploadFile } from '../helpers/server.js'
+import { clearQueues, recreateContainers, recreateQueues } from '@defra/bng-azure-storage-test-utils'
+const LEGAL_AGREEMENT_FORM_ELEMENT_NAME = 'legalAgreement'
+const url = '/land/upload-legal-agreement'
+
+const mockDataPath = 'packages/application-to-register-webapp/src/__mock-data__/uploads/legal-agreements'
+jest.mock('../../../utils/azure-signalr.js')
+
+describe('Legal agreement upload controller tests', () => {
+  beforeAll(async () => {
+    await recreateQueues()
+  })
+  describe('GET', () => {
+    it(`should render the ${url.substring(1)} view`, async () => {
+      await submitGetRequest({ url })
+    })
+  })
+
+  describe('POST', () => {
+    const mockLegalAgreement = [
+      {
+        location: 'mockUserId/mockUploadType/mockFilename',
+        mapConfig: {}
+      }
+    ]
+    const baseConfig = {
+      uploadType: 'legal-agreement',
+      url,
+      formName: LEGAL_AGREEMENT_FORM_ELEMENT_NAME,
+      eventData: mockLegalAgreement
+    }
+
+    beforeEach(async () => {
+      await recreateContainers()
+      await clearQueues()
+    })
+
+    it('should upload legal agreement document to cloud storage', (done) => {
+      jest.isolateModules(async () => {
+        const uploadConfig = Object.assign({}, baseConfig)
+        uploadConfig.filePath = `${mockDataPath}/legal-agreement.pdf`
+        await uploadFile(uploadConfig)
+        setImmediate(() => {
+          done()
+        })
+      })
+    })
+
+    it('should cause an internal server error when file upload processing fails', (done) => {
+      jest.isolateModules(async () => {
+        const config = Object.assign({}, baseConfig)
+        config.filePath = `${mockDataPath}/legal-agreement.pdf`
+        config.generateFormDataError = true
+        await uploadFile(config)
+        setImmediate(() => {
+          done()
+        })
+      })
+    })
+
+    it('should cause an internal server error response when upload notification processing fails', (done) => {
+      jest.isolateModules(async () => {
+        const config = Object.assign({}, baseConfig)
+        config.filePath = `${mockDataPath}/legal-agreement.pdf`
+        config.generateHandleEventsError = true
+        await uploadFile(config)
+        setImmediate(() => {
+          done()
+        })
+      })
+    })
+    //TODO ensure that empty files test are processed correctly
+    //It is very intermittent at them oment
+    // it('should cause an internal server error when non-file data is uploaded', (done) => {
+    //   jest.isolateModules(async () => {
+    //     const config = Object.assign({}, baseConfig)
+    //     await uploadFile(config)
+    //     setImmediate(() => {
+    //       done()
+    //     })
+    //   })
+    // })
+    //Add tests to actually test the controller.
+  })
+})
