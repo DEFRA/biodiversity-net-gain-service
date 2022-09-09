f091dba648a98554cf1822a9e4e20bfe7f8cdc4c
Tedd <teddmason@gmail.com>
2022-09-09 13:02:34 +0100

    legal agreement error message and file size fix (#120)

--- packages/application-to-register-webapp/src/routes/land/check-legal-agreement.js
+++ packages/application-to-register-webapp/src/routes/land/check-legal-agreement.js
@@ -9,25 +9,29 @@ const handlers = {
   },
   post: async (request, h) => {
     const checkLegalAgreement = request.payload.checkLegalAgreement
-    const legalAgreementLocation = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_LOCATION)
+    const context = await getContext(request)
     request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_CHECKED, checkLegalAgreement)
     if (checkLegalAgreement === 'no') {
       // delete the file from blob storage
       const config = {
         containerName: 'trusted',
-        blobName: legalAgreementLocation
+        blobName: context.fileLocation
       }
       await blobStorageConnector.deleteBlobIfExists(config)
       request.yar.clear(constants.redisKeys.LEGAL_AGREEMENT_LOCATION)
       return h.redirect(constants.routes.UPLOAD_LEGAL_AGREEMENT)
     } else if (checkLegalAgreement === 'yes') {
-      return h.redirect('/' + constants.views.CHECK_LEGAL_AGREEMENT, {
-        filename: legalAgreementLocation === null ? '' : path.basename(legalAgreementLocation),
-        fileSize: request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_FILE_SIZE),
-        err: { text: '!TODO: Journey continuation not implemented' }
-      })
+      context.err = [{
+        text: '!TODO: Journey continuation not implemented',
+        href: '#check-upload-correct-yes'
+      }]
+      return h.redirect('/' + constants.views.CHECK_LEGAL_AGREEMENT, context)
     } else {
-      return h.view(constants.views.CHECK_LEGAL_AGREEMENT, { filename: path.basename(legalAgreementLocation), err: { text: 'Select yes if this is the correct file' } })
+      context.err = [{
+        text: 'Select yes if this is the correct file',
+        href: '#check-upload-correct-yes'
+      }]
+      return h.view(constants.views.CHECK_LEGAL_AGREEMENT, context)
     }
   }
 }
@@ -36,7 +40,8 @@ const getContext = async request => {
   const fileLocation = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_LOCATION)
   return {
     filename: fileLocation === null ? '' : path.parse(fileLocation).base,
-    fileSize: request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_FILE_SIZE)
+    fileSize: request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_FILE_SIZE),
+    fileLocation
   }
 }
 
