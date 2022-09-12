10ee5e83728755f5ecf5067c9d3b74c349826221
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-09-12 11:49:44 +0100

    Fixed merge conflict with constants.js

--- packages/application-to-register-webapp/src/routes/land/check-land-boundary.js
+++ packages/application-to-register-webapp/src/routes/land/check-land-boundary.js
@@ -11,7 +11,7 @@ const handlers = {
   post: async (request, h) => {
     const checkLandBoundary = request.payload.checkLandBoundary
     const landBoundaryLocation = request.yar.get(constants.redisKeys.LAND_BOUNDARY_LOCATION)
-    request.yar.set(constants.redisKeys.LAND_BOUNDAY_CHECKED, checkLandBoundary)
+    request.yar.set(constants.redisKeys.LAND_BOUNDARY_CHECKED, checkLandBoundary)
     if (checkLandBoundary === 'no') {
       // delete the file from blob storage
       const config = {
