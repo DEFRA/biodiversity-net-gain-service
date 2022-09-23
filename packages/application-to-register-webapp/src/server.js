67a8abadd7025c8f908c6d6c0baf1cd7c9893c89
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-09-23 09:07:12 +0100

    Register database intial comit

--- packages/application-to-register-webapp/src/server.js
+++ packages/application-to-register-webapp/src/server.js
@@ -7,7 +7,11 @@ import logging from './plugins/logging.js'
 import session from './plugins/session.js'
 import cache from './plugins/cache.js'
 import Blipp from 'blipp'
-import { KEEP_ALIVE_TIMEOUT_MS, SERVER_PORT } from './utils/config.js'
+import {
+  DATABASE_HOST, DATABASE_NAME, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, INITIALISE_DATABASE,
+  KEEP_ALIVE_TIMEOUT_MS, SERVER_PORT
+} from './utils/config.js'
+import createDatabaseConfiguration from './db-access/schema-init.js'
 
 const createServer = async options => {
   // Create the hapi server
@@ -41,7 +45,21 @@ const init = async server => {
   // Override the default keep alive timeout if required.
   // This is important for file uploads in containerised development environments.
   server.listener.keepAliveTimeout = parseInt(KEEP_ALIVE_TIMEOUT_MS) || server.listener.keepAliveTimeout
+  if (INITIALISE_DATABASE) {
+    const connected = await createDatabaseConfiguration(DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD)
+    if (connected) {
+      console.info('Database initialisation successful')
+      await startServer(server)
+    } else {
+      console.log(`Database initialisation failed ${connected} unable to start server`)
+    }
+  } else {
+    // Start the server
+    await startServer(server)
+  }
+}
 
+const startServer = async (server) => {
   // Start the server
   await server.start()
 }
