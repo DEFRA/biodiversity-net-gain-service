405989e47e73bf2a2da22540354f275c96d2b06a
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-09-30 10:02:48 +0100

    Initial commit

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
@@ -2,7 +2,23 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    return h.view(constants.views.LEGAL_AGREEMENT_TYPE)
+    const legalAgrementType = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
+    const selectedType = {selectedType: {
+      conservationType: false,
+      planningObligationType: false,
+      dontHave: false
+    }}
+    switch (legalAgrementType) {
+      case 'Conservation covenant':
+        selectedType.selectedType.conservationType = true
+        break
+      case 'Planning obligation (section 106 agreement)':
+        selectedType.selectedType.planningObligationType = true
+        break
+      case 'I do not have a legal agreement':
+        selectedType.selectedType.dontHave = true
+    }
+    return h.view(constants.views.LEGAL_AGREEMENT_TYPE, selectedType)
   },
   post: async (request, h) => {
     const legalAgrementType = request.payload.legalAgrementType
@@ -11,7 +27,7 @@ const handlers = {
       if (legalAgrementType !== 'I do not have a legal agreement') {
         return h.redirect(constants.routes.UPLOAD_LEGAL_AGREEMENT)
       } else if (legalAgrementType === 'I do not have a legal agreement') {
-        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {})
+        return h.redirect('/' + constants.views.LEGAL_AGREEMENT_TYPE, {})
       } else {
         return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {
           err: [{
