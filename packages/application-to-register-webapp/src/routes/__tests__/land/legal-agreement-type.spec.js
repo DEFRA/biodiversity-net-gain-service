6d8c9e2276f3180139d777ae98b397cdc6cb4731
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-09-30 12:41:49 +0100

    Added unit tests

--- packages/application-to-register-webapp/src/routes/__tests__/land/legal-agreement-type.spec.js
+++ packages/application-to-register-webapp/src/routes/__tests__/land/legal-agreement-type.spec.js
@@ -1,89 +1,18 @@
+import constants from '../../../utils/constants.js'
 import { submitGetRequest, submitPostRequest } from '../helpers/server.js'
-import constants from '../../../utils/constants'
 const url = '/land/legal-agreement-type'
 
 describe(url, () => {
-  const redisMap = new Map()
-
   describe('GET', () => {
-    it(`should render the ${url.substring(1)} view without any selection`, async () => {
+    it(`should render the ${url.substring(1)} view`, async () => {
       const response = await submitGetRequest({ url })
       expect(response.statusCode).toBe(200)
-    })
-
-    it(`should render the ${url.substring(1)} view with conservation selected`, async () => {
-      jest.isolateModules(async () => {
-        redisMap.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, 'Conservation covenant')
-        let viewResult, contextResult
-        const legalAgreementDetails = require('../../land/legal-agreement-type')
-        const request = {
-          yar: redisMap,
-          info: {
-            referrer: 'check-legal-agreement-details'
-          }
-        }
-        const h = {
-          view: (view, context) => {
-            viewResult = view
-            contextResult = context
-          }
-        }
-        await legalAgreementDetails.default[0].handler(request, h)
-        expect(viewResult).toEqual('land/legal-agreement-type')
-        expect(contextResult.conservationType).toEqual(true)
-      })
-    })
-
-    it(`should render the ${url.substring(1)} view with planning selected`, async () => {
-      jest.isolateModules(async () => {
-        redisMap.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, 'Planning obligation (section 106 agreement)')
-        let viewResult, contextResult
-        const legalAgreementDetails = require('../../land/legal-agreement-type')
-        const request = {
-          yar: redisMap,
-          info: {
-            referrer: 'agreement-details'
-          }
-        }
-        const h = {
-          view: (view, context) => {
-            viewResult = view
-            contextResult = context
-          }
-        }
-        await legalAgreementDetails.default[0].handler(request, h)
-        expect(viewResult).toEqual('land/legal-agreement-type')
-        expect(contextResult.planningObligationType).toEqual(true)
-      })
-    })
-
-    it(`should render the ${url.substring(1)} view with dont have selected`, async () => {
-      jest.isolateModules(async () => {
-        redisMap.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, 'I do not have a legal agreement')
-        let viewResult, contextResult
-        const legalAgreementDetails = require('../../land/legal-agreement-type')
-        const request = {
-          yar: redisMap,
-          info: {
-            referrer: 'check-legal-agreement-details'
-          }
-        }
-        const h = {
-          view: (view, context) => {
-            viewResult = view
-            contextResult = context
-          }
-        }
-        await legalAgreementDetails.default[0].handler(request, h)
-        expect(viewResult).toEqual('land/legal-agreement-type')
-        expect(contextResult.dontHave).toEqual(true)
-      })
     })
   })
 
   describe('POST', () => {
     let postOptions
-    beforeEach(async () => {
+    beforeEach(() => {
       postOptions = {
         url,
         payload: {}
@@ -96,24 +25,6 @@ describe(url, () => {
     })
 
     it('should allow the choice of Planning obligation (section 106 agreement) legal agreement', async () => {
-      postOptions.payload.legalAgrementType = 'Planning obligation (section 106 agreement)'
-      const response = await submitPostRequest(postOptions)
-      expect(response.statusCode).toBe(302)
-    })
-
-    it('should go back to detail if referred', async () => {
-      const h = {
-        view: jest.fn()
-      }
-      const request = {
-        yar: redisMap,
-        info: {
-          referrer: 'agreement-details'
-        }
-      }
-      const legalAgreementDetails = require('../../land/legal-agreement-type')
-      await legalAgreementDetails.default[0].handler(request, h)
-
       postOptions.payload.legalAgrementType = 'Planning obligation (section 106 agreement)'
       const response = await submitPostRequest(postOptions)
       expect(response.statusCode).toBe(302)
