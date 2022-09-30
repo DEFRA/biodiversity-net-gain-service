b7d50c9aac14fae58ef52c55941da27ca38cafe0
Tedd Mason <teddmason@gmail.com>
2022-09-30 16:00:04 +0100

    linting

--- packages/application-to-register-webapp/src/routes/confirmation.js
+++ packages/application-to-register-webapp/src/routes/confirmation.js
@@ -2,9 +2,9 @@ import constants from '../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-   return h.view(constants.views.CONFIRMATION, {
-    gainSiteReference: request.yar.get(constants.redisKeys.GAIN_SITE_REFERENCE)
-   })
+    return h.view(constants.views.CONFIRMATION, {
+      gainSiteReference: request.yar.get(constants.redisKeys.GAIN_SITE_REFERENCE)
+    })
   }
 }
 
