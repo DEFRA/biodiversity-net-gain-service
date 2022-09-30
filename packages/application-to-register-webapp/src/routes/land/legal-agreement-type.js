91d2a62426c68ed32fac81dfeb05d9c0323c3e32
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-09-30 10:02:48 +0100

    Initial commit

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
@@ -1,40 +1,40 @@
 import constants from '../../utils/constants.js'
-let referredFrom
-const handlers = {
 
+const handlers = {
   get: async (request, h) => {
-    referredFrom = request.info.referrer
-    const legalAgreementDocumentType = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
-    const documentType = {
+    const legalAgrementType = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
+    const selectedType = {selectedType: {
       conservationType: false,
       planningObligationType: false,
       dontHave: false
-    }
-    switch (legalAgreementDocumentType) {
+    }}
+    switch (legalAgrementType) {
       case 'Conservation covenant':
-        documentType.conservationType = true
+        selectedType.selectedType.conservationType = true
         break
       case 'Planning obligation (section 106 agreement)':
-        documentType.planningObligationType = true
+        selectedType.selectedType.planningObligationType = true
         break
-      default :
-        documentType.dontHave = true
-        break
+      case 'I do not have a legal agreement':
+        selectedType.selectedType.dontHave = true
     }
-    return h.view(constants.views.LEGAL_AGREEMENT_TYPE, documentType)
+    return h.view(constants.views.LEGAL_AGREEMENT_TYPE, selectedType)
   },
   post: async (request, h) => {
     const legalAgrementType = request.payload.legalAgrementType
-
     if (legalAgrementType !== undefined) {
       request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, legalAgrementType)
       if (legalAgrementType !== 'I do not have a legal agreement') {
-        if (referredFrom) {
-          return h.redirect(`/${constants.views.LEGAL_AGREEMENT_SUMMARY}`)
-        }
         return h.redirect(constants.routes.UPLOAD_LEGAL_AGREEMENT)
+      } else if (legalAgrementType === 'I do not have a legal agreement') {
+        return h.redirect('/' + constants.views.LEGAL_AGREEMENT_TYPE, {})
       } else {
-        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {})
+        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {
+          err: [{
+            text: 'Select which type of legal agreement you have',
+            href: '#legalAgrementType'
+          }]
+        })
       }
     } else {
       return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {
