b1055b0333c4f7e689db7c842cd665fc0ac78163
Tedd Mason <teddmason@gmail.com>
2022-10-03 13:53:02 +0100

    unit tests

---
+++ packages/application-to-register-webapp/src/utils/__tests__/http.spec.js
@@ -0,0 +1,41 @@
+import { postJson } from '../http.js'
+jest.mock('@hapi/wreck')
+
+describe('http', () => {
+  it('postJson should handle a 200 response code', () => {
+    jest.isolateModules(async () => {
+      const wreck = require('@hapi/wreck')
+      wreck.post = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            res: {
+              statusCode: 200
+            },
+            payload: '{ "foo": "bar" }'
+          })
+        })
+      })
+
+      const response = await postJson('/', {})
+      expect(response).toEqual({ foo : 'bar' })
+    })
+  })
+  it('postJson should handle a non 200 response code as an error', () => {
+    jest.isolateModules(async () => {
+      const wreck = require('@hapi/wreck')
+      wreck.post = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            res: {
+              statusCode: 400
+            }
+          })
+        })
+      })
+
+      await expect(postJson('/', {}))
+        .rejects
+        .toThrow('Requested resource returned a non 200 status code')
+    })
+  })
+})
