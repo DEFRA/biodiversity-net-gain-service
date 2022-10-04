08f87c77e457093177c1a1ad9952d23921f4ccd6
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-04 08:39:36 +0100

    Page flow completed

--- packages/application-to-register-webapp/src/utils/constants.js
+++ packages/application-to-register-webapp/src/utils/constants.js
@@ -8,6 +8,9 @@ const LEGAL_AGREEMENT_TYPE = 'land/legal-agreement-type'
 const LEGAL_AGREEMENT_FILE_OPTION = 'legal-agreement-file-option'
 const ADD_LEGAL_AGREEMENT_PARTIES = 'land/add-legal-agreement-parties'
 const LEGAL_AGREEMENT_START_DATE = 'land/legal-agreement-start-date'
+const LEGAL_AGREEMENT_FILE_OPTION = 'legal-agreement-file-option'
+const ADD_LEGAL_AGREEMENT_PARTIES = 'land/add-legal-agreement-parties'
+const LEGAL_AGREEMENT_START_DATE = 'land/legal-agreement-start-date'
 const CHECK_MANAGEMENT_PLAN = 'land/check-management-plan-file'
 const CHECK_YOUR_ANSWERS = 'check-your-answers'
 const CONFIRMATION = 'confirmation'
@@ -158,6 +161,7 @@ const redisKeys = {
   LEGAL_AGREEMENT_FILE_TYPE,
   LEGAL_AGREEMENT_FILE_OPTION,
   LEGAL_AGREEMENT_PARTIES,
+  LEGAL_AGREEMENT_FILE_OPTION,
   MANAGEMENT_PLAN_CHECKED,
   MANAGEMENT_PLAN_LOCATION,
   MANAGEMENT_PLAN_FILE_SIZE,
@@ -195,6 +199,8 @@ const routes = {
   CHECK_LEGAL_AGREEMENT,
   LEGAL_AGREEMENT_TYPE,
   LEGAL_AGREEMENT_TYPE,
+  ADD_LEGAL_AGREEMENT_PARTIES,
+  LEGAL_AGREEMENT_START_DATE,
   ADD_LEGAL_AGREEMENT_PARTIES,
   LEGAL_AGREEMENT_START_DATE,
   CHECK_MANAGEMENT_PLAN,
