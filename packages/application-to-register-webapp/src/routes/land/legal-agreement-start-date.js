df6675a0d6df3b52a5ed7ae65bb9839dabf5e54a
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-04 08:39:36 +0100

    Page flow completed

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-start-date.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-start-date.js
@@ -5,50 +5,11 @@ const handlers = {
     return h.view(constants.views.LEGAL_AGREEMENT_START_DATE)
   },
   post: async (request, h) => {
-    const legalAgreementStartDateDay = request.payload['legalAgreementStartDate-day']
-    const legalAgreementStartDateMonth = request.payload['legalAgreementStartDate-month']
-    const legalAgreementStartDateYear = request.payload['legalAgreementStartDate-year']
-
-    const datePattern = /^(0?[1-9]|[12][0-9]|3[01])[\\/\\-](0?[1-9]|1[012])[\\/\\-]\d{4}$/
-    const dateValue = [getDateValue(legalAgreementStartDateDay), getDateValue(legalAgreementStartDateMonth), legalAgreementStartDateYear].join('/')
-    const validDate = dateValue.match(datePattern)
-    if (!validDate) {
-      let errorMessage = 'Start date must be a real date'
-      if (legalAgreementStartDateDay.length === 0 && legalAgreementStartDateMonth.length === 0 && legalAgreementStartDateYear.length === 0) {
-        errorMessage = 'Enter the start date of the legal agreement'
-      } else {
-        if (legalAgreementStartDateDay.length === 0) {
-          errorMessage = 'Start date must include a day'
-        } else if (legalAgreementStartDateMonth.length === 0) {
-          errorMessage = 'Start date must include a month'
-        } else if (legalAgreementStartDateYear.length === 0) {
-          errorMessage = 'Start date must include a year'
-        }
-      }
-      return h.view(constants.views.LEGAL_AGREEMENT_START_DATE, {
-        err: [{
-          text: errorMessage,
-          href: '#legalAgreementStartDate'
-        }],
-        legalAgreementStartDateDay,
-        legalAgreementStartDateMonth,
-        legalAgreementStartDateYear
-      })
-    } else {
-      request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_START_DAY, legalAgreementStartDateDay)
-      request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_START_MONTH, legalAgreementStartDateMonth)
-      request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_START_YEAR, legalAgreementStartDateYear)
-      return h.view(constants.views.LEGAL_AGREEMENT_START_DATE, {
-        legalAgreementStartDateDay,
-        legalAgreementStartDateMonth,
-        legalAgreementStartDateYear
-      })
-    }
+    // const legalAgrementType = request.payload.legalAgrementType
+    return h.view(constants.views.LEGAL_AGREEMENT_START_DATE)
   }
 }
-function getDateValue (date) {
-  return date.length < 2 ? '0' + date : date
-}
+
 export default [{
   method: 'GET',
   path: constants.routes.LEGAL_AGREEMENT_START_DATE,
