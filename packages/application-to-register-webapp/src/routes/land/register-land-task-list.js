44ce71d007cf17c595a15070b455590dcd554567
Tedd Mason <teddmason@gmail.com>
2022-10-05 10:59:40 +0100

    unit tests

--- packages/application-to-register-webapp/src/routes/land/register-land-task-list.js
+++ packages/application-to-register-webapp/src/routes/land/register-land-task-list.js
@@ -1,16 +1,11 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (request, h) => h.view(constants.views.REGISTER_LAND_TASK_LIST),
-  post: async (request, h) => h.redirect(constants.routes.REGISTER_LAND_TASK_LIST)
+  get: async (request, h) => h.view(constants.views.REGISTER_LAND_TASK_LIST)
 }
 
 export default [{
   method: 'GET',
   path: constants.routes.REGISTER_LAND_TASK_LIST,
   handler: handlers.get
-}, {
-  method: 'POST',
-  path: constants.routes.REGISTER_LAND_TASK_LIST,
-  handler: handlers.post
 }]
