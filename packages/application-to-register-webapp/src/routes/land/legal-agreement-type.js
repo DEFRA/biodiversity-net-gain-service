e7ee6cfee1e54655fd04d88c0760febfe25c5de7
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-09 03:18:54 +0100

    Add legal agreement party stable

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
@@ -2,25 +2,7 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    const legalAgrementType = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
-    const selectedType = {
-      selectedType: {
-        conservationType: false,
-        planningObligationType: false,
-        dontHave: false
-      }
-    }
-    switch (legalAgrementType) {
-      case 'Conservation covenant':
-        selectedType.selectedType.conservationType = true
-        break
-      case 'Planning obligation (section 106 agreement)':
-        selectedType.selectedType.planningObligationType = true
-        break
-      case 'I do not have a legal agreement':
-        selectedType.selectedType.dontHave = true
-    }
-    return h.view(constants.views.LEGAL_AGREEMENT_TYPE, selectedType)
+    return h.view(constants.views.LEGAL_AGREEMENT_TYPE)
   },
   post: async (request, h) => {
     const legalAgrementType = request.payload.legalAgrementType
