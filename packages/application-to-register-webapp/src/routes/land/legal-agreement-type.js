65d669afff637bb932843c10f2127a5ef1402696
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-10 14:02:39 +0100

    Update to comply with sonar issues

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-type.js
@@ -1,26 +1,8 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (request, h) => {
-    const legalAgrementType = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
-    const selectedType = {
-      selectedType: {
-        conservationType: false,
-        planningObligationType: false,
-        dontHave: false
-      }
-    }
-    switch (legalAgrementType) {
-      case 'Conservation covenant':
-        selectedType.selectedType.conservationType = true
-        break
-      case 'Planning obligation (section 106 agreement)':
-        selectedType.selectedType.planningObligationType = true
-        break
-      case 'I do not have a legal agreement':
-        selectedType.selectedType.dontHave = true
-    }
-    return h.view(constants.views.LEGAL_AGREEMENT_TYPE, selectedType)
+  get: async (_request, h) => {
+    return h.view(constants.views.LEGAL_AGREEMENT_TYPE)
   },
   post: async (request, h) => {
     const legalAgrementType = request.payload.legalAgrementType
@@ -28,15 +10,8 @@ const handlers = {
       request.yar.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, legalAgrementType)
       if (legalAgrementType !== 'I do not have a legal agreement') {
         return h.redirect(constants.routes.UPLOAD_LEGAL_AGREEMENT)
-      } else if (legalAgrementType === 'I do not have a legal agreement') {
-        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {})
       } else {
-        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {
-          err: [{
-            text: 'Select which type of legal agreement you have',
-            href: '#legalAgrementType'
-          }]
-        })
+        return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {})
       }
     } else {
       return h.view(constants.views.LEGAL_AGREEMENT_TYPE, {
