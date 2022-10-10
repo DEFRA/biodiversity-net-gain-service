7bcdbb7887351b506896715a1278e41d39e9aed1
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-10 09:53:59 +0100

    Changed date regex for date validation

--- packages/application-to-register-webapp/src/routes/land/legal-agreement-start-date.js
+++ packages/application-to-register-webapp/src/routes/land/legal-agreement-start-date.js
@@ -9,7 +9,7 @@ const handlers = {
     const legalAgreementStartDateMonth = request.payload['legalAgreementStartDate-month']
     const legalAgreementStartDateYear = request.payload['legalAgreementStartDate-year']
 
-    const datePattern = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+    const datePattern = /^(0?[1-9]|[12][0-9]|3[01])[\\/\\-](0?[1-9]|1[012])[\\/\\-]\d{4}$/
     const dateValue = [getDateValue(legalAgreementStartDateDay), getDateValue(legalAgreementStartDateMonth), legalAgreementStartDateYear].join('/')
     const validDate = dateValue.match(datePattern)
     if (!validDate) {
