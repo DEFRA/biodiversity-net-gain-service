7b2e3ff5116dead2c5eb2defdc118a1bb6c59812
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-10 09:09:31 +0100

    Added units tests

--- packages/application-to-register-webapp/src/routes/__tests__/land/check-legal-agreement-details.spec.js
+++ packages/application-to-register-webapp/src/routes/__tests__/land/check-legal-agreement-details.spec.js
@@ -8,7 +8,9 @@ describe(url, () => {
   beforeEach(() => {
     rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE, 'Test type')
     rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_LOCATION, mockDataPath)
-    rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_START_DATE_KEY, '2020-03-11T00:00:00.000Z')
+    rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_START_DAY, '01')
+    rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_START_MONTH, '02')
+    rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_START_YEAR, '2022')
     rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_PARTIES, {
       organisations: [{
         index: 1,
@@ -40,7 +42,7 @@ describe(url, () => {
         expect(viewResult).toEqual('land/check-legal-agreement-details')
         expect(contextResult.legalAgreementType).toEqual('Test type')
         expect(contextResult.legalAgreementFileName).toEqual('legal-agreements')
-        expect(contextResult.legalAgreementStartDate).toEqual('11 Mar 2020')
+        expect(contextResult.legalAgreementStartDate).toEqual('1 Feb 2022')
       })
     })
   })
@@ -59,42 +61,10 @@ describe(url, () => {
           }
         }
         await legalAgreementDetails.default[1].handler(request, h)
-        expect(viewResult).toEqual('land/check-legal-agreement-details')
+        expect(viewResult).toEqual('/land/check-legal-agreement-details')
         expect(contextResult.legalAgreementType).toEqual('Test type')
         expect(contextResult.legalAgreementFileName).toEqual('legal-agreements')
-        expect(contextResult.legalAgreementStartDate).toEqual('11 Mar 2020')
-      })
-    })
-    it('should continue with the flow with other party role', async () => {
-      jest.isolateModules(async () => {
-        let viewResult, contextResult
-        const legalAgreementDetails = require('../../land/check-legal-agreement-details.js')
-        rdisMap.set(constants.redisKeys.LEGAL_AGREEMENT_PARTIES, {
-          organisations: [{
-            index: 1,
-            value: 'Test'
-          }],
-          roles: [{
-            otherPartyName: 'County Council',
-            organisationIndex: 1,
-            rowIndex: 0,
-            county_council: true
-          }]
-        })
-        const request = {
-          yar: rdisMap
-        }
-        const h = {
-          redirect: (view, context) => {
-            viewResult = view
-            contextResult = context
-          }
-        }
-        await legalAgreementDetails.default[1].handler(request, h)
-        expect(viewResult).toEqual('land/check-legal-agreement-details')
-        expect(contextResult.legalAgreementType).toEqual('Test type')
-        expect(contextResult.legalAgreementFileName).toEqual('legal-agreements')
-        expect(contextResult.legalAgreementStartDate).toEqual('11 Mar 2020')
+        expect(contextResult.legalAgreementStartDate).toEqual('1 Feb 2022')
       })
     })
   })
