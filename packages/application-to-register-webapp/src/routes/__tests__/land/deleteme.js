6e3cb74e262f1e6500ca1a1a7a4583435f0277a9
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-11 12:56:15 +0100

    Update with master

--- packages/application-to-register-webapp/src/routes/__tests__/land/deleteme.js
+++
@@ -1,7 +0,0 @@
-import moment from 'moment'
-
-const date = '2020-02-30'
-const dateFormat = 'YYYY-MM-DD'
-const toDateFormat = moment(date).format(dateFormat)
-const validDate = moment(toDateFormat, dateFormat, true).isValid()
-console.log('***** is it valid = ' + validDate)
