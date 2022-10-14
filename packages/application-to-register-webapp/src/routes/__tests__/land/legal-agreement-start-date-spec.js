adf46aed5b40ba71d360c7c453a3988cfcf41817
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-14 11:55:25 +0100

    Fixed rebase issues

--- packages/application-to-register-webapp/src/routes/__tests__/land/legal-agreement-start-date-spec.js
+++ packages/application-to-register-webapp/src/routes/__tests__/land/legal-agreement-start-date-spec.js
@@ -1,4 +1,5 @@
 import { submitGetRequest, submitPostRequest } from '../helpers/server.js'
+import constants from '../../../utils/constants'
 
 const url = '/land/legal-agreement-start-date'
 
@@ -50,6 +51,28 @@ describe(url, () => {
       const response = await submitPostRequest(postOptions)
       expect(response.statusCode).toBe(302)
       expect(response.headers.location).toBe('/land/check-legal-agreement-details')
+    })
+
+    it('should add a legal agreement start date and back to referrer', async () => {
+      const h = {
+        view: jest.fn()
+      }
+      const redisMap = new Map()
+      redisMap.set(constants.redisKeys.LEGAL_AGREEMENT_START_DATE_KEY, '22-10-2022')
+      const request = {
+        yar: redisMap,
+        info: {
+          referrer: 'check-legal-agreement-details'
+        }
+      }
+      const legalAgreementParties = require('../../land/legal-agreement-start-date')
+      await legalAgreementParties.default[0].handler(request, h)
+      postOptions.payload['legalAgreementStartDate-day'] = '01'
+      postOptions.payload['legalAgreementStartDate-month'] = '01'
+      postOptions.payload['legalAgreementStartDate-year'] = '1971'
+
+      const response = await submitPostRequest(postOptions)
+      expect(response.statusCode).toBe(302)
     })
 
     it('should fail to add a legal agreement start date with empty dates', async () => {
