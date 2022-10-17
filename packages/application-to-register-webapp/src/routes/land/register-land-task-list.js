94e2b984e9736ece3dd2c48abaae94b7185a47ee
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-17 09:01:09 +0100

    Work in progress

--- packages/application-to-register-webapp/src/routes/land/register-land-task-list.js
+++ packages/application-to-register-webapp/src/routes/land/register-land-task-list.js
@@ -1,7 +1,58 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (_request, h) => h.view(constants.views.REGISTER_LAND_TASK_LIST)
+  get: async (request, h) => {
+    const dataContent = {
+      taskList: [
+        {
+          taskTitle: 'Your details',
+          tasks: [
+            {
+              title: 'Add your details',
+              status: 'NOT STARTED',
+              taskUrl: '/land/legal-agreement-type'
+            }
+          ]
+        },
+        {
+          taskTitle: 'Land information',
+          tasks: [
+            {
+              title: 'Add land boundary details',
+              status: 'NOT STARTED'
+            },
+            {
+              title: 'Add land ownership details',
+              status: 'NOT STARTED'
+            }
+          ]
+        },
+        {
+          taskTitle: 'Habitat information',
+          tasks: [
+            {
+              title: 'Upload Biodiversity Metric 3.1',
+              status: 'NOT STARTED'
+            },
+            {
+              title: 'Add habitat management and monitoring details',
+              status: 'NOT STARTED'
+            }
+          ]
+        },
+        {
+          taskTitle: 'Legal information',
+          tasks: [
+            {
+              title: 'Add legal agreement details',
+              status: 'NOT STARTED'
+            }
+          ]
+        }
+      ]
+    }
+    return h.view(constants.views.REGISTER_LAND_TASK_LIST, dataContent)
+  }
 }
 
 export default [{
