3791442a8560bef34e5d6f28fb739bf58534324f
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-20 11:28:13 +0100

    Updated JS file details

--- packages/application-to-register-webapp/client/js/pages/land/add-legal-agreement-parties.js
+++ packages/application-to-register-webapp/client/js/pages/land/add-legal-agreement-parties.js
@@ -1,10 +1,19 @@
-/* eslint-env jquery */
+/* global $ */
 $(document).ready(function () {
   if (window.location.pathname === '/land/add-legal-agreement-parties') {
     $('.govuk-body').bind('DOMSubtreeModified', function () {
+      const count = $('.govuk-grid-row').find('.moj-add-another__item').length
+      const errorContainer = $('.govuk-grid-row').find('.moj-add-another__item')[count - 1]
+      if (count > 1 && $(errorContainer).find('div').hasClass('govuk-form-group--error')) {
+        $(errorContainer).find('div').removeClass('govuk-form-group--error')
+        $(errorContainer).find('div').find('p').removeClass('govuk-error-message')
+        $(errorContainer).find('div').find('input').removeClass('govuk-input--error')
+      }
+
       $('.govuk-radios__item').click((event) => {
         if ($(event.target.parentNode).next('div').hasClass('govuk-radios__conditional--hidden')) {
           $(event.target.parentNode).next('div').removeClass('govuk-radios__conditional--hidden')
+          $(event.target.parentNode).next('div').find('input').val('')
         } else {
           $(event.target.parentNode).parent().show().find('.govuk-radios__conditional').addClass('govuk-radios__conditional--hidden')
         }
