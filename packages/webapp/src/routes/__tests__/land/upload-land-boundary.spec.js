9ee4f722bde730ea91d345cb2808ae5afa52de13
Jean-Rene Lingofe <rene.lingofe@4energy.co.uk>
2022-10-31 14:28:50 +0000

    BNG-527 fixing navigation issues

--- packages/webapp/src/routes/__tests__/land/upload-land-boundary.spec.js
+++ packages/webapp/src/routes/__tests__/land/upload-land-boundary.spec.js
@@ -3,7 +3,7 @@ import { clearQueues, recreateContainers, recreateQueues } from '@defra/bng-azur
 const LAND_BOUNDARY_FORM_ELEMENT_NAME = 'landBoundary'
 const url = '/land/upload-land-boundary'
 
-const mockDataPath = 'packages/webapp/src/__mock-data__/uploads/legal-agreements'
+const mockDataPath = 'packages/application-to-register-webapp/src/__mock-data__/uploads/legal-agreements'
 jest.mock('../../../utils/azure-signalr.js')
 
 describe('Land boundary upload controller tests', () => {
