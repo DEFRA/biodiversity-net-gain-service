d1f3b9eb7b793d1db8ed7aec4296e37fff18148f
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2022-11-04 14:49:11 +0530

    Added connector lib for metric file data extraction

--- packages/connectors-lib/src/azure-blob-storage-connector.js
+++ packages/connectors-lib/src/azure-blob-storage-connector.js
@@ -74,6 +74,18 @@ const getBlockBlobClient = (containerName, blobName) => {
   return containerClient.getBlockBlobClient(blobName)
 }
 
+const downloadBlobToFileIfExists = async (logger, config) => {
+  const blockBlobClient = getBlockBlobClient(config.containerName, config.blobName)
+  const blobExists = await blockBlobClient.exists()
+  let returnValue;
+  if(blobExists){
+    returnValue = await blockBlobClient.downloadToFile(config.fileNameWithPath);
+  } else {
+    logger.log('Blob does not exist');
+  }
+  return returnValue;
+}
+
 export const blobStorageConnector = Object.freeze({
   copyBlob,
   deleteBlobIfExists,
