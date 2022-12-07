4c32c2add764b72f4064bea5e2ca4cd40b3eb126
Tedd <teddmason@gmail.com>
2022-12-07 12:26:54 +0000

    Addition of geospatial uploads to journey (#208)
    * Additional of geospatial uploads to journey
    
    several test fixes
    
    unit test fixes
    
    sonarcloud smells
    
    taking out css that wasn't needed
    
    * updating file reference
    
    * adding in data-testid attributes for Shaun
    
    * Fixing WGS84 hectares calculation, and map view extent to fit to feature
    
    * adding some unit tests to cover hectare and grid ref calculations

--- packages/geoprocessing-service/jest.config.cjs
+++ packages/geoprocessing-service/jest.config.cjs
@@ -4,5 +4,8 @@ module.exports = {
     '@defra/bng-connectors-lib/azure-storage': '<rootDir>/node_modules/@defra/bng-azure-storage-test-utils/node_modules/@defra/bng-connectors-lib/src/helpers/azure-storage.js'
   },
   setupFiles: ['<rootDir>/.jest/test.env.js'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.js']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(geodesy)/)'
+  ]
 }
