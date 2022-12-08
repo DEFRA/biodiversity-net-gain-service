f4ecb6ad0f44819e73cbc4565f93d597b1baac30
ajjawalk <ajinkya.jawalkar@capgemini.com>
2022-12-08 15:26:48 +0000

    WIP: Test cases need to fix for check-metric-file.js

--- packages/webapp/src/routes/__tests__/developer/check-metric-file.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-metric-file.spec.js
@@ -1,6 +1,25 @@
 import constants from '../../../utils/constants.js'
 import { submitGetRequest, submitPostRequest } from '../helpers/server.js'
 const url = '/developer/check-metric-file'
+const mockDataPath = 'packages/webapp/src/__mock-data__/uploads/metric-file'
+const mockMetricData = {
+  startPage: {
+    planningAuthority: 'Your District Council ',
+    projectName: 'A Major Development',
+    applicant: 'A Developer',
+    applicationType: 'Outline Planning',
+    planningApplicationReference: 'A111111',
+    assessor: 'A.Junior',
+    reviewer: 'A.Senior',
+    metricVersion: 'v1.1',
+    assessmentDate: 44441,
+    planningAuthorityReviewer: 'A.N.Officer',
+    cellStyleConventions: undefined,
+    enterData: undefined,
+    automaticLookup: undefined,
+    result: undefined
+  }
+}
 
 describe(url, () => {
   describe('GET', () => {
@@ -10,16 +29,44 @@ describe(url, () => {
   })
 
   describe('POST', () => {
-    let postOptions
+    let redisMap, postOptions
+    const mockFileLocation = `${mockDataPath}/metric-file.xlsx`
     beforeEach(() => {
+      redisMap = new Map()
       postOptions = {
         url,
         payload: {}
       }
     })
-    it('should allow confirmation that the correct metric file has been uploaded', async () => {
+
+    // it('should allow confirmation that the correct metric file has been uploaded', async () => {
+    //   postOptions.payload.checkUploadMetric = constants.confirmLandBoundaryOptions.YES
+    //   await submitPostRequest(postOptions)
+    // })
+
+    it('should allow confirmation that the correct metric file has been uploaded', done => {
       postOptions.payload.checkUploadMetric = constants.confirmLandBoundaryOptions.YES
-      await submitPostRequest(postOptions)
+      jest.isolateModules(async () => {
+        try {
+          let viewResult
+          const confirmDevelopmentDetails = require('../../developer/check-metric-file.js')
+          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, mockFileLocation)
+          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockMetricData.startPage)
+          const request = {
+            yar: redisMap
+          }
+          const h = {
+            view: (view) => {
+              viewResult = view
+            }
+          }
+          await confirmDevelopmentDetails.default[0].handler(request, h)
+          expect(viewResult).toEqual(url.substring(url.indexOf('/') + 1))
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
     })
 
     it('should allow an alternative metric file to be uploaded ', async () => {
--- packages/webapp/src/routes/developer/check-metric-file.js
+++ packages/webapp/src/routes/developer/check-metric-file.js
@@ -1,6 +1,10 @@
 import constants from '../../utils/constants.js'
 import path from 'path'
 import { blobStorageConnector } from '@defra/bng-connectors-lib'
+import { logger } from 'defra-logging-facade'
+import { extractMetricData } from '../../utils/extract-developer-metric.js'
+import { downloadStreamAndQueueMessage } from '../../utils/azure-storage.js'
+import { handleEvents } from '../../utils/azure-signalr.js'
 
 const href = '#check-upload-correct-yes'
 const handlers = {
@@ -22,9 +26,15 @@ const handlers = {
       request.yar.clear(constants.redisKeys.DEVELOPER_METRIC_LOCATION)
       return h.redirect(constants.routes.DEVELOPER_UPLOAD_METRIC)
     } else if (checkUploadMetric === constants.CHECK_UPLOAD_METRIC_OPTIONS.YES) {
-      return h.redirect('/' + constants.views.DEVELOPER_CONFIRM_DEV_DETAILS, {
-        err: { text: '!TODO: Journey continuation not implemented' }
-      })
+      const metricFileName = request.yar.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION)
+      const config = buildConfig(request.yar.id, path.basename(metricFileName))
+      try {
+        const metricFileData = await extractMetricData(logger, config)
+        request.yar.set(constants.redisKeys.DEVELOPER_METRIC_DATA, metricFileData[0].metricData)
+        return h.redirect('/' + constants.views.DEVELOPER_CONFIRM_DEV_DETAILS)
+      } catch (error) {
+        logger.error(error)
+      }
     } else {
       return h.view(constants.views.DEVELOPER_CHECK_UPLOAD_METRIC, {
         filename: path.basename(metricUploadLocation),
@@ -45,6 +55,52 @@ const getContext = request => {
   return {
     filename: fileLocation === null ? '' : path.parse(fileLocation).base,
     fileSize: request.yar.get(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE)
+  }
+}
+
+const buildConfig = (sessionId, fileName) => {
+  const config = {}
+  buildBlobConfig(sessionId, config, fileName)
+  buildQueueConfig(config)
+  buildFunctionConfig(config)
+  buildSignalRConfig(sessionId, config)
+  buildFileValidationConfig(config)
+  return config
+}
+
+const buildBlobConfig = (sessionId, config, fileName) => {
+  config.blobConfig = {
+    blobName: `${sessionId}/${constants.uploadTypes.DEVELOPER_METRIC_UPLOAD_TYPE}/${fileName}`,
+    containerName: 'trusted',
+    fileName
+  }
+}
+
+const buildQueueConfig = config => {
+  config.queueConfig = {
+    uploadType: constants.uploadTypes.DEVELOPER_METRIC_EXTRACTION_UPLOAD_TYPE,
+    queueName: 'trusted-file-queue'
+  }
+}
+
+const buildFunctionConfig = config => {
+  config.functionConfig = {
+    extractMetricFunction: downloadStreamAndQueueMessage,
+    handleEventsFunction: handleEvents
+  }
+}
+
+const buildSignalRConfig = (sessionId, config) => {
+  config.signalRConfig = {
+    eventProcessingFunction: null,
+    timeout: parseInt(process.env.UPLOAD_PROCESSING_TIMEOUT_MILLIS) || 180000,
+    url: `${process.env.SIGNALR_URL}?userId=${sessionId}`
+  }
+}
+
+const buildFileValidationConfig = config => {
+  config.fileValidationConfig = {
+    fileExt: constants.metricFileExt
   }
 }
 
