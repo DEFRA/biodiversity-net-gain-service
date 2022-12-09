c2c4d42e38c96dc95f200ba17de6b61e00dacccd
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2022-12-09 15:47:18 +0530

    WIP: Added more test cases for coverage

--- packages/webapp/src/routes/__tests__/developer/check-metric-file.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-metric-file.spec.js
@@ -1,7 +1,9 @@
 import constants from '../../../utils/constants.js'
-import { submitGetRequest, submitPostRequest } from '../helpers/server.js'
+import { submitPostRequest } from '../helpers/server.js'
+
 const url = '/developer/check-metric-file'
 const mockDataPath = 'packages/webapp/src/__mock-data__/uploads/metric-file'
+const mockFileLocation = `${mockDataPath}/metric-file.xlsx`
 const mockMetricData = {
   startPage: {
     planningAuthority: 'Your District Council ',
@@ -23,14 +25,30 @@ const mockMetricData = {
 
 describe(url, () => {
   describe('GET', () => {
+    let viewResult, contextResult
+    const redisMap = new Map()
     it(`should render the ${url.substring(1)} view`, async () => {
-      await submitGetRequest({ url })
+      const checkMetricFile = require('../../developer/check-metric-file.js')
+      redisMap.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, mockFileLocation)
+      // redisMap.set(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE, (4 * 1024))
+      const request = {
+        yar: redisMap
+      }
+      const h = {
+        view: (view, context) => {
+          viewResult = view
+          contextResult = context
+        }
+      }
+      await checkMetricFile.default[0].handler(request, h)
+      expect(viewResult).toEqual(constants.views.DEVELOPER_CHECK_UPLOAD_METRIC)
+      expect(contextResult.filename).toEqual('metric-file.xlsx')
     })
   })
 
   describe('POST', () => {
+    jest.mock('@defra/bng-connectors-lib')
     let redisMap, postOptions
-    const mockFileLocation = `${mockDataPath}/metric-file.xlsx`
     beforeEach(() => {
       redisMap = new Map()
       postOptions = {
@@ -39,43 +57,63 @@ describe(url, () => {
       }
     })
 
-    // it('should allow confirmation that the correct metric file has been uploaded', async () => {
-    //   postOptions.payload.checkUploadMetric = constants.confirmLandBoundaryOptions.YES
-    //   await submitPostRequest(postOptions)
-    // })
-
-    it('should allow confirmation that the correct metric file has been uploaded', done => {
+    it('should allow confirmation that the correct metric file has been uploaded', (done) => {
       jest.isolateModules(async () => {
         try {
-          let viewResult
-          const confirmDevelopmentDetails = require('../../developer/check-metric-file.js')
+          const checkMetricFile = require('../../developer/check-metric-file.js')
           redisMap.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, mockFileLocation)
-          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockMetricData.startPage)
+          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockMetricData)
           const request = {
             yar: redisMap,
             payload: {
               checkUploadMetric: constants.confirmLandBoundaryOptions.YES
             }
           }
-          const h = {
-
-            redirect: (view) => {
-              viewResult = view
-            }
-          }
-          await confirmDevelopmentDetails.default[1].handler(request, h)
-          expect(viewResult).toEqual(url.substring(url.indexOf('/') + 1))
+          const extractDeveloperMetric = require('../../../utils/extract-developer-metric.js')
+          const spy = jest.spyOn(extractDeveloperMetric, 'extractMetricData')
+          await checkMetricFile.default[1].handler(request)
+          expect(spy).toHaveBeenCalledTimes(1)
           done()
         } catch (err) {
           done(err)
         }
       })
-    })
+    }, 1800000)
 
-    it('should allow an alternative metric file to be uploaded ', async () => {
-      postOptions.payload.checkUploadMetric = constants.confirmLandBoundaryOptions.NO
-      const response = await submitPostRequest(postOptions)
-      expect(response.headers.location).toBe(constants.routes.DEVELOPER_UPLOAD_METRIC)
+    it('should allow an alternative metric file to be uploaded ', (done) => {
+      jest.isolateModules(async () => {
+        try {
+          let viewResult
+          const checkMetricFile = require('../../developer/check-metric-file.js')
+          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, mockFileLocation)
+          postOptions.payload.checkUploadMetric = constants.confirmLandBoundaryOptions.NO
+          const request = {
+            yar: redisMap,
+            payload: {
+              checkUploadMetric: constants.confirmLandBoundaryOptions.NO
+            }
+          }
+          const h = {
+            redirect: (view) => {
+              viewResult = view
+            },
+            view: (view) => {
+              viewResult = view
+            }
+          }
+          const { blobStorageConnector } = require('@defra/bng-connectors-lib')
+          const spy = jest.spyOn(blobStorageConnector, 'deleteBlobIfExists')
+          await checkMetricFile.default[1].handler(request, h)
+          expect(viewResult).toEqual(constants.routes.DEVELOPER_UPLOAD_METRIC)
+          expect(spy).toHaveBeenCalledWith({
+            containerName: 'untrusted',
+            blobName: mockFileLocation
+          })
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
     })
 
     it('should detect an invalid response from user', async () => {
