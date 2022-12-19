3244fc9bd2dcbf2d2271698b9046e05b88538a71
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-12-19 15:32:11 +0000

    Add database environment variables for unit tests

--- bin/init-jest-debug-env-file.sh
+++ bin/init-jest-debug-env-file.sh
@@ -6,4 +6,5 @@ mkdir -p ${JEST_DEBUG_DIR}
 echo NODE_DEV=test > ${JEST_DEBUG_ENV_FILE}
 grep "export AZURE" bin/run-unit-tests.sh | sed 's/^export //' >> ${JEST_DEBUG_ENV_FILE}
 grep "export AZURE\|export SIGNALR" docker/secrets/WEBAPP_ENV | sed 's/^export //' >> ${JEST_DEBUG_ENV_FILE}
+grep "export POSTGRES" docker/secrets/DATABASE_VERSION_CONTROL_ENV | sed 's/^export //' >> ${JEST_DEBUG_ENV_FILE}
 echo "(Re)created ${JEST_DEBUG_ENV_FILE}"
