557ffa07d16f71ac1513af69cbd854dc8847917d
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-12-19 15:35:32 +0000

    Rename environment variables

--- packages/database-version-control/src/migrations/2022.12.16T14.14.02.create-bng-user.js
+++ packages/database-version-control/src/migrations/2022.12.16T14.14.02.create-bng-user.js
@@ -1,14 +1,14 @@
 /** @type {import('@slonik/migrator').Migration} */
 exports.up = async ({ context: { connection, sql } }) => {
   let createRoleSql
-  if (process.env.BNG_USER_PASSWORD) {
+  if (process.env.POSTGRES_BNG_USER_PASSWORD) {
     // Prepare to create a user in a non-Microsoft Azure Database for Postgres instance.
-    const bngUserPassword = sql.literalValue([process.env.BNG_USER_PASSWORD])
+    const bngUserPassword = sql.literalValue([process.env.POSTGRES_BNG_USER_PASSWORD])
     createRoleSql = sql`CREATE ROLE bng_user WITH LOGIN PASSWORD ${bngUserPassword} IN ROLE bng_readwrite;`
-  } else if (process.env.BNG_CLIENT_ID) {
+  } else if (process.env.POSTGRES_BNG_CLIENT_ID) {
     // Prepare to create a managed identity based user in a Microsoft Azure Database for Postgres instance.
     // https://techcommunity.microsoft.com/t5/azure-database-for-postgresql/connect-from-function-app-with-managed-identity-to-azure/ba-p/1517032
-    const bngClientId = sql.literalValue([process.env.BNG_CLIENT_ID])
+    const bngClientId = sql.literalValue([process.env.POSTGRES_BNG_CLIENT_ID])
     createRoleSql = sql`
       SET aad_validate_oids_in_tenant = off;
       CREATE ROLE bng_user WITH LOGIN PASSWORD ${bngClientId} IN ROLE azure_ad_user, bng_readwrite;
@@ -27,7 +27,7 @@ exports.up = async ({ context: { connection, sql } }) => {
       $$
     `)
   } else {
-    throw new Error('BNG_USER_PASSWORD or BNG_CLIENT_ID must be specified to create the BNG database user')
+    throw new Error('POSTGRES_BNG_USER_PASSWORD or POSTGRES_BNG_CLIENT_ID must be specified to create the BNG database user')
   }
 }
 
