f32ec859db39d3f1607e7b3ce5367f8a49645705
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2022-12-21 13:31:04 +0000

    Add database version control installation support

--- bin/local-install.sh
+++ bin/local-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 (cd packages/azure-functions && npm run local:install)
+(cd packages/database-version-control && npm run local:install)
 (cd packages/webapp && npm run local:install )
 ./bin/init-jest-debug-env-file.sh
