875ef7bdfa33410e05a40a62a33c6319dab422fb
Tedd Mason <teddmason@gmail.com>
2023-01-05 14:46:04 +0000

    stopping generate application reference call if already exists

--- packages/azure-functions/ProcessApplication/index.mjs
+++ packages/azure-functions/ProcessApplication/index.mjs
@@ -16,11 +16,13 @@ const buildConfig = body => {
 export default async function (context, req) {
   context.log('Processing', JSON.stringify(req.body))
   try {
-    // Generate gain site reference
-    const db = new postgresConnector.Db(process.env.POSTGRES_CONNECTION_STRING)
-    const applicationReference = await createApplicationReference(db)
-    req.body.landownerGainSiteRegistration.gainSiteReference = applicationReference.rows[0].fn_create_application_reference
-    await db.end()
+    // Generate gain site reference if not already present
+    if (!req.body.landownerGainSiteRegistration.gainSiteReference) {
+      const db = new postgresConnector.Db(process.env.POSTGRES_CONNECTION_STRING)
+      const applicationReference = await createApplicationReference(db)
+      req.body.landownerGainSiteRegistration.gainSiteReference = applicationReference.rows[0].fn_create_application_reference
+      await db.end()
+    }
 
     const config = buildConfig(req.body)
     context.bindings.outputSbQueue = config.serviceBusConfig.message
