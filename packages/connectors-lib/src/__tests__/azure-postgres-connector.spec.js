38ca75f9d5058b967ce96741c74bbdc79ad34105
Tedd Mason <teddmason@gmail.com>
2023-01-06 12:09:15 +0000

    linting

--- packages/connectors-lib/src/__tests__/azure-postgres-connector.spec.js
+++ packages/connectors-lib/src/__tests__/azure-postgres-connector.spec.js
@@ -4,14 +4,15 @@ jest.mock('pg')
 describe('PostgresConnector', () => {
   it('should initialise', () => {
     const db = new postgresConnector.Db('testConnectionString')
+    expect(typeof db.init).toEqual('function')
+    expect(typeof db.query).toEqual('function')
+    expect(typeof db.end).toEqual('function')
   })
   it('should query', () => {
-    const pg = require('pg')
     const db = new postgresConnector.Db('testConnectionString')
     db.query('test query')
   })
   it('should end', async () => {
-    const pg = require('pg')
     const db = new postgresConnector.Db('testConnectionString')
     await db.end()
   })
