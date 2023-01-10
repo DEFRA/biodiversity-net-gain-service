4cd2d2b3bd0efab6165d1d1b2b63d73c2b801918
Tedd Mason <teddmason@gmail.com>
2023-01-10 13:50:44 +0000

    adding in extra test coverage

--- packages/database-version-control/src/__tests__/migrator-spec.js
+++ packages/database-version-control/src/__tests__/migrator-spec.js
@@ -3,12 +3,16 @@ const migrator = require('../migrator.js')
 
 describe('The database version control migrator', () => {
   it('should initialise correctly and be able to run and rollback all pending migrations programmatically', async () => {
-    process.env.POSTGRES_SSL_MODE = 'require'
     const migrations = fs.readdirSync('packages/database-version-control/src/migrations')
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
     await migrator.up()
     expect((await migrator.pending()).map(m => m.name)).toEqual([])
     await migrator.down({ to: 0 })
+    expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
+  }, 10000)
+  it('should initialise correctly when POSTGRES_SSL_MODE is set to require', async () => {
+    process.env.POSTGRES_SSL_MODE = 'require'
+    const migrations = fs.readdirSync('packages/database-version-control/src/migrations')
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
   }, 10000)
 })
