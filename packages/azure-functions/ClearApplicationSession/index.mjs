951be66def6ec87589592a21da87025be6d43a1a
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-02-09 20:27:24 +0000

    Move database connection closure to finally block

--- packages/azure-functions/ClearApplicationSession/index.mjs
+++ packages/azure-functions/ClearApplicationSession/index.mjs
@@ -1,8 +1,13 @@
 import { clearApplicationSession } from '../Shared/db-queries.js'
 import getDBConnection from '../Shared/get-db-connection.js'
 
-export default async function (_context, _req) {
-  const db = await getDBConnection()
-  await clearApplicationSession(db)
-  await db.end()
+export default async function (context, _timer) {
+  let db
+  try {
+    const db = await getDBConnection()
+    context.log('Clearing expired application sessions')
+    await clearApplicationSession(db)
+  } finally {
+    await db?.end()
+  }
 }
