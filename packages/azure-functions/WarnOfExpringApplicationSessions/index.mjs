56068aecc61428cf0c70a9bba335d6aa1775131b
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-02-09 20:36:46 +0000

    Warn of expiring application sessions

---
+++ packages/azure-functions/WarnOfExpringApplicationSessions/index.mjs
@@ -0,0 +1,20 @@
+import { getExpiringApplicationSessions } from '../Shared/db-queries.js'
+import getDBConnection from '../Shared/get-db-connection.js'
+
+export default async function (context, _timer) {
+  let db
+  try {
+    db = await getDBConnection()
+    context.bindings.expiringApplicationSessionNotificationQueue = []
+    context.log('Getting application sessions that require warning of expiry')
+    const result = await getExpiringApplicationSessions(db)
+    for (const row of result?.rows) {
+      context.bindings.expiringApplicationSessionNotificationQueue.push({
+        id: row.application_session_id,
+        notificationType: 'email'
+      })
+    }
+  } finally {
+    await db?.end()
+  }
+}
