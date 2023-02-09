1a4cbe980d330a82786eba3ff44d91a0916d9ab4
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-02-09 12:04:00 +0000

    offsite habitat baseline table data display

--- packages/webapp/src/routes/developer/confirm-off-site-gain.js
+++ packages/webapp/src/routes/developer/confirm-off-site-gain.js
@@ -3,7 +3,31 @@ import constants from '../../utils/constants.js'
 const handlers = {
   get: (request, h) => {
     const context = getContext(request)
-    return h.view(constants.views.DEVELOPER_CONFIRM_OFF_SITE_GAIN, context)
+    const offSiteHabitatTableContent = context.offSiteHabitatBaseline.map(item => item.broadHabitat !== null
+      ? (
+          [
+            {
+              html: "<span class='govuk-body-m govuk-!-display-block govuk-!-margin-top-0 govuk-!-margin-bottom-0'>" + item.broadHabitat + "</span> <span class='govuk-body-s govuk-!-display-block govuk-!-margin-top-0 govuk-!-margin-bottom-0'>" + item.habitatType + '</span>',
+              classes: 'govuk-!-width-two-thirds'
+            },
+            {
+              text: item.score
+            }
+          ]
+        )
+      : null)
+    offSiteHabitatTableContent.push(
+      [
+        {
+          html: "<span class='govuk-body-m govuk-!-display-block govuk-!-margin-top-0 govuk-!-margin-bottom-0'>Biodiversity net gain added</span>",
+          classes: 'govuk-!-width-two-thirds'
+        },
+        {
+          text: '3%' // THIS VALUE CALCULATION SHOULD BE DONE
+        }
+      ]
+    )
+    return h.view(constants.views.DEVELOPER_CONFIRM_OFF_SITE_GAIN, { offSiteHabitatTableContent, ...context })
   },
   post: async (request, h) => {
     const confirmOffsiteGain = request.payload.confirmOffsiteGain
