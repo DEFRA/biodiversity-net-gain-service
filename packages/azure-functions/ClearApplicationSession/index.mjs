85ad3dde71d1c2f92631076dfee95f5426bd85f9
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-02-10 22:09:28 +0000

    Increase code quality

--- packages/azure-functions/ClearApplicationSession/index.mjs
+++ packages/azure-functions/ClearApplicationSession/index.mjs
@@ -4,7 +4,7 @@ import getDBConnection from '../Shared/get-db-connection.js'
 export default async function (context, _timer) {
   let db
   try {
-    const db = await getDBConnection()
+    db = await getDBConnection()
     context.log('Clearing expired application sessions')
     await clearApplicationSession(db)
   } finally {
