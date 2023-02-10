4c0d969d2a874398072d9192886ba4a3c6c77b97
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-02-10 21:56:03 +0000

    Increase delay

--- bin/start-test-double-infrastructure.sh
+++ bin/start-test-double-infrastructure.sh
@@ -2,6 +2,6 @@
 export DOCKER_BUILDKIT=1
 docker-compose -f docker/infrastructure.yml up -d azure_services azurite postgis
 echo Waiting for test double infrastructure to initialise
-# Sleep for thirty seconds as the nc utility appears to report successful connectivity on test double infrastructure ports before requests can be processed successfully.
-sleep 30
+# Sleep for sixty seconds as the nc utility appears to report successful connectivity on test double infrastructure ports before requests can be processed successfully.
+sleep 60
 npm run bootstrap-azurite
