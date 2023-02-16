011f8fda1b2fbf1c6a033aefcb1ac976aa6622c2
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-02-16 16:33:47 +0530

    WIP: PR code smell fixes for #233

--- packages/bng-metric-service/src/helpers/bng-metric-extraction-service.js
+++ packages/bng-metric-service/src/helpers/bng-metric-extraction-service.js
@@ -28,17 +28,16 @@ const readAndExtractContent = async (contentInputStream, extractionConfiguration
 }
 
 const extractSingleSheetContent = (workbook, extractionConfiguration) => {
-  // console.log("WB", workbook)
   if (_.isUndefined(extractionConfiguration) || _.isUndefined(workbook)) {
     logger.log(`${new Date().toUTCString()} Undifined metric extraction config`)
-    return
+    return null
   }
 
   const worksheet = workbook.Sheets[extractionConfiguration.sheetName]
 
   if (_.isUndefined(worksheet)) {
     logger.log(`${new Date().toUTCString()} Worksheet not found`)
-    return
+    return null
   }
 
   const titleCellValue = worksheet[extractionConfiguration.titleCellAddress]?.v || ''
@@ -54,19 +53,19 @@ const extractSingleSheetContent = (workbook, extractionConfiguration) => {
     worksheet['!ref'] = `${extractionConfiguration.startCell}:${worksheet['!ref'].split(':')[1]}`
   }
 
-  let data = {}
-  data.sheetTitle = sheetTitle
+  let data = {
+    sheetTitle
+  }
   // IF: Start sheet have vertical header & row structure, so not need to add header config option here
   // ELSE: Header config option useful for sheet whcih have multiple rows like 'D-1 Off-Site Habitat Baseline'
   if (extractionConfiguration.sheetName === 'Start') {
     data = xslx.utils.sheet_to_json(worksheet, { blankrows: false })
-    data = parseVerticalColumns(data, extractionConfiguration)
-  } else {
-    data = xslx.utils.sheet_to_json(worksheet, { skipHidden: false, blankrows: true, header: 3 })
-    data = prepareDataValues(data, extractionConfiguration)
+    data = parseVerticalColumns(data)
+    return data
   }
 
-  return data
+  data = xslx.utils.sheet_to_json(worksheet, { skipHidden: false, blankrows: true, header: 3 })
+  return prepareDataValues(data, extractionConfiguration)
 }
 
 /** ================================================================================================
@@ -74,7 +73,7 @@ const extractSingleSheetContent = (workbook, extractionConfiguration) => {
  *@param key type: string | Key of the extracted data
  *@return type: array
  *================================================================================================**/
-const getFormatedNewKey = (key) => {
+const getFormatedNewKey = key => {
   const newKey = _.isString(key) ? key.trim().replace(':', '') : undefined
   return _.camelCase(newKey)
 }
@@ -84,9 +83,7 @@ const getFormatedNewKey = (key) => {
  *@param data type: array | Extracted raw data to json from metric file.
  *@return type: array
  *================================================================================================**/
-const parseVerticalColumns = (data) => {
-  return data.reduce((obj, item) => Object.assign(obj, { [getFormatedNewKey(Object.values(item)[0])]: Object.values(item)[1] }), {})
-}
+const parseVerticalColumns = data => data.reduce((obj, item) => Object.assign(obj, { [getFormatedNewKey(Object.values(item)[0])]: Object.values(item)[1] }), {})
 
 /** ================================================================================================
  ** Returns parsed data array on the basis of given raw array.
@@ -98,37 +95,42 @@ const prepareDataValues = (data, config) => {
   const result = []
   const header = config.cellHeaders
   const requiredField = config.requiredField
-  if (!_.isEmpty(data)) {
-    const keyValues = Object.keys(data[0])
-    for (const item of data) {
-      // Check if required field is non empty to avoid unneccessary looping
-      if (_.isEmpty(item[requiredField]) || (!_.isUndefined(item[getFormatedNewKey(requiredField)]) && _.isEmpty(item[getFormatedNewKey(requiredField)]))) {
-        break
-      }
 
-      const tmpArr = keyValues.reduce((acc, el, i, arr) => {
-        // Filtering keys to remove trailing space
-        let _key = _.isString(el) ? el.trim().replace(':', '') : ''
+  if (_.isEmpty(data)) {
+    logger.log(`${new Date().toUTCString()} Data does not extracted for ${config.sheetName}`)
+    return null
+  }
 
-        // Checking if key exists into header config and EMPTY coulmn entries would be neglected
-        if (_.includes(header, el) || el.indexOf('EMPTY') === -1) {
-          // Checking for duplicates key and appending with index to differntiate
-          if (arr.indexOf(_key) !== i && acc.indexOf(_key) < 0) {
-            _key = `${_key}_${i}`
-          }
-
-          // Renamed keys in camelCase for easy to use and to avoid code smell
-          _key = _.camelCase(_key)
-          acc.push({ [_key]: item[el] })
-        }
-        return acc
-      }, [])
-      result.push(Object.assign({}, ...tmpArr))
+  const keyValues = Object.keys(data[0])
+  for (const item of data) {
+    // Check if required field is non empty to avoid unneccessary looping
+    const isUndefinedItem = _.isEmpty(item[requiredField])
+    const checkOnKeyFormatting = !_.isUndefined(item[getFormatedNewKey(requiredField)]) && _.isEmpty(item[getFormatedNewKey(requiredField)])
+    if (isUndefinedItem && checkOnKeyFormatting) {
+      break
     }
+
+    const tmpArr = keyValues.reduce((acc, el, i, arr) => {
+      // Filtering keys to remove trailing space
+      let _key = _.isString(el) ? el.trim() : ''
+      _key = _key.replace(':', '')
+
+      // Checking if key exists into header config and EMPTY coulmn entries would be neglected
+      if (_.includes(header, el) || el.indexOf('EMPTY') === -1) {
+        // Checking for duplicates key and appending with index to differntiate
+        if (arr.indexOf(_key) !== i && acc.indexOf(_key) < 0) {
+          _key = `${_key}_${i}`
+        }
+
+        // Renamed keys in camelCase for easy to use and to avoid code smell
+        _key = _.camelCase(_key)
+        acc.push({ [_key]: item[el] })
+      }
+      return acc
+    }, [])
+    result.push(Object.assign({}, ...tmpArr))
   }
   return result
 }
 
-export default (contentInputStream, config) => {
-  return readAndExtractContent(contentInputStream, config)
-}
+export default (contentInputStream, config) => readAndExtractContent(contentInputStream, config)
