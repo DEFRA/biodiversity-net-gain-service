0ed28eb666dd109e43d5f7b87b885f562175e7f6
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-02-22 17:52:52 +0000

    Update unit testing timings

--- packages/database-version-control/src/__tests__/migrator-spec.js
+++ packages/database-version-control/src/__tests__/migrator-spec.js
@@ -9,7 +9,7 @@ describe('The database version control migrator', () => {
     expect((await migrator.pending()).map(m => m.name)).toEqual([])
     await migrator.down({ to: 0 })
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
-  }, 10000)
+  }, 20000)
   it('should initialise correctly when POSTGRES_SSL_MODE is set to require', async () => {
     jest.isolateModules(async () => {
       process.env.POSTGRES_SSL_MODE = 'require'
