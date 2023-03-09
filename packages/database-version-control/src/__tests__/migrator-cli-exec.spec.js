24975769b0713a8c5e4a6d0bafc95242aab743c8
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-09 21:23:00 +0000

    Add exec options

--- packages/database-version-control/src/__tests__/migrator-cli-exec.spec.js
+++ packages/database-version-control/src/__tests__/migrator-cli-exec.spec.js
@@ -15,16 +15,17 @@ describe('The database version control migrator command line interface', () => {
     })
   })
   it('should initialise correctly and be able to run and rollback all pending migrations using the command line interface', async () => {
+    const execOptions = { maxBuffer: 5 * 1024 * 1024 }
     const migrations = fs.readdirSync(migrationsDirectoryPath)
     // Jest does not appear to generate test coverage for subprocesses by default.
     // https://github.com/facebook/jest/issues/3190 and https://github.com/facebook/jest/issues/5274
     // Test coverage for activating the command line interface is achieved using a very basic unit test
     // in migrator-cli.spec.js.
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
-    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js up')
-    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js pending')
+    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js up', execOptions)
+    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js pending', execOptions)
     expect((await migrator.pending()).map(m => m.name)).toEqual([])
-    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js down --to 0')
+    await exec('. bin/jest-database-version-control-env.sh && node packages/database-version-control/src/migrator-cli.js down --to 0', execOptions)
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
   }, 20000)
 })
