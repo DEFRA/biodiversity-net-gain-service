bb716cd07d9618a2f690185c5821a56dea815af2
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-09 17:30:02 +0000

    Create dummy data migrations

--- packages/database-version-control/src/__tests__/migrator-spec.js
+++ packages/database-version-control/src/__tests__/migrator-spec.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
 const migrator = require('../migrator.js')
 
+const dataMigrationsDirectoryPath = 'packages/database-version-control/src/data-migrations'
+
 describe('The database version control migrator', () => {
+  beforeAll(() => {
+    // Create a dummy migration file for each data migration file that is gzipped.
+    const dataMigrationFiles = fs.readdirSync(dataMigrationsDirectoryPath)
+    const migrationsDirectory = 'packages/database-version-control/src/migrations'
+    dataMigrationFiles.forEach(dataMigrationFile => {
+      fs.writeFileSync(`${migrationsDirectory}/${dataMigrationFile.substring(0, dataMigrationFile.length - 3)}`, 'mock data')
+    })
+  })
   it('should initialise correctly and be able to run and rollback all pending migrations programmatically', async () => {
-    const migrations = fs.readdirSync('packages/database-version-control/src/migrations')
+    const migrations = fs.readdirSync(dataMigrationsDirectoryPath)
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
     await migrator.up()
     expect((await migrator.pending()).map(m => m.name)).toEqual([])
