60194a315eb9b560b88a15e4bc6e9c1ae806516b
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-09 18:18:09 +0000

    Correct paths

--- packages/database-version-control/src/__tests__/migrator-cli-exec.spec.js
+++ packages/database-version-control/src/__tests__/migrator-cli-exec.spec.js
@@ -3,17 +3,19 @@ const util = require('node:util')
 const exec = util.promisify(require('node:child_process').exec)
 const migrator = require('../migrator.js')
 
+const migrationsDirectoryPath = 'packages/database-version-control/src/migrations'
+
 describe('The database version control migrator command line interface', () => {
   beforeAll(() => {
     // Create a dummy migration file for each data migration file that is gzipped.
     const dataMigrationFiles = fs.readdirSync('packages/database-version-control/src/data-migrations')
-    const migrationsDirectory = 'packages/database-version-control/src/migrations'
+    const migrationsDirectory = migrationsDirectoryPath
     dataMigrationFiles.forEach(dataMigrationFile => {
       fs.writeFileSync(`${migrationsDirectory}/${dataMigrationFile.substring(0, dataMigrationFile.length - 3)}`, 'mock data')
     })
   })
   it('should initialise correctly and be able to run and rollback all pending migrations using the command line interface', async () => {
-    const migrations = fs.readdirSync('packages/database-version-control/src/migrations')
+    const migrations = fs.readdirSync(migrationsDirectoryPath)
     // Jest does not appear to generate test coverage for subprocesses by default.
     // https://github.com/facebook/jest/issues/3190 and https://github.com/facebook/jest/issues/5274
     // Test coverage for activating the command line interface is achieved using a very basic unit test
--- packages/database-version-control/src/__tests__/migrator-spec.js
+++ packages/database-version-control/src/__tests__/migrator-spec.js
@@ -1,19 +1,19 @@
 const fs = require('fs')
 const migrator = require('../migrator.js')
 
-const dataMigrationsDirectoryPath = 'packages/database-version-control/src/data-migrations'
+const migrationsDirectoryPath = 'packages/database-version-control/src/migrations'
 
 describe('The database version control migrator', () => {
   beforeAll(() => {
     // Create a dummy migration file for each data migration file that is gzipped.
-    const dataMigrationFiles = fs.readdirSync(dataMigrationsDirectoryPath)
-    const migrationsDirectory = 'packages/database-version-control/src/migrations'
+    const dataMigrationFiles = fs.readdirSync('packages/database-version-control/src/data-migrations')
+    const migrationsDirectory = migrationsDirectoryPath
     dataMigrationFiles.forEach(dataMigrationFile => {
       fs.writeFileSync(`${migrationsDirectory}/${dataMigrationFile.substring(0, dataMigrationFile.length - 3)}`, 'mock data')
     })
   })
   it('should initialise correctly and be able to run and rollback all pending migrations programmatically', async () => {
-    const migrations = fs.readdirSync(dataMigrationsDirectoryPath)
+    const migrations = fs.readdirSync(migrationsDirectoryPath)
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
     await migrator.up()
     expect((await migrator.pending()).map(m => m.name)).toEqual([])
