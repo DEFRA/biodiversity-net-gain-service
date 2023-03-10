fce63f679253593a6cb7d19d1a253e41eae0999c
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-03-10 21:23:42 +0530

    Fixed eslint errors

--- packages/webapp/src/routes/developer/details-email.js
+++ packages/webapp/src/routes/developer/details-email.js
@@ -31,8 +31,8 @@ const checkEmailFormat = emailAddress => {
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+    )
+}
 
 const validateEmail = emailAddress => {
   const error = {}
