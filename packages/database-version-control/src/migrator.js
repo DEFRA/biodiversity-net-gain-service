48a1dc7c06ab91273c6301f670fc1bf662a0e5cf
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-13 13:38:52 +0000

    Extract data migrations during postinstall

--- packages/database-version-control/src/migrator.js
+++ packages/database-version-control/src/migrator.js
@@ -1,9 +1,3 @@
-const fs = require('fs')
-const { pipeline } = require('stream')
-const { createGunzip } = require('zlib')
-const { promisify } = require('util')
-const pipe = promisify(pipeline)
-const { createReadStream, createWriteStream } = require('fs')
 const path = require('path')
 const { SlonikMigrator } = require('@slonik/migrator')
 const { createPool, sql } = require('slonik')
@@ -40,34 +34,3 @@ const migrator = new SlonikMigrator({
 })
 
 module.exports = migrator
-
-const gunzipDataMigrations = async () => {
-  const srcDirectoryPath = path.join(__dirname, '/')
-  const dataMigrationsDirectoryPath = `${srcDirectoryPath}data-migrations/`
-  // Provide an option to disable data migrations during unit tests.
-  const migrationsDirectoryPath =
-    process.env.NODE_ENV === 'test' && process.env.DISABLE_DATA_MIGRATIONS === '1'
-      ? dataMigrationsDirectoryPath
-      : `${srcDirectoryPath}migrations/`
-
-  // Ensure that non-gzipped files in the data-migrations directory are ignored.
-  const dataMigrationFiles = fs.readdirSync(dataMigrationsDirectoryPath).filter(f => f.endsWith('.gz'))
-  const promises = []
-  dataMigrationFiles.forEach(dataMigrationFile => {
-    const inputFilePath = `${dataMigrationsDirectoryPath}${dataMigrationFile}`
-    const outputFilePath = `${migrationsDirectoryPath}${dataMigrationFile.substring(0, dataMigrationFile.length - 3)}`
-    promises.push(gunzipDataMigration(`${inputFilePath}`, `${outputFilePath}`))
-  })
-  return Promise.all(promises)
-}
-
-const gunzipDataMigration = async (inputFilePath, outputFilePath) => {
-  const gunzip = createGunzip()
-  const source = createReadStream(inputFilePath)
-  const destination = createWriteStream(outputFilePath)
-  return pipe(source, gunzip, destination)
-}
-
-(
-  async () => { await gunzipDataMigrations() }
-)()
