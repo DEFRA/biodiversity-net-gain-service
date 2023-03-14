efa0182aacd554afc007d3e4c3b7781a20b76fb4
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-14 12:13:13 +0000

    Detect crossing of English coastline

--- packages/database-version-control/src/migrations/2023.02.22T10.08.11.create-is-polygon-in-england-only-27700-function.sql
+++ packages/database-version-control/src/migrations/2023.02.22T10.08.11.create-is-polygon-in-england-only-27700-function.sql
@@ -1,35 +1,50 @@
 CREATE OR REPLACE FUNCTION bng.fn_is_polygon_in_england_only_27700(
     _boundaryAsGeoJson text)
   RETURNS boolean AS
-$BODY$
+$$
 
 BEGIN
-
-  BEGIN
+  -- Check if any part of the polygon is in Scotland or Wales.
+  IF EXISTS (
+    SELECT
+      1
+    FROM (
+      SELECT
+        nation,
+        COUNT(nation) OVER () AS row_count
+      FROM
+        bng.nation_boundary_27700
+      WHERE
+        st_intersects(st_setsrid(st_geomfromgeojson(_boundaryAsGeoJson), 27700), geometry)
+    ) n
+    WHERE
+      n.nation = 'England'
+      AND n.row_count = 1
+  )
+  THEN
+    -- Check if any part of the polygon crosses the coastline of England.
     IF EXISTS (
       SELECT
         1
-      FROM ( 
+      FROM (
         SELECT
-	        nation,
-	        COUNT(nation) OVER () AS row_count
+          nation
         FROM
-	        bng.nation_boundary_27700
+          bng.nation_boundary_27700
         WHERE
-	        st_intersects(st_setsrid(st_geomfromgeojson(_boundaryAsGeoJson), 27700), geometry)
-      ) n
-      WHERE
-        n.nation = 'England'
-        AND n.row_count = 1
+          st_covers(geometry, st_setsrid(st_geomfromgeojson(_boundaryAsGeoJson), 27700))
+          AND nation = 'England') a
     )
     THEN
       RETURN true;
     ELSE
       RETURN false;
     END IF;
-  END;
-
+  ELSE
+    RETURN false;
+  END IF;
 END;
-$BODY$
+
+$$
   LANGUAGE plpgsql VOLATILE
   COST 100;
