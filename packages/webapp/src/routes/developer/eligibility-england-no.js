4d05e31d875889083f207281d80fda4752c6be05
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-03-20 15:37:07 +0530

    Improved test coverage by modifiying upcomming pages

--- packages/webapp/src/routes/developer/eligibility-england-no.js
+++ packages/webapp/src/routes/developer/eligibility-england-no.js
@@ -4,9 +4,6 @@ import constants from '../../utils/constants.js'
 const handlers = {
   get: async (request, h) => {
     return h.view(constants.views.DEVELOPER_ELIGIBILITY_NO)
-  },
-  post: async (request, h) => {
-    return h.redirect('#')
   }
 }
 
@@ -14,8 +11,4 @@ export default [{
   method: 'GET',
   path: constants.routes.DEVELOPER_ELIGIBILITY_NO,
   handler: handlers.get
-}, {
-  method: 'POST',
-  path: constants.routes.DEVELOPER_ELIGIBILITY_NO,
-  handler: handlers.post
 }]
--- packages/webapp/src/routes/developer/eligibility-landowner-consent.js
+++ packages/webapp/src/routes/developer/eligibility-landowner-consent.js
@@ -4,9 +4,6 @@ import constants from '../../utils/constants.js'
 const handlers = {
   get: async (request, h) => {
     return h.view(constants.views.DEVELOPER_ELIGIBILITY_LO_CONSENT)
-  },
-  post: async (request, h) => {
-    return h.redirect('#')
   }
 }
 
@@ -14,8 +11,4 @@ export default [{
   method: 'GET',
   path: constants.routes.DEVELOPER_ELIGIBILITY_LO_CONSENT,
   handler: handlers.get
-}, {
-  method: 'POST',
-  path: constants.routes.DEVELOPER_ELIGIBILITY_LO_CONSENT,
-  handler: handlers.post
 }]
