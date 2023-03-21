a5366ff746555fad5865b4c17b2d795dc79fd15a
Tedd <teddmason@gmail.com>
2023-03-21 08:30:44 +0000

    Adding in core.js helper to hide/show defra-js-x classes (#248)

--- packages/webapp/client/js/core.js
+++ packages/webapp/client/js/core.js
@@ -1,4 +1,16 @@
 'use strict'
 // "bng" represents the global namespace for client side js accross the service
-
 window.bng = {}
+
+// Hide defra-js-hide elements on page load
+const nonJsElements = document.getElementsByClassName('defra-js-hide')
+Array.prototype.forEach.call(nonJsElements, function (element) {
+  element.style.display = 'none'
+})
+
+// Show defra-js-show elements on page load
+// To use this set class to defra-js-show and give hidden attribute to hide by default
+const jsElements = document.getElementsByClassName('defra-js-show')
+Array.prototype.forEach.call(jsElements, function (element) {
+  element.removeAttribute('hidden')
+})
