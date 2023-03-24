087032695f9459514d10d5dfb5d44bc1275fc1d2
z424brave <kelly.damian@btinternet.com>
2023-03-24 11:49:03 +0000

    add test to increase coverage

--- packages/webapp/src/routes/__tests__/cookies.js
+++ packages/webapp/src/routes/__tests__/cookies.js
@@ -9,4 +9,18 @@ describe(url, () => {
       await submitGetRequest({ url })
     })
   })
+  describe('GET', () => {
+    it(`should render the ${url.substring(1)} view with link back to referring page`, async () => {
+      const referer = '/start'
+      const options = {
+        method: 'GET',
+        headers: {
+          referer
+        },
+        url
+      }
+      const res = await submitGetRequest(options)
+      expect(res.payload).toContain(referer)
+    })
+  })
 })
