0c5ff414ca2d23f6c2a01b74ec4a58c3e43ebb3d
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-03-30 08:48:07 +0100

    Load NTv2 format files at runtime

--- packages/geoprocessing-service/bin/copy-ntv2-format-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp src/ntv2-format-files/OSTN15_NTv2_ETRStoOSGB.gsb node_modules/gdal-async/deps/libproj/proj/data/OSTN15_NTv2_ETRStoOSGB.gsb
-cp src/ntv2-format-files/OSTN15_NTv2_OSGBtoETRS.gsb node_modules/gdal-async/deps/libproj/proj/data/OSTN15_NTv2_OSGBtoETRS.gsb
