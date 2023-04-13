383bcb405e70cceda00e3cba3c323ae9127b55c5
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-04-13 15:48:17 +0100

    fix/BNGP-2572-geospatial-upload-regression (#267)
    * fix/BNGP-2572-geospatial-upload-regression
    
    https://eaflood.atlassian.net/browse/BNGP-2572
    
    Copy NTv2 format files at build time due to read only file systems in the official cloud environments.
    Use a postinstall script that does not assume a particular directory structure when copying.
    
    * Copy NTv2 files in a portable way at build time
    
    * v0.75.1
    
    * Remove basic debug logging

--- packages/geoprocessing-service/src/helpers/__tests__/copy-ntv2-format-files.spec.js
+++ packages/geoprocessing-service/src/helpers/__tests__/copy-ntv2-format-files.spec.js
@@ -1,8 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import copyNtv2FormatFiles from '../copy-ntv2-format-files.cjs'
-
-console.log(process.cwd() + '**')
 const gdalDataDirectory = path.join('packages', 'geoprocessing-service', 'node_modules', 'gdal-async', 'deps', 'libproj', 'proj', 'data')
 
 describe('NTv2 format files', () => {
@@ -10,9 +7,16 @@ describe('NTv2 format files', () => {
     fs.unlinkSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_ETRStoOSGB.gsb'))
     fs.unlinkSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_OSGBtoETRS.gsb'))
   })
-  it('should be copied to the GDAL data directory', () => {
-    copyNtv2FormatFiles()
-    expect(fs.existsSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_ETRStoOSGB.gsb'))).toBeTruthy()
-    expect(fs.existsSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_OSGBtoETRS.gsb'))).toBeTruthy()
+  it('should be copied to the GDAL data directory', done => {
+    jest.isolateModules(async () => {
+      try {
+        require('../copy-ntv2-format-files.cjs')
+        expect(fs.existsSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_ETRStoOSGB.gsb'))).toBeTruthy()
+        expect(fs.existsSync(path.join(gdalDataDirectory, 'OSTN15_NTv2_OSGBtoETRS.gsb'))).toBeTruthy()
+        done()
+      } catch (err) {
+        done(err)
+      }
+    })
   })
 })
