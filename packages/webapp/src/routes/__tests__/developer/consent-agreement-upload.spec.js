308461574f1ad461fb563e78794318c02ad90d14
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-04-24 13:40:25 +0530

    Improved test coverage for upload written consent

--- packages/webapp/src/routes/__tests__/developer/consent-agreement-upload.spec.js
+++ packages/webapp/src/routes/__tests__/developer/consent-agreement-upload.spec.js
@@ -44,9 +44,9 @@ describe(url, () => {
           const config = Object.assign({}, baseConfig)
           config.filePath = `${mockDataPath}/sample.docx`
           config.hasError = false
-          // config.headers = {
-          //   referer: 'http://localhost:3000/developer/consent-agreement-check'
-          // }
+          config.headers = {
+            referer: 'http://localhost:3000/developer/consent-agreement-check'
+          }
           const response = await uploadFile(config)
           expect(response.statusCode).toEqual(302)
           setImmediate(() => {
@@ -91,25 +91,6 @@ describe(url, () => {
         }
       })
     })
-
-    // it('should display expected error details when an empty file is uploaded', (done) => {
-    //   jest.isolateModules(async () => {
-    //     try {
-    //       const config = Object.assign({}, baseConfig)
-    //       config.filePath = `${mockDataPath}/empty-file.doc`
-    //       config.generateEmptyFileUploadError = true
-    //       // config.hasError = true
-    //       const response = await uploadFile(config)
-    //       expect(response.payload).toContain('There is a problem')
-    //       expect(response.payload).toContain('The selected file is empty')
-    //       setImmediate(() => {
-    //         done()
-    //       })
-    //     } catch (err) {
-    //       done(err)
-    //     }
-    //   })
-    // })
 
     it('should display expected error details when upload screening detects a threat', (done) => {
       jest.isolateModules(async () => {
@@ -175,6 +156,60 @@ describe(url, () => {
           config.filePath = `${mockDataPath}/sample.docx`
           config.generateUnexpectedValidationError = true
           await uploadFile(config)
+          setImmediate(() => {
+            done()
+          })
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+
+    it('should not upload a consent file more than 50 MB', (done) => {
+      jest.isolateModules(async () => {
+        try {
+          const uploadConfig = Object.assign({}, baseConfig)
+          uploadConfig.hasError = true
+          uploadConfig.filePath = `${mockDataPath}/big-file.doc`
+          const response = await uploadFile(uploadConfig)
+          expect(response.payload).toContain('There is a problem')
+          expect(response.payload).toContain('The selected file must not be larger than 50MB')
+          setImmediate(() => {
+            done()
+          })
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+
+    it('should display an error message when non-file data is uploaded', (done) => {
+      jest.isolateModules(async () => {
+        try {
+          const config = Object.assign({}, baseConfig)
+          config.hasError = true
+          const response = await uploadFile(config)
+          expect(response.payload).toContain('There is a problem')
+          expect(response.payload).toContain('Select a written consent')
+          setImmediate(() => {
+            done()
+          })
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+
+    it('should display an error when an empty file is uploaded', (done) => {
+      jest.isolateModules(async () => {
+        try {
+          const config = Object.assign({}, baseConfig)
+          config.filePath = `${mockDataPath}/empty-file.doc`
+          config.generateInvalidFeatureCountError = true
+          config.hasError = true
+          const response = await uploadFile(config)
+          expect(response.payload).toContain('There is a problem')
+          expect(response.payload).toContain('The selected file is empty')
           setImmediate(() => {
             done()
           })
