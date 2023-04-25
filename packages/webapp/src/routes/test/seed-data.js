1e1cf461a3d349e46345b2c72dbe98f82da6ac2c
Tedd Mason <teddmason@gmail.com>
2023-04-25 15:06:30 +0100

    switching data to a string

--- packages/webapp/src/routes/test/seed-data.js
+++ packages/webapp/src/routes/test/seed-data.js
@@ -1,11 +1,11 @@
 import constants from '../../utils/constants.js'
-import applicant from '../../__mock-data__/test-application.json.js'
+import applicant from '../../__mock-data__/test-application.js'
 
 const seedData = {
   method: 'GET',
   path: constants.routes.TEST_SEED_DATA,
   handler: async (request, h) => {
-    request.yar._store = applicant.data
+    request.yar._store = JSON.parse(applicant.dataString)
     return h.view(constants.views.TEST_SEED_DATA)
   }
 }
