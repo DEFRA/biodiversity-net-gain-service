de858354d331e272dc8362d71efb447bc6fa37d0
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-05-04 11:50:03 +0100

    test case added for process developer upload concent

--- packages/azure-functions/ProcessTrustedFile/helpers/process-developer-upload-consent.js
+++ packages/azure-functions/ProcessTrustedFile/helpers/process-developer-upload-consent.js
@@ -1,15 +1,8 @@
 import buildSignalRMessage from '../../Shared/build-signalr-message.js'
 
 export default async function (context, config) {
-  let signalRMessageArguments
-  try {
-    signalRMessageArguments = [{
-      location: config.fileConfig.fileLocation
-    }]
-  } catch (err) {
-    context.log.error(err)
-    signalRMessageArguments = [{ errorCode: err.code }]
-  } finally {
-    context.bindings.signalRMessages = [buildSignalRMessage(config.signalRMessageConfig, signalRMessageArguments)]
-  }
+  const signalRMessageArguments = [{
+    location: config.fileConfig.fileLocation
+  }]
+  context.bindings.signalRMessages = [buildSignalRMessage(config.signalRMessageConfig, signalRMessageArguments)]
 }
