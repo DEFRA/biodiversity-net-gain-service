5b1112db758355e9bc6ccfefbaba99c7f469a67f
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-05-26 14:17:32 +0100

    partial commit for Application summary

--- packages/webapp/src/routes/developer/check-answers.js
+++ packages/webapp/src/routes/developer/check-answers.js
@@ -1,7 +1,31 @@
 import constants from '../../utils/constants.js'
+import developerApplication from '../../utils/developerApplication.js'
+import {
+  listArray,
+  boolToYesNo,
+  dateToString,
+  hideClass
+} from '../../utils/helpers.js'
+import { logger } from 'defra-logging-facade'
 
 const handlers = {
-  get: async (_request, h) => h.view(constants.views.DEVELOPER_CHECK_ANSWERS)
+  get: async (request, h) => {
+    logger.info('Developer JSON payload for powerApp', developerApplication(request.yar).developerAllocation)
+    return h.view(constants.views.DEVELOPER_CHECK_ANSWERS, {
+      developerApplication: developerApplication(request.yar).developerAllocation,
+      ...getContext(request)
+    })
+  }
+}
+
+const getContext = request => {
+  return {
+    listArray,
+    boolToYesNo,
+    dateToString,
+    hideClass,
+    routes: constants.routes
+  }
 }
 
 export default [{
