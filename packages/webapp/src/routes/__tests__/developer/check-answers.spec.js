e0854588ec5876293b7f1bb8b0edef845f94f804
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-05-30 14:09:30 +0530

    WIP: Added additional email address section

--- packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
@@ -157,12 +157,20 @@ describe(url, () => {
 
   describe('POST', () => {
     jest.mock('@defra/bng-connectors-lib')
+    let redisMap
+    beforeEach(() => {
+      redisMap = new Map()
+    })
 
     it('should throw error if developer object validation failed', (done) => {
       jest.isolateModules(async () => {
         try {
           let viewResult
           const checkAnswerFile = require('../../developer/check-answers.js')
+          redisMap.set(constants.redisKeys.DEVELOPER_FULL_NAME, 'Test User')
+          redisMap.set(constants.redisKeys.DEVELOPER_EMAIL_VALUE, 'test@example.com')
+          redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockDevelopmentDetails)
+          redisMap.set(constants.redisKeys.BIODIVERSITY_NET_GAIN_NUMBER, '123')
           const request = {
             yar: redisMap,
             payload: {}
