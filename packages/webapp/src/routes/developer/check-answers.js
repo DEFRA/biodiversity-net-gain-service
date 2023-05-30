677bbd318c07743a7a2499df1cf10db6ffd080e9
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-05-30 14:09:30 +0530

    WIP: Added additional email address section

--- packages/webapp/src/routes/developer/check-answers.js
+++ packages/webapp/src/routes/developer/check-answers.js
@@ -11,9 +11,13 @@ import { logger } from 'defra-logging-facade'
 
 const handlers = {
   get: async (request, h) => {
-    logger.info('GET Developer JSON payload for powerApp', developerApplication(request.yar))
+    const applicationData = developerApplication(request.yar)
+    const additionalEmailAddresses = getAdditionalEmailAddressArray(applicationData.developerAllocation.additionalEmailAddresses)
+    logger.info('GET Developer JSON payload for powerApp', applicationData)
     return h.view(constants.views.DEVELOPER_CHECK_ANSWERS, {
-      developerApplication: developerApplication(request.yar),
+      developmentDetails: applicationData.developerAllocation.developmentDetails,
+      files: applicationData.files,
+      additionalEmailAddresses,
       ...getContext(request)
     })
   },
@@ -27,7 +31,26 @@ const handlers = {
   }
 }
 
-const getContext = request => {
+const getAdditionalEmailAddressArray = additionalEmailAddresses =>
+  additionalEmailAddresses && additionalEmailAddresses.map(item => ({
+    key: {
+      text: 'Email'
+    },
+    value: {
+      html: `<span>${item.email}</span>`
+    },
+    actions: {
+      items: [
+        {
+          href: constants.routes.DEVELOPER_EMAIL_ENTRY,
+          text: 'Change',
+          visuallyHiddenText: ' Addtional email addresses'
+        }
+      ]
+    }
+  }))
+
+const getContext = _request => {
   return {
     listArray,
     boolToYesNo,
@@ -41,4 +64,9 @@ export default [{
   method: 'GET',
   path: constants.routes.DEVELOPER_CHECK_ANSWERS,
   handler: handlers.get
+},
+{
+  method: 'POST',
+  path: constants.routes.DEVELOPER_CHECK_ANSWERS,
+  handler: handlers.post
 }]
