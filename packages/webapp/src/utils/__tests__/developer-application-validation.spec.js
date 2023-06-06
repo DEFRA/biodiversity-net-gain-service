dba64fc61c1b73beea496e32dc895479aecf708c
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-06-06 22:49:12 +0530

    WIP: Added test cases to improve its coverage and modified helper method

--- packages/webapp/src/utils/__tests__/developer-application-validation.spec.js
+++ packages/webapp/src/utils/__tests__/developer-application-validation.spec.js
@@ -1,18 +1,18 @@
 import developerApplicationValidation from '../developer-application-validation.js'
 import developerApplication from '../developer-application.js'
 import constants from '../../utils/constants.js'
-import developerApplicationSession from '../../__mocks__/developer-application-session.js'
+import setDeveloperApplicationSession from '../../__mocks__/developer-application-session.js'
 
 describe('developer-application-validation', () => {
   describe('validate', () => {
     it('Should pass validation for normal test', () => {
-      const session = developerApplicationSession()
+      const session = setDeveloperApplicationSession()
       const { value, error } = developerApplicationValidation.validate(developerApplication(session))
       expect(error).toBeUndefined()
       expect(value).not.toBeUndefined()
     })
     it('Should fail validation if a .required() field is missing', () => {
-      const session = developerApplicationSession()
+      const session = setDeveloperApplicationSession()
       session.set(constants.redisKeys.DEVELOPER_FULL_NAME, undefined)
       const { value, error } = developerApplicationValidation.validate(developerApplication(session))
       expect(error.message).toEqual('"developerAllocation.applicant.name" is required')
