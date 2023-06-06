ce87c107d51d955d37c9b452ba755a9a53ec39d0
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-06-06 22:49:12 +0530

    WIP: Added test cases to improve its coverage and modified helper method

--- packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
@@ -1,4 +1,4 @@
-import developerApplicationSession from '../../../__mocks__/developer-application-session.js'
+import setDeveloperApplicationSession from '../../../__mocks__/developer-application-session.js'
 import checkAnswers from '../../../routes/developer/check-answers.js'
 import constants from '../../../utils/constants.js'
 import { submitGetRequest } from '../helpers/server.js'
@@ -40,7 +40,7 @@ describe(url, () => {
     it('Should process a valid application correctly', done => {
       jest.isolateModules(async () => {
         try {
-          const session = developerApplicationSession()
+          const session = setDeveloperApplicationSession()
           const postHandler = checkAnswers[1].handler
 
           const http = require('../../../utils/http.js')
@@ -73,7 +73,7 @@ describe(url, () => {
     it('Should fail if backend errors', done => {
       jest.isolateModules(async () => {
         try {
-          const session = developerApplicationSession()
+          const session = setDeveloperApplicationSession()
           const postHandler = checkAnswers[1].handler
 
           const http = require('../../../utils/http.js')
@@ -91,7 +91,7 @@ describe(url, () => {
     it('Should throw an error page if validation fails for application', done => {
       jest.isolateModules(async () => {
         try {
-          const session = developerApplicationSession()
+          const session = setDeveloperApplicationSession()
           const postHandler = checkAnswers[1].handler
           session.set(constants.redisKeys.DEVELOPER_FULL_NAME, undefined)
 
@@ -119,7 +119,7 @@ describe(url, () => {
       jest.isolateModules(async () => {
         try {
           const postHandler = checkAnswers[1].handler
-          const session = developerApplicationSession()
+          const session = setDeveloperApplicationSession()
           session.set(constants.redisKeys.DEVELOPER_CONSENT_ANSWER, undefined)
 
           const http = require('../../../utils/http.js')
@@ -142,7 +142,7 @@ describe(url, () => {
 
           await postHandler({ yar: session }, h)
           expect(viewArgs).toEqual('')
-          expect(redirectArgs[0]).toEqual('/application-submitted')
+          expect(redirectArgs[0]).toEqual('/developer/confirm')
           done()
         } catch (err) {
           done(err)
@@ -151,3 +151,72 @@ describe(url, () => {
     })
   })
 })
+
+// const mockDevelopmentDetails = {
+//   startPage: {
+//     projectName: 'Test Project',
+//     planningAuthority: 'Test Authority',
+//     planningApplicationReference: 'Test Application Reference'
+//   }
+// }
+
+// describe(url, () => {
+//   describe('GET', () => {
+//     let viewResult, contextResult
+//     const redisMap = new Map()
+//     it(`should render the ${url.substring(1)} view`, async () => {
+//       const checkAnswerFile = require('../../developer/check-answers.js')
+
+//       redisMap.set(constants.redisKeys.DEVELOPER_FULL_NAME, 'Test User')
+//       redisMap.set(constants.redisKeys.DEVELOPER_EMAIL_VALUE, 'test@example.com')
+//       redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockDevelopmentDetails)
+//       redisMap.set(constants.redisKeys.BIODIVERSITY_NET_GAIN_NUMBER, '123')
+//       const request = {
+//         yar: redisMap
+//       }
+//       const h = {
+//         view: (view, context) => {
+//           viewResult = view
+//           contextResult = context
+//         }
+//       }
+//       await checkAnswerFile.default[0].handler(request, h)
+//       expect(viewResult).toEqual(constants.views.DEVELOPER_CHECK_ANSWERS)
+//       expect(contextResult.developmentDetails).toEqual({
+//         projectName: mockDevelopmentDetails.startPage.projectName,
+//         localAuthority: mockDevelopmentDetails.startPage.planningAuthority,
+//         planningReference: mockDevelopmentDetails.startPage.planningApplicationReference
+//       })
+//     })
+//   })
+
+//   describe('POST', () => {
+//     jest.mock('@defra/bng-connectors-lib')
+//     let redisMap
+//     beforeEach(() => {
+//       redisMap = new Map()
+//     })
+
+//     it('should throw error if developer object validation failed', (done) => {
+//       jest.isolateModules(async () => {
+//         try {
+//           let viewResult
+//           const checkAnswerFile = require('../../developer/check-answers.js')
+//           redisMap.set(constants.redisKeys.DEVELOPER_FULL_NAME, 'Test User')
+//           redisMap.set(constants.redisKeys.DEVELOPER_EMAIL_VALUE, 'test@example.com')
+//           redisMap.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockDevelopmentDetails)
+//           redisMap.set(constants.redisKeys.BIODIVERSITY_NET_GAIN_NUMBER, '123')
+//           const request = {
+//             yar: redisMap,
+//             payload: {}
+//           }
+//           const h = {
+//             redirect: (view) => {
+//               viewResult = view
+//             },
+//             view: (view) => {
+//               viewResult = view
+//             }
+//           }
+//           await checkAnswerFile.default[1].handler(request, h)
+//           expect(viewResult).toEqual(constants.routes.DEVELOPER_ROUTING_REGISTER)
