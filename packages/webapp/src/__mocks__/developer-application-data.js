016a460dfa6461f7160dfefff87e05cf90afde5d
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-06-06 22:49:12 +0530

    WIP: Added test cases to improve its coverage and modified helper method

--- packages/webapp/src/__mocks__/developer-application-data.js
+++ packages/webapp/src/__mocks__/developer-application-data.js
@@ -1,3 +1,110 @@
-const developerApplicationMock = JSON.parse('{"developerAllocation":{"applicant":{"name":"Ajinkya Jawalkar","emailAddress":"ajinkya.jawalkar@capgemini.com","role":"Developer"},"developmentDetails":{"projectName":"BNG-TEST","localAuthority":"BNG-DEFRA","planningReference":"TEST12345678"},"additionalEmailAddresses":[{"fullName":"Ajinkya1 Jawalkar","email":"ajinkya1.jawalkar@capgemini.com"}],"biodiversityGainSiteNumber":"AZ12208461","confirmDevelopmentDetails":"yes","confirmOffsiteGainDetails":"yes","metricData":{"startPage":{"planningAuthority":"BNG-DEFRA","projectName":"BNG-TEST","applicant":"Ajinkya Jawalkar","applicationType":"Test Application","planningApplicationReference":"TEST12345678","assessor":"Test Assessor","assessmentDate":45058,"reviewer":"BNG","metricVersion":4,"planningAuthorityReviewer":"BNG-DEFRA-AUTH","requiredNetGain":0.1,"irreplaceableHabitatPresentOnSiteAtBaseline":"Yes","totalSiteAreaIncludingIrreplacableHabitatArea":5,"⚠":"Warning ","▲":"Error encountered please resolve before submitting","onSiteBaselineMapReferenceNumber":"Off-site post-intervention reference number","cells":{}},"d1OffSiteHabitatBaseline":[{"Broad habitat":"Heathland and shrub","Habitat type":"Gorse scrub","Total habitat units":"Check Data ⚠","Register reference number":"AZ12208460"},{"Broad habitat":"Sparsely vegetated land","Habitat type":"Tall forbs","Area (hectares)":3,"Total habitat units":"Check Data ⚠","Register reference number":"AZ12208461"},{"Broad habitat":"Woodland and forest","Habitat type":"Other coniferous woodland","Area (hectares)":1,"Total habitat units":"Check Data ⚠","Register reference number":"AZ12208460"},{"Broad habitat":"Lakes","Habitat type":"Marl lakes","Area (hectares)":1,"Total habitat units":"Check Data ⚠","Register reference number":"AZ12208462"},{"Area (hectares)":5,"Total habitat units":0}],"d2OffSiteHabitatCreation":[{"Proposed habitat":"Total habitat area","Area (hectares)":0,"Habitat units delivered":0}],"d3OffSiteHabitatEnhancement":[{"Area (hectares)":0,"Habitat units delivered":0}],"e1OffSiteHedgeBaseline":[{"Hedgerow type":"Species-rich native hedgerow - associated with bank or ditch","Length (km)":1,"Total hedgerow units":15.524999999999999,"Register reference number":"AZ12208460","Condition":"Good"},{"Hedgerow type":"Native hedgerow with trees - associated with bank or ditch","Length (km)":1,"Total hedgerow units":14.850000000000001,"Register reference number":"AZ12208460","Condition":"Good"},{"Hedgerow type":"Native hedgerow with trees - associated with bank or ditch","Length (km)":2,"Total hedgerow units":29.700000000000003,"Register reference number":"AZ12208461","Condition":"Good"},{"Length (km)":4,"Total hedgerow units":60.075}],"e2OffSiteHedgeCreation":[{"Length (km)":0,"Hedge units delivered":0}],"e3OffSiteHedgeEnhancement":[{"Length (km)":0,"Hedge units delivered":0}],"f1OffSiteWaterCBaseline":[{"Length (km)":0}],"f2OffSiteWaterCCreation":[{"Watercourse units delivered":0,"Length (km)":0}],"f3OffSiteWaterCEnhancement":[{"Length (km)":0,"Watercourse units delivered":0}]},"referenceNumber":"","submittedOn":"2023-06-05T09:14:15.482Z","files":[{"contentMediaType":"application/vnd.ms-excel.sheet.macroenabled.12","fileType":"developer-upload-metric","fileSize":5131037,"fileLocation":"978f462f-e91a-4875-9678-3970157204b1/developer-upload-metric/Sample Metric File.xlsm","fileName":"Sample Metric File.xlsm"},{"contentMediaType":"application/pdf","fileType":"developer-upload-consent","fileSize":13264,"fileLocation":"978f462f-e91a-4875-9678-3970157204b1/developer-upload-consent/5May1204.pdf","fileName":"5May1204.pdf"}]}}')
-
-export default developerApplicationMock
+export default {
+  developerAllocation: {
+    applicant: {
+      name: 'Test User',
+      emailAddress: 'test@example.com',
+      role: 'Developer'
+    },
+    developmentDetails: {
+      projectName: 'BNG-TEST',
+      localAuthority: 'BNG-DEFRA',
+      planningReference: 'TEST12345678'
+    },
+    additionalEmailAddresses: [
+      {
+        fullName: 'Test User',
+        email: 'test1@example.com'
+      }
+    ],
+    biodiversityGainSiteNumber: 'AZ12208461',
+    confirmDevelopmentDetails: 'yes',
+    confirmOffsiteGainDetails: 'yes',
+    metricFileChecked: 'yes',
+    metricData: {
+      startPage: {
+        planningAuthority: 'Dummy Authority',
+        projectName: 'Dummy Project',
+        applicant: 'Test User',
+        planningApplicationReference: 'TEST12345678',
+        planningAuthorityReviewer: 'Dummy Auth Reviewer'
+      },
+      d1OffSiteHabitatBaseline: [
+        {
+          Broadhabitat: 'Heathland and shrub',
+          Habitattype: 'Gorse scrub',
+          'Total habitatunits': 'Check Data ⚠',
+          'Register referencenumber': 'AZ12208460'
+        }
+      ],
+      d2OffSiteHabitatCreation: [
+        {
+          Proposedhabitat: 'Total habitat area',
+          'Area (hectare)': 0,
+          'Habitat unitsdelivered': 0
+        }
+      ],
+      d3OffSiteHabitatEnhancement: [
+        {
+          'Area (hectare)': 0,
+          'Habitat unitsdelivered': 0
+        }
+      ],
+      e1OffSiteHedgeBaseline: [
+        {
+          Hedgerowtype: 'Species-rich native hedgerow - associated with bank or ditch',
+          'Length (km)': 1,
+          'Total hedgerowunits': 15.524999999999999,
+          'Register referencenumber': 'AZ12208460',
+          Condition: 'Good'
+        }
+      ],
+      e2OffSiteHedgeCreation: [
+        {
+          'Length (km)': 0,
+          'Hedge unitsdelivered': 0
+        }
+      ],
+      e3OffSiteHedgeEnhancement: [
+        {
+          'Length (km)': 0,
+          'Hedge unitsdelivered': 0
+        }
+      ],
+      f1OffSiteWaterCBaseline: [
+        {
+          'Length (km)': 0
+        }
+      ],
+      f2OffSiteWaterCCreation: [
+        {
+          'Watercourse unitsdelivered': 0,
+          'Length (km)': 0
+        }
+      ],
+      f3OffSiteWaterCEnhancement: [
+        {
+          'Length (km)': 0,
+          'Watercourse unitsdelivered': 0
+        }
+      ]
+    },
+    referenceNumber: 'TEST-1234',
+    submittedOn: '2023-06-05T09:14:15.482Z',
+    files: [
+      {
+        contentMediaType: 'application/vnd.ms-excel.sheet.macroenabled.12',
+        fileType: 'developer-upload-metric',
+        fileSize: 5131037,
+        fileLocation: 'mock/developer-upload-metric/Sample Metric File.xlsm',
+        fileName: 'Sample Metric File.xlsm'
+      },
+      {
+        contentMediaType: 'application/pdf',
+        fileType: 'developer-upload-consent',
+        fileSize: 13264,
+        fileLocation: 'mock/developer-upload-consent/5May1204.pdf',
+        fileName: '5May1204.pdf'
+      }
+    ]
+  }
+}
--- packages/webapp/src/__mocks__/developer-application-session.js
+++ packages/webapp/src/__mocks__/developer-application-session.js
@@ -1,12 +1,27 @@
 import Session from './session.js'
 import developerApplicationMock from './developer-application-data.js'
+import constants from '../utils/constants.js'
 
-const developerApplicationSession = () => {
+const setDeveloperApplicationSession = () => {
   const session = new Session()
-  Object.keys(developerApplicationMock.developerAllocation).forEach((item) => {
-    session.set(item, developerApplicationMock.developerAllocation[item])
-  })
+  const mockData = developerApplicationMock.developerAllocation
+  session.set(constants.redisKeys.DEVELOPER_FULL_NAME, mockData.applicant.name)
+  session.set(constants.redisKeys.DEVELOPER_EMAIL_VALUE, mockData.applicant.emailAddress)
+  session.set(constants.redisKeys.DEVELOPER_METRIC_DATA, mockData.metricData)
+  session.set(constants.redisKeys.BIODIVERSITY_NET_GAIN_NUMBER, mockData.biodiversityGainSiteNumber)
+  session.set(constants.redisKeys.METRIC_FILE_CHECKED, mockData.metricFileChecked)
+  session.set(constants.redisKeys.CONFIRM_OFFSITE_GAIN_CHECKED, mockData.confirmOffsiteGainDetails)
+  session.set(constants.redisKeys.DEVELOPER_METRIC_FILE_TYPE, mockData.files[0].fileType)
+  session.set(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE, mockData.files[0].fileSize)
+  session.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, mockData.files[0].fileLocation)
+  session.set(constants.redisKeys.DEVELOPER_METRIC_FILE_NAME, mockData.files[0].fileName)
+  session.set(constants.redisKeys.DEVELOPER_CONSENT_FILE_TYPE, mockData.files[1].fileType)
+  session.set(constants.redisKeys.DEVELOPER_CONSENT_FILE_SIZE, mockData.files[1].fileSize)
+  session.set(constants.redisKeys.DEVELOPER_CONSENT_FILE_LOCATION, mockData.files[1].fileLocation)
+  session.set(constants.redisKeys.DEVELOPER_CONSENT_FILE_NAME, mockData.files[1].fileName)
+  session.set(constants.redisKeys.DEVELOPER_ADDITIONAL_EMAILS, mockData.additionalEmailAddresses)
+  session.set(constants.redisKeys.APPLICATION_REFERENCE, mockData.referenceNumber)
   return session
 }
 
-export default developerApplicationSession
+export default setDeveloperApplicationSession
