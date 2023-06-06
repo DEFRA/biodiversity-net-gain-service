8d021e9c50b0a27f90a89ca95fac2501572b9516
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-06-06 12:51:52 +0100

    developer check-answers partial commit

--- packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
@@ -1,3 +1,5 @@
+import developerApplicationSession from '../../../__mocks__/developer-application-session.js'
+import checkAnswers from '../../../routes/developer/check-answers.js'
 import constants from '../../../utils/constants.js'
 import { submitGetRequest } from '../helpers/server.js'
 import developerApplicationData from '../../../__mocks__/developer-application-data.js'
@@ -30,35 +32,118 @@ describe(url, () => {
   })
 
   describe('GET', () => {
-    let viewResult, contextResult
-    const redisMap = new Map()
     it(`should render the ${url.substring(1)} view`, async () => {
       await submitGetRequest({ url }, 200, developerApplicationData)
     })
   })
-
   describe('POST', () => {
-    jest.mock('@defra/bng-connectors-lib')
-
-    it('should throw error if developer object validation failed', (done) => {
+    it('Should process a valid application correctly', done => {
       jest.isolateModules(async () => {
         try {
-          let viewResult
-          const checkAnswerFile = require('../../developer/check-answers.js')
-          const request = {
-            yar: redisMap,
-            payload: {}
-          }
+          const session = developerApplicationSession()
+          const postHandler = checkAnswers[1].handler
+
+          const http = require('../../../utils/http.js')
+          http.postJson = jest.fn().mockImplementation(() => {
+            return {
+              applicationReference: 'test-reference'
+            }
+          })
+
+          let viewArgs = ''
+          let redirectArgs = ''
           const h = {
-            redirect: (view) => {
-              viewResult = view
+            view: (...args) => {
+              viewArgs = args
             },
-            view: (view) => {
-              viewResult = view
+            redirect: (...args) => {
+              redirectArgs = args
             }
           }
-          await checkAnswerFile.default[1].handler(request, h)
-          expect(viewResult).toEqual(constants.routes.DEVELOPER_ROUTING_REGISTER)
+
+          await postHandler({ yar: session }, h)
+          expect(viewArgs).toEqual('')
+          expect(redirectArgs).toEqual([constants.routes.DEVELOPER_APPLICATION_SUBMITTED])
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('Should fail if backend errors', done => {
+      jest.isolateModules(async () => {
+        try {
+          const session = developerApplicationSession()
+          const postHandler = checkAnswers[1].handler
+
+          const http = require('../../../utils/http.js')
+          http.postJson = jest.fn().mockImplementation(() => {
+            throw new Error('test error')
+          })
+
+          await expect(postHandler({ yar: session })).rejects.toThrow('test error')
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('Should throw an error page if validation fails for application', done => {
+      jest.isolateModules(async () => {
+        try {
+          const session = developerApplicationSession()
+          const postHandler = checkAnswers[1].handler
+          session.set(constants.redisKeys.DEVELOPER_FULL_NAME, undefined)
+
+          let viewArgs = ''
+          let redirectArgs = ''
+          const h = {
+            view: (...args) => {
+              viewArgs = args
+            },
+            redirect: (...args) => {
+              redirectArgs = args
+            }
+          }
+
+          await expect(postHandler({ yar: session }, h)).rejects.toThrow('ValidationError: "developerAllocation.applicant.name" is required')
+          expect(viewArgs).toEqual('')
+          expect(redirectArgs).toEqual('')
+          done()
+        } catch (err) {
+          done(err)
+        }
+      })
+    })
+    it('pre bug fix test, should not fail if applicant consent has been taken', done => {
+      jest.isolateModules(async () => {
+        try {
+          const postHandler = checkAnswers[1].handler
+          const session = developerApplicationSession()
+          session.set(constants.redisKeys.DEVELOPER_CONSENT_ANSWER, undefined)
+
+          const http = require('../../../utils/http.js')
+          http.postJson = jest.fn().mockImplementation(() => {
+            return {
+              applicationReference: 'test-reference'
+            }
+          })
+
+          let viewArgs = ''
+          let redirectArgs = ''
+          const h = {
+            view: (...args) => {
+              viewArgs = args
+            },
+            redirect: (...args) => {
+              redirectArgs = args
+            }
+          }
+
+          await postHandler({ yar: session }, h)
+          expect(viewArgs).toEqual('')
+          expect(redirectArgs[0]).toEqual('/application-submitted')
+          done()
         } catch (err) {
           done(err)
         }
--- packages/webapp/src/routes/developer/check-answers.js
+++ packages/webapp/src/routes/developer/check-answers.js
@@ -1,11 +1,13 @@
 import constants from '../../utils/constants.js'
 import developerApplication from '../../utils/developer-application.js'
-import developerApplicationValidation from '../../utils/developer-application-validation.js'
 import {
   initialCapitalization,
   dateToString,
   hideClass
 } from '../../utils/helpers.js'
+import { logger } from 'defra-logging-facade'
+import developerApplicationValidation from '../../utils/developer-application-validation.js'
+import { postJson } from '../../utils/http.js'
 
 const handlers = {
   get: async (request, h) => {
@@ -19,8 +21,8 @@ const handlers = {
       throw new Error(error)
     }
     const result = await postJson(`${constants.AZURE_FUNCTION_APP_URL}/processdeveloperapplication`, value)
-    request.yar.set(constants.redisKeys.APPLICATION_REFERENCE, result.referenceNumber)
-    return h.redirect(constants.routes.DEVELOPER_CONFIRM)
+    request.yar.set(constants.redisKeys.APPLICATION_REFERENCE, result.gainSiteReference)
+    return h.redirect(constants.routes.DEVELOPER_APPLICATION_SUBMITTED)
   }
 }
 
@@ -43,8 +45,8 @@ const getAdditionalEmailAddressArray = additionalEmailAddresses =>
     }
   }))
 
-const getContext = request => {
-  const applicationData = developerApplication(request.yar)
+const getContext = _request => {
+  const applicationData = developerApplication(_request.yar)
   const additionalEmailAddresses = getAdditionalEmailAddressArray(applicationData.developerAllocation.additionalEmailAddresses)
 
   const developmentDetails = applicationData.developerAllocation.developmentDetails
@@ -52,7 +54,11 @@ const getContext = request => {
   const biodiversityGainSiteNumber = applicationData.developerAllocation.biodiversityGainSiteNumber
   const confirmDevelopmentDetails = applicationData.developerAllocation.confirmDevelopmentDetails
   const confirmOffsiteGainDetails = applicationData.developerAllocation.confirmOffsiteGainDetails
+  logger.info('GET Developer JSON payload for powerApp', JSON.stringify(applicationData))
   return {
+    initialCapitalization,
+    dateToString,
+    hideClass,
     routes: constants.routes,
     application: applicationData,
     developmentDetails,
@@ -60,10 +66,7 @@ const getContext = request => {
     files,
     biodiversityGainSiteNumber,
     confirmDevelopmentDetails,
-    confirmOffsiteGainDetails,
-    initialCapitalization,
-    dateToString,
-    hideClass
+    confirmOffsiteGainDetails
   }
 }
 
@@ -71,8 +74,7 @@ export default [{
   method: 'GET',
   path: constants.routes.DEVELOPER_CHECK_ANSWERS,
   handler: handlers.get
-},
-{
+}, {
   method: 'POST',
   path: constants.routes.DEVELOPER_CHECK_ANSWERS,
   handler: handlers.post
