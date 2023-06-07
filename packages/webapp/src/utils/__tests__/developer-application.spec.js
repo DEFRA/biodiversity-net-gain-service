489132be9e4f7d406f4bc3e459bc37afca1cfc37
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-06-07 14:38:43 +0530

    Added more test case for util method

--- packages/webapp/src/utils/__tests__/developer-application.spec.js
+++ packages/webapp/src/utils/__tests__/developer-application.spec.js
@@ -1,22 +1,32 @@
-import developerApplicationSession from '../../__mocks__/developer-application-session'
+import setDeveloperApplicationSession from '../../__mocks__/developer-application-session'
 import developerApplication from '../developer-application'
 import constants from '../constants.js'
 
 describe('developer-application', () => {
   it('Should set the metric file has been uploaded', () => {
-    const session = developerApplicationSession()
+    const session = setDeveloperApplicationSession()
     session.set(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE, 5131037)
-    session.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, '978f462f-e91a-4875-9678-3970157204b1/developer-upload-metric/Sample Metric File.xlsm')
+    session.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, 'mock/developer-upload-metric/Sample Metric File.xlsm')
     session.set(constants.redisKeys.DEVELOPER_METRIC_FILE_TYPE, 'developer-upload-metric')
     session.set(constants.redisKeys.DEVELOPER_ADDITIONAL_EMAILS, [
       {
-        fullName: 'Ajinkya1 Jawalkar',
-        email: 'ajinkya1.jawalkar@capgemini.com'
+        fullName: 'Test User',
+        email: 'test@example.com'
       }
     ])
     const app = developerApplication(session)
     expect(app.developerAllocation.files[0].fileType).toEqual('developer-upload-metric')
     expect(app.developerAllocation.files[0].fileSize).toEqual(5131037)
-    expect(app.developerAllocation.additionalEmailAddresses).toEqual([{ fullName: 'Ajinkya1 Jawalkar', email: 'ajinkya1.jawalkar@capgemini.com' }])
+    expect(app.developerAllocation.additionalEmailAddresses).toEqual([{ fullName: 'Test User', email: 'test@example.com' }])
+  })
+
+  it('Should handle nullable fields if session data not exists', () => {
+    const session = setDeveloperApplicationSession()
+    session.clear(constants.redisKeys.APPLICATION_REFERENCE)
+    session.clear(constants.redisKeys.DEVELOPER_ADDITIONAL_EMAILS)
+
+    const app = developerApplication(session)
+    expect(app.developerAllocation.referenceNumber).toEqual('')
+    expect(app.developerAllocation.additionalEmailAddresses).toEqual([])
   })
 })
