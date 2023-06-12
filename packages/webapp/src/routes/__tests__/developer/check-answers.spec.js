e7f74907c4d67885c8bb80034e916ee85c171f80
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-06-12 17:18:17 +0530

    Modified test cases and route

--- packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
+++ packages/webapp/src/routes/__tests__/developer/check-answers.spec.js
@@ -1,9 +1,9 @@
-import setDeveloperApplicationSession from '../../../__mocks__/developer-application-session.js'
-import checkAnswers from '../../../routes/developer/check-answers.js'
 import constants from '../../../utils/constants.js'
 import { submitGetRequest } from '../helpers/server.js'
 import developerApplicationData from '../../../__mocks__/developer-application-data.js'
+import setDeveloperApplicationSession from '../../__mocks__/developer-application-session.js'
 
+const checkAnswers = require('../../../routes/developer/check-answers.js')
 const url = constants.routes.DEVELOPER_CHECK_ANSWERS
 jest.mock('../../../utils/http.js')
 
--- packages/webapp/src/routes/developer/check-answers.js
+++ packages/webapp/src/routes/developer/check-answers.js
@@ -1,11 +1,11 @@
 import constants from '../../utils/constants.js'
 import developerApplication from '../../utils/developer-application.js'
+import developerApplicationValidation from '../../utils/developer-application-validation.js'
 import {
   initialCapitalization,
   dateToString,
   hideClass
 } from '../../utils/helpers.js'
-import developerApplicationValidation from '../../utils/developer-application-validation.js'
 import { postJson } from '../../utils/http.js'
 
 const handlers = {
@@ -63,8 +63,4 @@ export default [{
   method: 'GET',
   path: constants.routes.DEVELOPER_CHECK_ANSWERS,
   handler: handlers.get
-}, {
-  method: 'POST',
-  path: constants.routes.DEVELOPER_CHECK_ANSWERS,
-  handler: handlers.post
 }]
