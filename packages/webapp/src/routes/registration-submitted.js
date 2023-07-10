1e01d9beeeb4ad05697f5f4104659fe499612410
usman-j-shaikh <usman.jilani-shaikh@capgemini.com>
2023-07-10 20:29:54 +0530

    Removed formatting method from registration submitted

--- packages/webapp/src/routes/registration-submitted.js
+++ packages/webapp/src/routes/registration-submitted.js
@@ -1,9 +1,8 @@
 import constants from '../utils/constants.js'
-import { formatAppRef } from '../utils/helpers.js'
 
 const handlers = {
   get: async (request, h) => {
-    const applicationReference = formatAppRef(request.yar.get(constants.redisKeys.APPLICATION_REFERENCE))
+    const applicationReference = request.yar.get(constants.redisKeys.APPLICATION_REFERENCE)
     // Reset user session as submitted
     request.yar.reset()
     return h.view(constants.views.REGISTRATION_SUBMITTED, {
