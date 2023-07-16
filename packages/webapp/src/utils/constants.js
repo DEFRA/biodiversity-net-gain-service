f3e57a4209e6598d31a19716461b5b8756d07952
Kiran Varma <varma.ap@gmail.com>
2023-07-16 14:51:47 +0000

    wip: all functionality working
    todo: validations and tests

--- packages/webapp/src/utils/constants.js
+++ packages/webapp/src/utils/constants.js
@@ -9,6 +9,7 @@ const LEGAL_AGREEMENT_TYPE = 'land/legal-agreement-type'
 const LEGAL_AGREEMENT_FILE_OPTION = 'legal-agreement-file-option'
 const LOCAL_LAND_CHARGE_FILE_OPTION = 'local-land-charge-file-option'
 const ADD_LEGAL_AGREEMENT_PARTIES = 'land/add-legal-agreement-parties'
+const LEGAL_PARTY_REMOVE = 'land/legal-party-remove'
 const LEGAL_PARTY_LIST = 'land/legal-party-list'
 const LEGAL_AGREEMENT_START_DATE = 'land/legal-agreement-start-date'
 const CHECK_MANAGEMENT_PLAN = 'land/check-management-plan-file'
@@ -258,9 +259,11 @@ const DEVELOPER_CONFIRM_OFF_SITE_GAIN = {
 
 const redisKeys = {
   ...developerConstants.redisKeys,
+  ADD_LEGAL_AGREEMENT_PARTIES,
   APPLICATION_REFERENCE,
   ORIGINAL_GEOSPATIAL_UPLOAD_LOCATION,
   REPROJECTED_GEOSPATIAL_UPLOAD_LOCATION,
+  LEGAL_PARTY_REMOVE,
   GEOSPATIAL_UPLOAD_LOCATION,
   GEOSPATIAL_MAP_CONFIG,
   GEOSPATIAL_UPLOAD_TYPE,
@@ -346,6 +349,7 @@ let routes = {
   CHECK_LEGAL_AGREEMENT,
   ADD_LEGAL_AGREEMENT_PARTIES,
   LEGAL_PARTY_LIST,
+  LEGAL_PARTY_REMOVE,
   LEGAL_AGREEMENT_START_DATE,
   LEGAL_AGREEMENT_TYPE,
   CHECK_MANAGEMENT_PLAN,
