37eddb8a83b91dda5479da1f2916a9a375da18aa
Tedd Mason <teddmason@gmail.com>
2023-07-18 08:08:01 +0100

    bug fix for when a post errors to set the auth object onPostHandler

--- packages/webapp/src/plugins/on-post-handler.js
+++ packages/webapp/src/plugins/on-post-handler.js
@@ -6,23 +6,26 @@ const onPostHandler = {
     register: (server, _options) => {
       server.ext('onPostHandler', function (request, h) {
         // filter out everything but view gets
-        if (request.response.variety === 'view' && request.method === 'get') {
-          // if getting a view then set headers to stop client caching
-          request.response.headers['cache-control'] = 'no-cache, no-store, must-revalidate'
-          if (request.headers.referer) {
-            // If referer was a check route then set the session referer
-            // Route then decides whether to redirect to referer or not
-            const setReferer = constants.setReferer.find(item => request.headers.referer.indexOf(item) > -1)
-            const clearReferer = constants.clearReferer.find(item => request.headers.referer.indexOf(item) > -1)
-            if (setReferer) {
-              request.yar.set(constants.redisKeys.REFERER, `/${setReferer}`)
-            } else if (clearReferer) {
+        if (request.response.variety === 'view') {
+          if (request.method === 'get') {
+            // if getting a view then set headers to stop client caching
+            request.response.headers['cache-control'] = 'no-cache, no-store, must-revalidate'
+            if (request.headers.referer) {
+              // If referer was a check route then set the session referer
+              // Route then decides whether to redirect to referer or not
+              const setReferer = constants.setReferer.find(item => request.headers.referer.indexOf(item) > -1)
+              const clearReferer = constants.clearReferer.find(item => request.headers.referer.indexOf(item) > -1)
+              if (setReferer) {
+                request.yar.set(constants.redisKeys.REFERER, `/${setReferer}`)
+              } else if (clearReferer) {
+                request.yar.clear(constants.redisKeys.REFERER)
+              }
+            } else {
+              // If no referer then clear referer key because user has broken the journey
               request.yar.clear(constants.redisKeys.REFERER)
             }
-          } else {
-            // If no referer then clear referer key because user has broken the journey
-            request.yar.clear(constants.redisKeys.REFERER)
           }
+
           // Add Account details to context if present
           if (request.auth?.isAuthenticated && request.auth.credentials?.account) {
             if (!h.request.response.source.context) {
