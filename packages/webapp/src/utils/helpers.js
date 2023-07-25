e6fdf5945fc8dfffa36e931fca9003dc5a4d722c
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-07-25 14:40:50 +0100

    resolved issues from existing code

--- packages/webapp/src/utils/helpers.js
+++ packages/webapp/src/utils/helpers.js
@@ -174,10 +174,10 @@ const hideClass = hidden => hidden ? 'hidden' : ''
 
 const getNameAndRoles = legalAgreementParties => {
   const partySelectionContent = []
-  legalAgreementParties && legalAgreementParties.organisations.forEach((organisation, index) => {
-    const selectedRole = legalAgreementParties.roles[index]
-    const roleName = selectedRole.value !== undefined ? selectedRole.value : selectedRole.otherPartyName
-    partySelectionContent.push(`${organisation.value} (${roleName})`)
+  legalAgreementParties && Object.values(legalAgreementParties).forEach((organisation, index) => {
+    // const selectedRole = legalAgreementParties.roles[index]
+    // const roleName = selectedRole.value !== undefined ? selectedRole.value : selectedRole.otherPartyName
+    partySelectionContent.push(`${organisation.organisationName} (${organisation.organisationRole})`)
   })
   return partySelectionContent
 }
