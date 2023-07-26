948f1974e957755834bc3270466ec8fdc38fc3fe
Sooraj Vijayan <sooraj.vijayan@defra.gov.uk>
2023-07-26 21:32:17 +0100

    feature/BNGP-3094-upload-search

--- packages/webapp/src/utils/application.js
+++ packages/webapp/src/utils/application.js
@@ -3,66 +3,33 @@ import path from 'path'
 import { getLegalAgreementParties } from './helpers.js'
 
 // Application object schema must match the expected payload format for the Operator application
+const getApplicant = (session) => ({
+  firstName: null,
+  lastName: session.get(constants.redisKeys.FULL_NAME),
+  role: session.get(constants.redisKeys.ROLE_KEY) === 'Other' ? `Other: ${session.get(constants.redisKeys.ROLE_OTHER)}` : session.get(constants.redisKeys.ROLE_KEY),
+  emailaddress: session.get(constants.redisKeys.EMAIL_VALUE)
+})
 
-const application = session => {
-  return {
-    landownerGainSiteRegistration: {
-      applicant: {
-        firstName: null,
-        lastName: session.get(constants.redisKeys.FULL_NAME),
-        role: session.get(constants.redisKeys.ROLE_KEY) === 'Other' ? `Other: ${session.get(constants.redisKeys.ROLE_OTHER)}` : session.get(constants.redisKeys.ROLE_KEY),
-        emailaddress: session.get(constants.redisKeys.EMAIL_VALUE)
-      },
-      files: [
-        {
-          contentMediaType: session.get(constants.redisKeys.LEGAL_AGREEMENT_FILE_TYPE),
-          fileType: 'legal-agreement',
-          fileSize: session.get(constants.redisKeys.LEGAL_AGREEMENT_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.LEGAL_AGREEMENT_LOCATION),
-          fileName: session.get(constants.redisKeys.LEGAL_AGREEMENT_LOCATION) && path.basename(session.get(constants.redisKeys.LEGAL_AGREEMENT_LOCATION))
-        }, getLandBoundaryFile(session), {
-          contentMediaType: session.get(constants.redisKeys.MANAGEMENT_PLAN_FILE_TYPE),
-          fileType: 'management-plan',
-          fileSize: session.get(constants.redisKeys.MANAGEMENT_PLAN_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.MANAGEMENT_PLAN_LOCATION),
-          fileName: session.get(constants.redisKeys.MANAGEMENT_PLAN_LOCATION) && path.basename(session.get(constants.redisKeys.MANAGEMENT_PLAN_LOCATION))
-        }, {
-          contentMediaType: session.get(constants.redisKeys.METRIC_FILE_TYPE),
-          fileType: 'metric',
-          fileSize: session.get(constants.redisKeys.METRIC_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.METRIC_LOCATION),
-          fileName: session.get(constants.redisKeys.METRIC_LOCATION) && path.basename(session.get(constants.redisKeys.METRIC_LOCATION))
-        }, {
-          contentMediaType: session.get(constants.redisKeys.LAND_OWNERSHIP_FILE_TYPE),
-          fileType: 'land-ownership',
-          fileSize: session.get(constants.redisKeys.LAND_OWNERSHIP_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.LAND_OWNERSHIP_LOCATION),
-          fileName: session.get(constants.redisKeys.LAND_OWNERSHIP_LOCATION) && path.basename(session.get(constants.redisKeys.LAND_OWNERSHIP_LOCATION))
-        },
-        {
-          contentMediaType: session.get(constants.redisKeys.LOCAL_LAND_CHARGE_FILE_TYPE),
-          fileType: 'local-land-charge',
-          fileSize: session.get(constants.redisKeys.LOCAL_LAND_CHARGE_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.LOCAL_LAND_CHARGE_LOCATION),
-          fileName: session.get(constants.redisKeys.LOCAL_LAND_CHARGE_LOCATION) && path.basename(session.get(constants.redisKeys.LOCAL_LAND_CHARGE_LOCATION))
-        }
-      ],
-      gainSiteReference: session.get(constants.redisKeys.APPLICATION_REFERENCE) || '',
-      habitatWorkStartDate: session.get(constants.redisKeys.HABITAT_WORKS_START_DATE_KEY),
-      landBoundaryGridReference: getGridReference(session),
-      landBoundaryHectares: getHectares(session),
-      legalAgreementParties: session.get(constants.redisKeys.LEGAL_AGREEMENT_PARTIES) && getLegalAgreementParties(session.get(constants.redisKeys.LEGAL_AGREEMENT_PARTIES)),
-      legalAgreementType: session.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE),
-      legalAgreementStartDate: session.get(constants.redisKeys.LEGAL_AGREEMENT_START_DATE_KEY),
-      otherLandowners: otherLandowners(session) || [],
-      managementMonitoringStartDate: session.get(constants.redisKeys.MANAGEMENT_MONITORING_START_DATE_KEY),
-      submittedOn: new Date().toISOString(),
-      landownerConsent: session.get(constants.redisKeys.LANDOWNER_CONSENT_KEY) || 'false',
-      metricData: session.get(constants.redisKeys.METRIC_DATA)
-    }
-  }
+const getFile = (session, fileType) => ({
+  contentMediaType: session.get(constants.redisKeys[fileType.toUpperCase() + '_FILE_TYPE']),
+  fileType: fileType,
+  fileSize: session.get(constants.redisKeys[fileType.toUpperCase() + '_FILE_SIZE']),
+  fileLocation: session.get(constants.redisKeys[fileType.toUpperCase() + '_LOCATION']),
+  fileName: session.get(constants.redisKeys[fileType.toUpperCase() + '_LOCATION']) && path.basename(session.get(constants.redisKeys[fileType.toUpperCase() + '_LOCATION']))
+})
+
+const getFiles = (session) => {
+  const files = [
+    getFile(session, 'legal-agreement'),
+    getLandBoundaryFile(session),
+    getFile(session, 'management-plan'),
+    getFile(session, 'metric'),
+    getFile(session, 'land-ownership'),
+    getFile(session, 'local-land-charge')
+  ]
+
+  return files
 }
-
 const otherLandowners = session => session.get(constants.redisKeys.LANDOWNERS) &&
   session.get(constants.redisKeys.LANDOWNERS).map(e => { return { name: e } })
 
@@ -110,5 +77,26 @@ const getHectares = session => session.get(constants.redisKeys.LAND_BOUNDARY_UPL
 const getGridReference = session => session.get(constants.redisKeys.LAND_BOUNDARY_UPLOAD_TYPE) === 'geospatialData'
   ? session.get(constants.redisKeys.GEOSPATIAL_GRID_REFERENCE)
   : session.get(constants.redisKeys.LAND_BOUNDARY_GRID_REFERENCE)
+
+const application = session => {
+  return {
+    landownerGainSiteRegistration: {
+      applicant: getApplicant(session),
+      files: getFiles(session),
+      gainSiteReference: session.get(constants.redisKeys.APPLICATION_REFERENCE) || '',
+      habitatWorkStartDate: session.get(constants.redisKeys.HABITAT_WORKS_START_DATE_KEY),
+      landBoundaryGridReference: getGridReference(session),
+      landBoundaryHectares: getHectares(session),
+      legalAgreementParties: session.get(constants.redisKeys.LEGAL_AGREEMENT_PARTIES) && getLegalAgreementParties(session.get(constants.redisKeys.LEGAL_AGREEMENT_PARTIES)),
+      legalAgreementType: session.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE),
+      legalAgreementStartDate: session.get(constants.redisKeys.LEGAL_AGREEMENT_START_DATE_KEY),
+      otherLandowners: otherLandowners(session) || [],
+      managementMonitoringStartDate: session.get(constants.redisKeys.MANAGEMENT_MONITORING_START_DATE_KEY),
+      submittedOn: new Date().toISOString(),
+      landownerConsent: session.get(constants.redisKeys.LANDOWNER_CONSENT_KEY) || 'false',
+      metricData: session.get(constants.redisKeys.METRIC_DATA)
+    }
+  }
+}
 
 export default application
