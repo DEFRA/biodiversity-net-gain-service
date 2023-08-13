01a37c141977e39da0aeaed066060242d49779e1
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2023-08-13 21:03:39 +0100

    Redirect to tier page if yar session empty, plus formatting

--- packages/webapp/src/routes/credits-estimation/credits-cost.js
+++ packages/webapp/src/routes/credits-estimation/credits-cost.js
@@ -1,8 +1,13 @@
 import constants from '../../credits/constants.js'
 
-const toLocaleString = str => str.toLocaleString('en-gb', { style: 'currency', currency: 'GBP', minimumFractionDigits: 0 })
-const getRow = ({ tier, unitAmount, cost }) =>
-  [{ text: tier.toUpperCase() }, { text: unitAmount, format: 'numeric' }, { text: toLocaleString(cost), format: 'numeric' }]
+const toLocaleString = str =>
+  str.toLocaleString('en-gb', { style: 'currency', currency: 'GBP', minimumFractionDigits: 0 })
+
+const getRow = ({ tier, unitAmount, cost }) => [
+  { text: tier.toUpperCase() },
+  { text: unitAmount, format: 'numeric' },
+  { text: toLocaleString(cost), format: 'numeric' }
+]
 
 export default [
   {
@@ -13,7 +18,13 @@ export default [
     },
     handler: (request, h) => {
       const creditCosts = request.yar.get(constants.redisKeys.ESTIMATOR_CREDITS_CALCULATION)
+
+      if (!creditCosts) {
+        return h.redirect(constants.routes.ESTIMATOR_CREDITS_TIER)
+      }
+
       const totalCost = toLocaleString(creditCosts.total)
+
       return h.view(constants.views.ESTIMATOR_CREDITS_COST, {
         backLink: constants.routes.ESTIMATOR_CREDITS_TIER,
         totalCost,
