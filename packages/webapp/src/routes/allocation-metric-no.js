8a48878128a3a2996655467ceead86609d02a464
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-08-30 09:23:49 +0000

    Added need metric file for developer journey

--- packages/webapp/src/routes/allocation-metric-no.js
+++
@@ -1,7 +0,0 @@
-import constants from '../utils/constants.js'
-
-export default {
-  method: 'GET',
-  path: constants.routes.ALLOCATION_METRIC_NO,
-  handler: (_request, h) => h.view(constants.views.ALLOCATION_METRIC_NO)
-}
