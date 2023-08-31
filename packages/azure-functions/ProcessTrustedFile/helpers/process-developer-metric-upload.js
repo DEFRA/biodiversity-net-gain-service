0ba344f0b4c16e52d36591ea4dfb1688d719c9dc
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-08-31 08:02:46 +0000

    Removed temparary debug logs

--- packages/azure-functions/ProcessTrustedFile/helpers/process-developer-metric-upload.js
+++ packages/azure-functions/ProcessTrustedFile/helpers/process-developer-metric-upload.js
@@ -2,7 +2,6 @@ import buildSignalRMessage from '../../Shared/build-signalr-message.js'
 import bngMetricService from '@defra/bng-metric-service'
 import { blobStorageConnector } from '@defra/bng-connectors-lib'
 import processMetric from '../../Shared/process-metric.js'
-import { logger } from 'defra-logging-facade'
 
 export default async function (context, config) {
   let signalRMessageArguments, metricData
@@ -15,7 +14,6 @@ export default async function (context, config) {
     if (response) {
       const documentStream = response.readableStreamBody
       const { startExtractionConfig, getExtractionConfigForDeveloper } = bngMetricService.extractionConfiguration
-      logger.info(`${new Date().toUTCString()} Config ${JSON.stringify(await getExtractionConfigForDeveloper())}`)
       const metricExtractionConfig = {
         extractionConfiguration: {
           start: startExtractionConfig,
@@ -28,14 +26,12 @@ export default async function (context, config) {
       throw new Error('Unable to retrieve blob for developer metric file')
     }
 
-    logger.info(`${new Date().toUTCString()} Metric data ${JSON.stringify(metricData)}`)
     signalRMessageArguments = [{
       location: config.fileConfig.fileLocation,
       metricData: processMetric(metricData)
     }]
   } catch (err) {
     context.log.error(err)
-    logger.info(`${new Date().toUTCString()} Error ${err}`)
     signalRMessageArguments = [{ errorCode: err.code }]
   } finally {
     context.bindings.signalRMessages = [buildSignalRMessage(config.signalRMessageConfig, signalRMessageArguments)]
