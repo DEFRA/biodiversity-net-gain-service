bef193e8792eddb5a48ea4b9d1f6f4bb4acb6343
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-08-31 10:21:13 +0100

    Fix developer seed data test

--- packages/webapp/src/routes/__tests__/test/seed-developer-data.spec.js
+++ packages/webapp/src/routes/__tests__/test/seed-developer-data.spec.js
@@ -6,7 +6,7 @@ describe(url, () => {
   describe('GET', () => {
     it(`should render the ${url.substring(1)} view`, async () => {
       const response = await submitGetRequest({ url })
-      expect(response.request.yar.get(constants.redisKeys.DEVELOPER_FULL_NAME)).toEqual('Test Test')
+      expect(response.request.yar.get(constants.redisKeys.DEVELOPER_FULL_NAME)).toEqual('Test Name')
       expect(response.request.yar.get(constants.redisKeys.DEVELOPER_EMAIL_VALUE)).toEqual('test@test.com')
     })
   })
