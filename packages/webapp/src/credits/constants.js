543673d2ec1200b76dd67abaa77db43bd26eed96
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-09-21 13:42:10 +0000

    Added new routes and view for purchase order number

--- packages/webapp/src/credits/constants.js
+++ packages/webapp/src/credits/constants.js
@@ -2,7 +2,8 @@ const CREDITS_ESTIMATION_PATH = '/credits-estimation'
 
 const routes = {
   ESTIMATOR_CREDITS_COST: '/credits-estimation/credits-cost',
-  ESTIMATOR_CREDITS_TIER: '/credits-estimation/credits-tier'
+  ESTIMATOR_CREDITS_TIER: '/credits-estimation/credits-tier',
+  ESTIMATOR_CREDITS_PURCHASE_ORDER: '/credits-estimation/credits-purchase-order'
 }
 
 const views = Object.fromEntries(Object.entries(routes).map(([k, v]) => [k, v.substring(1)]))
@@ -12,6 +13,8 @@ export default {
   routes,
   views,
   redisKeys: {
-    ESTIMATOR_CREDITS_CALCULATION: 'estimator-credits-calculation'
+    ESTIMATOR_CREDITS_CALCULATION: 'estimator-credits-calculation',
+    CREDITS_PURCHASE_ORDER_NUMBER: 'credits-purchase-order-number',
+    WILL_CREDITS_PO_IN_USE: 'will-credits-po-in-use'
   }
 }
---
+++ packages/webapp/src/routes/credits-estimation/credits-purchase-order.js
@@ -0,0 +1,81 @@
+import constants from '../../credits/constants.js'
+import { checked } from '../../utils/helpers.js'
+
+export default [
+  {
+    method: 'GET',
+    path: constants.routes.ESTIMATOR_CREDITS_PURCHASE_ORDER,
+    options: {
+      auth: false
+    },
+    handler: (request, h) => {
+      const willPOInUse = request.yar.get(constants.redisKeys.WILL_CREDITS_PO_IN_USE)
+      const purchaseOrderNumber = request.yar.get(constants.redisKeys.CREDITS_PURCHASE_ORDER_NUMBER)
+
+      return h.view(constants.views.ESTIMATOR_CREDITS_PURCHASE_ORDER, {
+        willPOInUse,
+        purchaseOrderNumber,
+        checked
+      })
+    }
+  },
+  {
+    method: 'POST',
+    path: constants.routes.ESTIMATOR_CREDITS_PURCHASE_ORDER,
+    options: {
+      auth: false
+    },
+    handler: async (request, h) => {
+      const willPOInUse = request.payload.willPOInUse
+      const purchaseOrderNumber = request.payload.purchaseOrderNumber
+      request.yar.set(constants.redisKeys.WILL_CREDITS_PO_IN_USE, willPOInUse)
+
+      const error = validateData(willPOInUse, purchaseOrderNumber)
+      if (error) {
+        return h.view(constants.views.ESTIMATOR_CREDITS_PURCHASE_ORDER, {
+          purchaseOrderNumber,
+          willPOInUse,
+          checked,
+          ...error
+        })
+      } else {
+        if (willPOInUse === 'yes') {
+          request.yar.set(constants.redisKeys.CREDITS_PURCHASE_ORDER_NUMBER, purchaseOrderNumber)
+
+          return h.view(constants.views.ESTIMATOR_CREDITS_PURCHASE_ORDER, {
+            purchaseOrderNumber,
+            willPOInUse,
+            checked
+          })
+        } else {
+          request.yar.clear(constants.redisKeys.CREDITS_PURCHASE_ORDER_NUMBER)
+
+          return h.view(constants.views.ESTIMATOR_CREDITS_PURCHASE_ORDER, {
+            purchaseOrderNumber: null,
+            willPOInUse,
+            checked
+          })
+        }
+      }
+    }
+  }
+]
+
+const validateData = (willPOInUse, purchaseOrderNumber) => {
+  const error = {}
+  if (!willPOInUse) {
+    error.err = [{
+      text: 'Select yes if you will be using a purchase order',
+      href: '#willPOInUseYes'
+    }]
+  } else if (willPOInUse === 'yes' && !purchaseOrderNumber?.trim()) {
+    error.err = [{
+      text: 'Purchase order number cannot be left blank',
+      href: '#purchaseOrderNumber'
+    }, {
+      text: '',
+      href: ''
+    }]
+  }
+  return error.err ? error : null
+}
