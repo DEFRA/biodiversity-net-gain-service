f9de661ae53d454bc606b819eb24ca22a8d56a73
Usman Shaikh <115730841+usman-j-shaikh@users.noreply.github.com>
2023-09-21 18:10:48 +0530

    Feature/BNGP-3450: Metric Upload - add I do not have a metric file option (#405)
    * Modified no metric file route and content
    
    * Added help section on upload metric file page of dj
    
    * Added need metric file for developer journey
    
    * Minor changes in test file
    
    * Minor fixes
    
    * Contact number changed

--- packages/webapp/src/utils/developer-constants.js
+++ packages/webapp/src/utils/developer-constants.js
@@ -23,6 +23,7 @@ const DEVELOPER_CONFIRM_OFF_SITE_GAIN = 'developer/confirm-off-site-gain'
 const DEVELOPER_CONSENT_AGREEMENT_UPLOAD = 'developer/consent-agreement-upload'
 const DEVELOPER_AGREEMENT_CHECK = 'developer/consent-agreement-check'
 const DEVELOPER_DOWNLOAD_CONSENT_FILE = 'developer/download-consent-file'
+const DEVELOPER_NEED_METRIC = 'developer/need-metric'
 // ./Routes constants
 
 // RedisKeys constants
@@ -120,7 +121,8 @@ export default {
     DEVELOPER_CONFIRM_OFF_SITE_GAIN,
     DEVELOPER_CONSENT_AGREEMENT_UPLOAD,
     DEVELOPER_AGREEMENT_CHECK,
-    DEVELOPER_DOWNLOAD_CONSENT_FILE
+    DEVELOPER_DOWNLOAD_CONSENT_FILE,
+    DEVELOPER_NEED_METRIC
   },
   redisKeys: {
     DEVELOPER_METRIC_DATA,
