6317a044225b07332693ec7055bd83f90328eef0
Tedd Mason <teddmason@gmail.com>
2023-10-02 17:37:29 +0100

    removing promise return in async function

--- packages/webapp/src/utils/azure-storage.js
+++ packages/webapp/src/utils/azure-storage.js
@@ -6,35 +6,32 @@ const uploadStreamAndAwaitScan = async (logger, config, stream) => {
   const { filename } = stream
   await blobStorageConnector.uploadStream(config.blobConfig, stream)
   logger.log(`${new Date().toUTCString()} ${filename} has been uploaded`)
+  const start = performance.now()
+  let blobTags
+  const timeout = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+  // Give Microsoft Defender 4s lead time
+  // After testing on Azure infrastructure apply a sensible lead time wait.
+  // await timeout(4000)
+  const wait = 1000
+  const maxAttempts = 20; let attempts = 0
+  do {
+    await timeout(wait)
+    blobTags = await blobStorageConnector.getBlobTags(config.blobConfig)
+    attempts++
+  } while (Object.keys(blobTags.tags).length === 0 && attempts < maxAttempts)
 
-  return new Promise(async (resolve, reject) => {
-    const start = performance.now()
-    let blobTags
-    const timeout = (ms) => {
-      return new Promise(resolve => setTimeout(resolve, ms))
-    }
-    // Give Microsoft Defender 4s lead time
-    // After testing on Azure infrastructure apply a sensible lead time wait.
-    // await timeout(4000)
-    const wait = 1000
-    const maxAttempts = 20; let attempts = 0
-    do {
-      await timeout(wait)
-      blobTags = await blobStorageConnector.getBlobTags(config.blobConfig)
-      attempts++
-    } while (Object.keys(blobTags.tags).length === 0 && attempts < maxAttempts)
+  const end = performance.now()
 
-    const end = performance.now()
-
-    if (Object.keys(blobTags.tags).length === 0) {
-      logger.log(`${new Date().toUTCString()} ${filename} No malware scan response after ${Math.round(end - start)}ms`)
-      reject(new Error(constants.uploadErrors.noFileScanResponse))
-    } else {
-      logger.log(`${new Date().toUTCString()} ${filename} malware scan response after ${Math.round(end - start)}ms`)
-      logger.log(`${new Date().toUTCString()} ${filename} malware scan results: ${JSON.stringify(blobTags.tags)}`)
-      resolve(blobTags.tags)
-    }
-  })
+  if (Object.keys(blobTags.tags).length === 0) {
+    logger.log(`${new Date().toUTCString()} ${filename} No malware scan response after ${Math.round(end - start)}ms`)
+    throw new Error(constants.uploadErrors.noFileScanResponse)
+  } else {
+    logger.log(`${new Date().toUTCString()} ${filename} malware scan response after ${Math.round(end - start)}ms`)
+    logger.log(`${new Date().toUTCString()} ${filename} malware scan results: ${JSON.stringify(blobTags.tags)}`)
+    return blobTags.tags
+  }
 }
 
 const deleteBlobFromContainers = async blobName => {
