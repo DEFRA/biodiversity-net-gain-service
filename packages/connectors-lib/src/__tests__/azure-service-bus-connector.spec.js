381abb90428355c6c29e4403ace454c6a27ab5a4
Tedd Mason <teddmason@gmail.com>
2023-10-03 10:36:38 +0100

    pulling out redundant connectors

--- packages/connectors-lib/src/__tests__/azure-service-bus-connector.spec.js
+++
@@ -1,8 +0,0 @@
-// import { serviceBusConnector } from '../azure-service-bus-connector.js'
-// jest.mock('@azure/service-bus')
-
-// describe('azure service bus connector', () => {
-//   it('should initialise', () => {
-//     serviceBusConnector.init('test string')
-//   })
-// })
