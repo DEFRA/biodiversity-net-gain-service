33dc5ea355b8198b1a6e8b831a3c46a4e99e011f
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-10-11 12:35:40 +0100

    added condition for ENV variables N or null

--- packages/webapp/src/routes/__tests__/land/register-land-task-list.spec.js
+++ packages/webapp/src/routes/__tests__/land/register-land-task-list.spec.js
@@ -16,7 +16,94 @@ describe(url, () => {
       await submitGetRequest({ url })
     })
 
-    it('should render view with no completed task', async () => {
+    it('should render view with no completed task: ENV ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL = N', async () => {
+      process.env.ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL = 'N'
+      let viewResult, contextResult
+      const h = {
+        view: (view, context) => {
+          viewResult = view
+          contextResult = context
+        }
+      }
+      const redisMap = new Map()
+      const request = {
+        yar: redisMap
+      }
+
+      const registerTaskList = require('../../../routes/land/register-land-task-list')
+      await registerTaskList.default[0].handler(request, h)
+
+      const response = await submitGetRequest(getOptions)
+      expect(response.statusCode).toBe(200)
+      expect(viewResult).toEqual('land/register-land-task-list')
+      expect(contextResult.registrationTasks.taskList.length).toEqual(4)
+      expect(contextResult.registrationTasks.taskList[0]).toEqual({
+        taskTitle: 'Land information',
+        tasks: [
+          {
+            title: 'Add land ownership details',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-ownership-details',
+            startTaskUrl: '/land/upload-ownership-proof',
+            inProgressUrl: '',
+            id: 'add-land-ownership'
+          },
+          {
+            title: 'Add biodiversity gain site boundary details',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-land-boundary-details',
+            startTaskUrl: '/land/choose-land-boundary-upload',
+            inProgressUrl: '',
+            id: 'add-land-boundary'
+          }
+        ]
+      })
+      expect(contextResult.registrationTasks.taskList[1]).toEqual({
+        taskTitle: 'Habitat information',
+        tasks: [
+          {
+            title: 'Add habitat baseline, creation and enhancements',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-metric-details',
+            startTaskUrl: '/land/upload-metric',
+            inProgressUrl: '',
+            id: 'add-habitat-information'
+          },
+          {
+            title: 'Add habitat management and monitoring details',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-management-monitoring-details',
+            startTaskUrl: '/land/upload-management-plan',
+            inProgressUrl: '',
+            id: 'add-habitat-management'
+          }
+        ]
+      })
+      expect(contextResult.registrationTasks.taskList[2]).toEqual({
+        taskTitle: 'Legal information',
+        tasks: [
+          {
+            title: 'Add legal agreement details',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-legal-agreement-details',
+            startTaskUrl: '/land/legal-agreement-type',
+            inProgressUrl: '',
+            id: 'add-legal-agreement'
+          },
+          {
+            title: 'Add local land charge search certificate',
+            status: 'NOT STARTED',
+            completedTaskUrl: '/land/check-local-land-charge-file',
+            startTaskUrl: '/land/upload-local-land-charge',
+            inProgressUrl: '',
+            id: 'add-local-land-charge-search-certificate'
+          }
+        ]
+      })
+    })
+
+    it('should render view with no completed task: ENV ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL = null', async () => {
+      process.env.ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL = null
       let viewResult, contextResult
       const h = {
         view: (view, context) => {
