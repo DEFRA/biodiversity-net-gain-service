72598e400980501a9bc1054f0de0855db8852d7d
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-10-11 08:52:04 +0100

    partial code commit - WIP code not working fully

--- packages/webapp/src/credits/constants.js
+++ packages/webapp/src/credits/constants.js
@@ -1,6 +1,8 @@
-const CREDITS_ESTIMATION_PATH = '/credits-estimation'
+import creditConstants from './credit-constants.js'
 
+const CREDITS_ESTIMATION_PATH = '/credits-estimation'
 const routes = {
+  ...creditConstants.routes,
   ESTIMATOR_CREDITS_COST: '/credits-estimation/credits-cost',
   ESTIMATOR_CREDITS_TIER: '/credits-estimation/credits-tier'
 }
@@ -8,10 +10,12 @@ const routes = {
 const views = Object.fromEntries(Object.entries(routes).map(([k, v]) => [k, v.substring(1)]))
 
 export default {
+  ...creditConstants.routes,
   CREDITS_ESTIMATION_PATH,
   routes,
   views,
   redisKeys: {
+    ...creditConstants.redisKeys,
     ESTIMATOR_CREDITS_CALCULATION: 'estimator-credits-calculation'
   }
 }
