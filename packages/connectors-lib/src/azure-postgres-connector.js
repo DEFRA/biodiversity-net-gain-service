8bb7f4ee56af172c0e24c3ac9e302c4e5cbe10d4
Tedd Mason <teddmason@gmail.com>
2023-10-16 09:47:59 +0100

    Adding in missing db ends and some console logging for db pool

--- packages/connectors-lib/src/azure-postgres-connector.js
+++ packages/connectors-lib/src/azure-postgres-connector.js
@@ -8,6 +8,8 @@ class Db {
   }
 
   init (config) {
+    console.log('init')
+    console.log(pool)
     if (!pool || pool.ending) {
       pool = new pg.Pool(config)
     }
@@ -15,11 +17,15 @@ class Db {
 
   query (query, vars) {
     this.init()
+    console.log('query')
+    console.log(query, vars)
     return pool.query(query, vars)
   }
 
   // end must always be called after queries have been completed to ensure clean shutdown of pool and clients
   async end () {
+    console.log('end')
+    console.log(pool)
     if (pool && !pool.ending) {
       await pool.end()
     }
--- packages/utils-lib/src/helpers/get-db-connection.js
+++ packages/utils-lib/src/helpers/get-db-connection.js
@@ -5,8 +5,11 @@ import DbConfig from './db-config.js'
 const getDBConnection = async () => {
   // Clone so that we don't store the token (which will expire)
   const dbConfig = JSON.parse(JSON.stringify(DbConfig))
+  console.log('getDBConnection')
+  console.log(dbConfig)
   if (!dbConfig.password) {
     const credential = new DefaultAzureCredential()
+    console.log('Getting database token')
     dbConfig.password = (await credential.getToken('https://ossrdbms-aad.database.windows.net')).token
   }
   return new postgresConnector.Db(dbConfig)
