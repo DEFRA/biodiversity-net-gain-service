5bb74e3192c4c32cda09cd6587f333cc2a5e2e5a
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-10-16 13:59:10 +0100

    Enable-Disable routes for developer and credit-estimation journey
    Setting Env variables to enable-disable routes for  developer and credit-estimation journey

--- packages/webapp/src/routes/manage-biodiversity-gains.js
+++ packages/webapp/src/routes/manage-biodiversity-gains.js
@@ -1,7 +1,18 @@
 import constants from '../utils/constants.js'
 
-export default {
+let enableDev = false
+if (process.env.ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY === 'Y') {
+  enableDev = true
+}
+
+const handlers = {
+  get: async (request, h) => h.view(constants.views.MANAGE_BIODIVERSITY_GAINS, {
+    enableDev
+  })
+}
+
+export default [{
   method: 'GET',
   path: constants.routes.MANAGE_BIODIVERSITY_GAINS,
-  handler: (_request, h) => h.view(constants.views.MANAGE_BIODIVERSITY_GAINS)
-}
+  handler: handlers.get
+}]
