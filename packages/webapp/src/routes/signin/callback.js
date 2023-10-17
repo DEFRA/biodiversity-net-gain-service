9929484dd92244d5ed5b79fba8f13d094c00a73f
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-10-17 17:00:53 +0100

    Updated code for tasklist implementation

--- packages/webapp/src/routes/signin/callback.js
+++ packages/webapp/src/routes/signin/callback.js
@@ -1,8 +1,9 @@
+import { Boom } from '@hapi/boom'
 import auth from '../../utils/auth.js'
 import constants from '../../utils/constants.js'
 import { postJson } from '../../utils/http.js'
 
-const getRedirectUrl = async (account, preAuthenticationRoute) => {
+const getRedirectUrl = async (request, account, preAuthenticationRoute) => {
   let redirectUrl = constants.routes.MANAGE_BIODIVERSITY_GAINS
   const applicationType = getApplicationType(preAuthenticationRoute)
   if (applicationType) {
@@ -11,7 +12,8 @@ const getRedirectUrl = async (account, preAuthenticationRoute) => {
       applicationType
     })
     if (applications?.length > 0) {
-      if (applications?.length === 1 && applications?.applicationStatus === 'IN PROGRESS') {
+      if (applications?.length === 1 && applications[0]?.applicationStatus === 'IN PROGRESS') {
+        await getApplicationSession(request, applications[0]?.applicationReference, account.idTokenClaims.contactId, applicationType)
         redirectUrl = applicationType === constants.applicationTypes.ALLOCATION ? constants.routes.DEVELOPER_TASKLIST : constants.routes.REGISTER_LAND_TASK_LIST
       } else {
         redirectUrl = applicationType === constants.applicationTypes.ALLOCATION ? constants.routes.DEVELOPER_DEVELOPMENT_PROJECTS : constants.routes.BIODIVERSITY_GAIN_SITES
@@ -20,7 +22,6 @@ const getRedirectUrl = async (account, preAuthenticationRoute) => {
       redirectUrl = applicationType === constants.applicationTypes.ALLOCATION ? constants.routes.DEVELOPER_TASKLIST : constants.routes.REGISTER_LAND_TASK_LIST
     }
   }
-
   return redirectUrl
 }
 
@@ -37,6 +38,19 @@ const getApplicationType = preAuthenticationRoute => {
   return applicationType
 }
 
+const getApplicationSession = async (request, applicationReference, contactId, applicationType) => {
+  const session = await postJson(`${constants.AZURE_FUNCTION_APP_URL}/getapplicationsession`, {
+    applicationReference,
+    contactId,
+    applicationType
+  })
+  if (Object.keys(session).length === 0) {
+    return Boom.badRequest(`${applicationType} with reference ${applicationReference} does not exist`)
+  } else {
+    request.yar.set(session)
+  }
+}
+
 export default [{
   method: 'GET',
   path: constants.routes.SIGNIN_CALLBACK,
@@ -48,7 +62,7 @@ export default [{
   handler: async (request, h) => {
     const account = await auth.authenticate(request.query.code, request.cookieAuth)
     const preAuthenticationRoute = request.yar.get(constants.redisKeys.PRE_AUTHENTICATION_ROUTE, true)
-    const redirectUrl = await getRedirectUrl(account, preAuthenticationRoute)
+    const redirectUrl = await getRedirectUrl(request, account, preAuthenticationRoute)
     return h.redirect(redirectUrl)
   }
 }]
