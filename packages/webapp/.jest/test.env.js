0e8393a7e9f965652dd9863d084d684a8630fb92
Tedd Mason <teddmason@gmail.com>
2023-10-18 15:36:11 +0100

    test env config

--- packages/webapp/.jest/test.env.js
+++ packages/webapp/.jest/test.env.js
@@ -3,6 +3,7 @@ process.env.MAX_GEOSPATIAL_LAND_BOUNDARY_UPLOAD_MB=50
 process.env.AZURE_STORAGE_ACCOUNT='devstoreaccount1'
 process.env.AZURE_STORAGE_ACCESS_KEY='Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=='
 process.env.AZURE_BLOB_SERVICE_URL='http://127.0.0.1:10000/devstoreaccount1'
+process.env.AZURE_QUEUE_SERVICE_URL='http://127.0.0.1:10000/devstoreaccount1'
 process.env.MAX_METRIC_UPLOAD_MB=50
 process.env.NODE_ENV='test'
 process.env.MAX_CONSENT_UPLOAD_MB=50
@@ -17,3 +18,4 @@ process.env.DEFRA_ID_REDIRECT_URI = 'redirectUrl'
 process.env.MAX_GEOSPATIAL_FILE_UPLOAD_MB=1
 process.env.ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL='Y'
 process.env.ENABLE_ROUTE_SUPPORT_FOR_ADDITIONAL_EMAIL='Y'
+process.env.DEFRA_ID_MOCK=false
