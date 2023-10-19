35fc252bb0af9477cf23d675c78f98125ecbbf7d
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-10-19 20:26:37 +0100

    test code refactor

--- packages/webapp/src/routes/__tests__/manage-biodiversity-gains.spec.js
+++ packages/webapp/src/routes/__tests__/manage-biodiversity-gains.spec.js
@@ -5,18 +5,7 @@ const url = constants.routes.MANAGE_BIODIVERSITY_GAINS
 describe(url, () => {
   describe('GET', () => {
     it(`should render the ${url.substring(1)} view`, async () => {
-      process.env.ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY === 'Y'
-      await submitGetRequest({ url }, 200, { enableDev: true })
-    })
-
-    it(`should render the ${url.substring(1)} view`, async () => {
-      process.env.ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY === 'N'
-      await submitGetRequest({ url }, 200, { enableDev: false })
-    })
-
-    it(`should render the ${url.substring(1)} view`, async () => {
-      process.env.ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY === undefined
-      await submitGetRequest({ url }, 200, { enableDev: false })
+      await submitGetRequest({ url }, 200, { enableDev: process.env.ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY === 'Y' })
     })
   })
 })
