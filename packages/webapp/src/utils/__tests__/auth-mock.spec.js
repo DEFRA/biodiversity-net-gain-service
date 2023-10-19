034af601e1b42bf86c2efbeed1b6dd3b35b793bb
Tedd Mason <teddmason@gmail.com>
2023-10-19 08:10:36 +0100

    linting

--- packages/webapp/src/utils/__tests__/auth-mock.spec.js
+++ packages/webapp/src/utils/__tests__/auth-mock.spec.js
@@ -1,18 +1,18 @@
 import authMock from '../auth-mock.js'
 
-describe('auth-mock', ()=>{
-  it('Should mock the auth util with getAuthenticationUrl', ()=> {
+describe('auth-mock', () => {
+  it('Should mock the auth util with getAuthenticationUrl', () => {
     jest.isolateModules(() => {
       const auth = require('../auth')
       authMock(auth)
       expect(auth.getAuthenticationUrl()).toEqual('/signin/callback')
     })
   })
-  it('Should mock the auth util with authenticate mocked function', ()=> {
+  it('Should mock the auth util with authenticate mocked function', () => {
     jest.isolateModules(() => {
       const auth = require('../auth')
       authMock(auth)
-      const token = auth.authenticate({}, { set: () => {}})
+      const token = auth.authenticate({}, { set: () => {} })
       expect(typeof token.idTokenClaims.exp).toBe('number')
       expect(token.idTokenClaims.email).toEqual('johnsmith@email.com')
       expect(token.idTokenClaims.contactId).not.toBeNull()
