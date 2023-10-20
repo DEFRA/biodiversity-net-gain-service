de2269c61f075f9bc2ff09aea77994a57be2e7dc
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-10-20 17:09:00 +0530

    WIP: Improving test coverage

--- packages/webapp/src/routes/__tests__/signin/callback.spec.js
+++ packages/webapp/src/routes/__tests__/signin/callback.spec.js
@@ -210,4 +210,108 @@ describe('Signin callback handler', () => {
       }
     })
   })
+
+  it('should redirect to the registrations tasklist when an authenticated registration user has one associated applications', done => {
+    const mockApplications = [{
+      applicationStatus: 'IN PROGRESS',
+      applicationReference: 'mock-ref'
+    }]
+    const preAuthenticationRoute = '/land/mock-route'
+    const redirectUrl = constants.routes.REGISTER_LAND_TASK_LIST
+    processRedirectionByApplicationType(mockApplications, preAuthenticationRoute, redirectUrl, done)
+  })
+
+  it('should redirect to the registrations tasklist when an authenticated registration user has more than one associated applications', done => {
+    const mockApplications = [{
+      applicationStatus: 'IN PROGRESS',
+      applicationReference: 'mock-ref-1'
+    },
+    {
+      applicationStatus: 'COMPLETED',
+      applicationReference: 'mock-ref-2'
+    }]
+    const preAuthenticationRoute = '/land/mock-route'
+    const redirectUrl = constants.routes.BIODIVERSITY_GAIN_SITES
+    processRedirectionByApplicationType(mockApplications, preAuthenticationRoute, redirectUrl, done)
+  })
+
+  it('should redirect to the allocation tasklist when an authenticated user has one associated applications', done => {
+    const mockApplications = [{
+      applicationStatus: 'IN PROGRESS',
+      applicationReference: 'mock-ref'
+    }]
+    const preAuthenticationRoute = '/developer/mock-route'
+    const redirectUrl = constants.routes.DEVELOPER_TASKLIST
+    processRedirectionByApplicationType(mockApplications, preAuthenticationRoute, redirectUrl, done)
+  })
+
+  it('should redirect to the allocation dashboard when an authenticated user has more than one associated applications', done => {
+    const mockApplications = [{
+      applicationStatus: 'IN PROGRESS',
+      applicationReference: 'mock-ref-1'
+    },
+    {
+      applicationStatus: 'COMPLETED',
+      applicationReference: 'mock-ref-2'
+    }]
+    const preAuthenticationRoute = '/developer/mock-route'
+    const redirectUrl = constants.routes.DEVELOPER_DEVELOPMENT_PROJECTS
+    processRedirectionByApplicationType(mockApplications, preAuthenticationRoute, redirectUrl, done)
+  })
+
+  it('should return a HTTP 400 status code if a registration application reference does not exist', done => {
+    jest.isolateModules(async () => {
+      try {
+        jest.resetAllMocks()
+        jest.mock('../../../utils/http.js')
+        const http = require('../../../utils/http.js')
+        http.postJson = jest.fn().mockImplementation(() => {
+          return {}
+        })
+        await submitGetRequest({ url }, 302, null)
+        done()
+      } catch (err) {
+        done(err)
+      }
+    })
+  })
 })
+
+const processRedirectionByApplicationType = (applications, preAuthenticationRoute, redirectUrl, done, hasAppSession = true) => {
+  jest.isolateModules(async () => {
+    try {
+      jest.resetAllMocks()
+      jest.mock('../../../utils/http.js')
+      const http = require('../../../utils/http.js')
+      http.postJson = jest.fn().mockImplementation(() => applications)
+      auth.authenticate = jest.fn().mockImplementation(() => {
+        return {
+          idTokenClaims: {
+            contactId: 'mock contact id'
+          }
+        }
+      })
+
+      const session = new Session()
+      session.set(constants.redisKeys.PRE_AUTHENTICATION_ROUTE, preAuthenticationRoute)
+
+      if (!hasAppSession) {
+        http.postJson = jest.fn().mockImplementation(() => [])
+      }
+
+      let redirectArgs = ''
+      const h = {
+        redirect: (...args) => {
+          redirectArgs = args
+        }
+      }
+      const getHandler = callback[0].handler
+      await getHandler({ yar: session, query: {} }, h)
+      expect(redirectArgs[0]).toEqual(redirectUrl)
+      expect(auth.authenticate).toHaveBeenCalledTimes(1)
+      done()
+    } catch (err) {
+      done(err)
+    }
+  })
+}
