8216581218214580fe8ab69301b0d7d42f04638a
Kiran Varma <varma.ap@gmail.com>
2023-10-27 23:29:49 +0000

    feat: initial changes

---
+++ packages/webapp/src/routes/land/applicant-details-is-agent.js
@@ -0,0 +1,50 @@
+import constants from '../../utils/constants.js'
+import { processRegistrationTask } from '../../utils/helpers.js'
+
+const handlers = {
+  get: async (request, h) => {
+    processRegistrationTask(request, {
+      taskTitle: 'Applicant information',
+      title: 'Add details about the person applying'
+    }, {
+      inProgressUrl: constants.routes.APPLICANT_DETAILS_IS_AGENT
+    })
+
+    const isApplicantAgent = request.yar.get(constants.redisKeys.APPLICANT_DETAILS_IS_AGENT)
+
+    return h.view(constants.views.APPLICANT_DETAILS_IS_AGENT, {
+      isApplicantAgent
+    })
+  },
+  post: async (request, h) => {
+    const { isApplicantAgent } = request.payload
+
+    console.log('isApplicantAgent--->', isApplicantAgent)
+
+    if (isApplicantAgent === 'yes') {
+      request.yar.set(constants.redisKeys.APPLICANT_DETAILS_IS_AGENT, isApplicantAgent)
+      return h.redirect(constants.routes.APPLICANT_DETAILS_IS_AGENT)
+    } else if (isApplicantAgent === 'no') {
+      request.yar.set(constants.redisKeys.APPLICANT_DETAILS_IS_AGENT, isApplicantAgent)
+      return h.redirect(constants.routes.APPLICANT_DETAILS_IS_AGENT)
+    } else {
+      return h.view(constants.views.APPLICANT_DETAILS_IS_AGENT, {
+        isApplicantAgent,
+        err: [{
+          text: 'Select yes if you want are an agent acting on behalf of a client',
+          href: '#isApplicantAgent'
+        }]
+      })
+    }
+  }
+}
+
+export default [{
+  method: 'GET',
+  path: constants.routes.APPLICANT_DETAILS_IS_AGENT,
+  handler: handlers.get
+}, {
+  method: 'POST',
+  path: constants.routes.APPLICANT_DETAILS_IS_AGENT,
+  handler: handlers.post
+}]
