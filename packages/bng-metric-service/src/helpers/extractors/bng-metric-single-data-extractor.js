6206bba389c96c899fd316ef91e5242a87b1ca62
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2023-10-31 13:09:27 +0000

    Update unit tests for extra data extracted from metric and update mock application data

--- packages/bng-metric-service/src/helpers/extractors/bng-metric-single-data-extractor.js
+++ packages/bng-metric-service/src/helpers/extractors/bng-metric-single-data-extractor.js
@@ -140,7 +140,7 @@ class BngMetricSingleDataExtractor {
   #removeUnwantedRows = (data, extractionConfiguration) => {
     const { rowsToBeRemovedTemplate } = extractionConfiguration
 
-    rowsToBeRemovedTemplate.forEach(rowTemplate => {
+    rowsToBeRemovedTemplate?.forEach(rowTemplate => {
       data = data.filter(row => {
         const keys = Object.keys(row)
         return !(keys.length === rowTemplate.length && keys.every(key => rowTemplate.includes(key)))
