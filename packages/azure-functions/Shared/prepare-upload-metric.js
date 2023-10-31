3733c6008abe05f23b83b023b2cd5546d7874cf6
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-10-31 15:38:51 +0530

    WIP: Managing duplicate azure function's code

--- packages/azure-functions/Shared/prepare-upload-metric.js
+++ packages/azure-functions/Shared/prepare-upload-metric.js
@@ -1,54 +1,54 @@
-import bngMetricService from "@defra/bng-metric-service";
-import { blobStorageConnector } from "@defra/bng-connectors-lib";
-import buildSignalRMessage from "./build-signalr-message.js";
-import processMetric from "./process-metric.js";
+import bngMetricService from '@defra/bng-metric-service'
+import { blobStorageConnector } from '@defra/bng-connectors-lib'
+import buildSignalRMessage from './build-signalr-message.js'
+import processMetric from './process-metric.js'
 
 export default async (context, config) => {
   const {
     extractionConfiguration,
     validationConfiguration,
-    extractMetricContent,
-  } = bngMetricService;
-  const { fileConfig, containerName, signalRMessageConfig } = config;
-  let signalRMessageArgs, metricData;
+    extractMetricContent
+  } = bngMetricService
+  const { fileConfig, containerName, signalRMessageConfig } = config
+  let signalRMessageArgs, metricData
   try {
     const blobConfig = {
       blobName: fileConfig.fileLocation,
-      containerName,
-    };
+      containerName
+    }
     const response = await blobStorageConnector.downloadStreamIfExists(
       context,
       blobConfig
-    );
+    )
     if (response) {
-      const docStream = response.readableStreamBody;
+      const docStream = response.readableStreamBody
       const metricExtractionConfig = {
         extractionConfiguration: {
           start: extractionConfiguration.startExtractionConfig,
-          ...extractionConfiguration["v4.0"],
+          ...extractionConfiguration['v4.0']
         },
-        validationConfiguration,
-      };
+        validationConfiguration
+      }
       metricData = await extractMetricContent(
         docStream,
         metricExtractionConfig
-      );
+      )
     } else {
-      throw new Error(`Unable to retrieve blob ${fileConfig.fileLocation}`);
+      throw new Error(`Unable to retrieve blob ${fileConfig.fileLocation}`)
     }
 
     signalRMessageArgs = [
       {
         location: fileConfig.fileLocation,
-        metricData: processMetric(metricData),
-      },
-    ];
+        metricData: processMetric(metricData)
+      }
+    ]
   } catch (err) {
-    context.log.error(err);
-    signalRMessageArgs = [{ errorCode: err.code }];
+    context.log.error(err)
+    signalRMessageArgs = [{ errorCode: err.code }]
   } finally {
     context.bindings.signalRMessages = [
-      buildSignalRMessage(signalRMessageConfig, signalRMessageArgs),
-    ];
+      buildSignalRMessage(signalRMessageConfig, signalRMessageArgs)
+    ]
   }
-};
+}
