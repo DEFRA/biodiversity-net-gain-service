f3959117eba3c44789443bfaa558159f944e9ffd
Tedd Mason <teddmason@gmail.com>
2023-11-03 16:03:24 +0000

    address boilerplate

--- packages/webapp/src/routes/land/non-uk-address.js
+++ packages/webapp/src/routes/land/non-uk-address.js
@@ -2,7 +2,26 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    return h.view(constants.views.NON_UK_ADDRESS)
+    const isApplicantAgent = request.yar.get(constants.redisKeys.APPLICANT_DETAILS_IS_AGENT)
+    const nonUkAddress = request.yar.get(constants.redisKeys.NON_UK_ADDRESS)
+    return h.view(constants.views.NON_UK_ADDRESS, {
+      isApplicantAgent,
+      nonUkAddress
+    })
+  },
+  post: async (request, h) => {
+    const { addressLine1, addressLine2, addressLine3, town, postcode, country } = request.payload
+    const nonUkAddress = {
+      addressLine1,
+      addressLine2,
+      addressLine3,
+      town,
+      postcode,
+      country
+    }
+    request.yar.set(constants.redisKeys.NON_UK_ADDRESS, nonUkAddress)
+    // TODO: Next route not yet implemented
+    return h.redirect(constants.routes.NON_UK_ADDRESS)
   }
 }
 
@@ -10,4 +29,8 @@ export default [{
   method: 'GET',
   path: constants.routes.NON_UK_ADDRESS,
   handler: handlers.get
+}, {
+  method: 'POST',
+  path: constants.routes.NON_UK_ADDRESS,
+  handler: handlers.post
 }]
--- packages/webapp/src/routes/land/uk-address.js
+++ packages/webapp/src/routes/land/uk-address.js
@@ -2,7 +2,25 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    return h.view(constants.views.UK_ADDRESS)
+    const isApplicantAgent = request.yar.get(constants.redisKeys.APPLICANT_DETAILS_IS_AGENT)
+    const ukAddress = request.yar.get(constants.redisKeys.UK_ADDRESS)
+    return h.view(constants.views.UK_ADDRESS, {
+      isApplicantAgent,
+      ukAddress
+    })
+  },
+  post: async (request, h) => {
+    const { addressLine1, addressLine2, town, county, postcode } = request.payload
+    const ukAddress = {
+      addressLine1,
+      addressLine2,
+      town,
+      county,
+      postcode
+    }
+    request.yar.set(constants.redisKeys.UK_ADDRESS, ukAddress)
+    // TODO: Next route not implemented
+    return h.redirect(constants.routes.UK_ADDRESS)
   }
 }
 
@@ -10,4 +28,8 @@ export default [{
   method: 'GET',
   path: constants.routes.UK_ADDRESS,
   handler: handlers.get
+}, {
+  method: 'POST',
+  path: constants.routes.UK_ADDRESS,
+  handler: handlers.post
 }]
