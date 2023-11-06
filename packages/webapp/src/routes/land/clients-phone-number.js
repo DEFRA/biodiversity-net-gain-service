262a25bedafadef945bedfcc9b3a2d6c30d8bd41
Tedd Mason <teddmason@gmail.com>
2023-11-06 14:33:32 +0000

    client phone number

--- packages/webapp/src/routes/land/clients-phone-number.js
+++ packages/webapp/src/routes/land/clients-phone-number.js
@@ -2,10 +2,31 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: async (request, h) => {
-    return h.view(constants.views.CLIENTS_PHONE_NUMBER)
+    const phone = request.yar.get(constants.redisKeys.CLIENTS_PHONE_NUMBER)
+    return h.view(constants.views.CLIENTS_PHONE_NUMBER, {
+      phone
+    })
   },
   post: async (request, h) => {
+    const phone = request.payload.phone
+    const error = validatePhone(phone)
+    if (error) {
+      return h.view(constants.views.CLIENTS_PHONE_NUMBER, {
+        err: error,
+        phone
+      })
+    }
+    request.yar.set(constants.redisKeys.CLIENTS_PHONE_NUMBER, phone)
+    return h.redirect(constants.routes.UPLOAD_WRITTEN_AUTHORISATION)
+  }
+}
 
+const validatePhone = (phone) => {
+  if (!phone) {
+    return [{
+      text: 'Enter a phone number',
+      href: '#phone'
+    }]
   }
 }
 
@@ -15,6 +36,6 @@ export default [{
   handler: handlers.get
 }, {
   method: 'POST',
-  path: constants.routes.CLIENTS_EMAIL_ADDRESS,
+  path: constants.routes.CLIENTS_PHONE_NUMBER,
   handler: handlers.post
 }]
