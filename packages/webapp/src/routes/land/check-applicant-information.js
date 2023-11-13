cbde0fa5192f6a49727ea4e6aa66cbfc55d26987
Tedd <teddmason@gmail.com>
2023-11-13 12:59:38 +0000

    Sorting out task list, check and submit, and clearing out old routes (#503)
    tested locally  https://eaflood.atlassian.net/browse/BNGP-3900?focusedCommentId=549591

--- packages/webapp/src/routes/land/check-applicant-information.js
+++ packages/webapp/src/routes/land/check-applicant-information.js
@@ -1,7 +1,15 @@
 import constants from '../../utils/constants.js'
+import { processRegistrationTask } from '../../utils/helpers.js'
 
 const handlers = {
   get: async (request, h) => {
+    processRegistrationTask(request, {
+      taskTitle: 'Applicant information',
+      title: 'Add details about the applicant'
+    }, {
+      status: constants.COMPLETE_REGISTRATION_TASK_STATUS,
+      inProgressUrl: constants.routes.CHECK_APPLICANT_INFORMATION
+    })
     return h.view(constants.views.CHECK_APPLICANT_INFORMATION)
   }
 }
