3bde647d83465a4339e605573fc5f19c0dd1c765
ajjawalk <ajinkya.jawalkar@capgemini.com>
2023-11-20 17:09:55 +0000

    WIP: sonar issue code duplication

--- packages/webapp/src/routes/credits/credits-metric-upload.js
+++ packages/webapp/src/routes/credits/credits-metric-upload.js
@@ -1,7 +1,7 @@
 import { logger } from 'defra-logging-facade'
 import { deleteBlobFromContainers } from '../../utils/azure-storage.js'
 import { buildConfig } from '../../utils/build-upload-config.js'
-import constants from '../../credits/constants.js'
+import creditsConstants from '../../credits/constants.js'
 import { uploadFile } from '../../utils/upload.js'
 import { getMaximumFileSizeExceededView, getMetricFileValidationErrors } from '../../utils/helpers.js'
 import { ThreatScreeningError, MalwareDetectedError } from '@defra/bng-errors-lib'
@@ -12,62 +12,62 @@ const processSuccessfulUpload = async (result, request, h) => {
   const creditsValidationError = getMetricFileValidationErrors(result.postProcess.metricData?.validation)
   if (creditsValidationError) {
     await deleteBlobFromContainers(result.config.blobConfig.blobName)
-    return h.view(constants.views.CREDITS_UPLOAD_METRIC, creditsValidationError)
+    return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, creditsValidationError)
   }
 
-  request.yar.set(constants.redisKeys.CREDITS_METRIC_LOCATION, result.config.blobConfig.blobName)
-  request.yar.set(constants.redisKeys.CREDITS_METRIC_FILE_SIZE, result.fileSize)
-  request.yar.set(constants.redisKeys.CREDITS_METRIC_FILE_TYPE, result.fileType)
-  request.yar.set(constants.redisKeys.CREDITS_METRIC_DATA, result.postProcess.metricData)
-  request.yar.set(constants.redisKeys.CREDITS_METRIC_FILE_NAME, result.filename)
+  request.yar.set(creditsConstants.redisKeys.CREDITS_METRIC_LOCATION, result.config.blobConfig.blobName)
+  request.yar.set(creditsConstants.redisKeys.CREDITS_METRIC_FILE_SIZE, result.fileSize)
+  request.yar.set(creditsConstants.redisKeys.CREDITS_METRIC_FILE_TYPE, result.fileType)
+  request.yar.set(creditsConstants.redisKeys.CREDITS_METRIC_DATA, result.postProcess.metricData)
+  request.yar.set(creditsConstants.redisKeys.CREDITS_METRIC_FILE_NAME, result.filename)
   logger.log(`${new Date().toUTCString()} Received metric data for ${result.config.blobConfig.blobName.substring(result.config.blobConfig.blobName.lastIndexOf('/') + 1)}`)
-  return h.redirect(constants.routes.CREDITS_CHECK_UPLOAD_METRIC)
+  return h.redirect(creditsConstants.routes.CREDITS_CHECK_UPLOAD_METRIC)
 }
 
 const processErrorUpload = (err, h) => {
   switch (err.message) {
-    case constants.uploadErrors.emptyFile:
-      return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+    case creditsConstants.uploadErrors.emptyFile:
+      return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
         err: [{
           text: 'The selected file is empty',
           href: UPLOAD_CREDIT_METRIC_ID
         }]
       })
-    case constants.uploadErrors.noFile:
-      return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+    case creditsConstants.uploadErrors.noFile:
+      return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
         err: [{
           text: 'Select a Biodiversity Metric',
           href: UPLOAD_CREDIT_METRIC_ID
         }]
       })
-    case constants.uploadErrors.unsupportedFileExt:
-      return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+    case creditsConstants.uploadErrors.unsupportedFileExt:
+      return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
         err: [{
           text: 'The selected file must be an XLSM or XLSX',
           href: UPLOAD_CREDIT_METRIC_ID
         }]
       })
-    case constants.uploadErrors.maximumFileSizeExceeded:
+    case creditsConstants.uploadErrors.maximumFileSizeExceeded:
       return maximumFileSizeExceeded(h)
     default:
       if (err instanceof ThreatScreeningError) {
-        return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+        return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
           err: [{
-            text: constants.uploadErrors.malwareScanFailed,
+            text: creditsConstants.uploadErrors.malwareScanFailed,
             href: UPLOAD_CREDIT_METRIC_ID
           }]
         })
       } else if (err instanceof MalwareDetectedError) {
-        return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+        return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
           err: [{
-            text: constants.uploadErrors.threatDetected,
+            text: creditsConstants.uploadErrors.threatDetected,
             href: UPLOAD_CREDIT_METRIC_ID
           }]
         })
       } else {
-        return h.view(constants.views.CREDITS_UPLOAD_METRIC, {
+        return h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC, {
           err: [{
-            text: constants.uploadErrors.uploadFailure,
+            text: creditsConstants.uploadErrors.uploadFailure,
             href: UPLOAD_CREDIT_METRIC_ID
           }]
         })
@@ -76,14 +76,14 @@ const processErrorUpload = (err, h) => {
 }
 
 const handlers = {
-  get: async (_request, h) => h.view(constants.views.CREDITS_UPLOAD_METRIC),
+  get: async (_request, h) => h.view(creditsConstants.views.CREDITS_UPLOAD_METRIC),
   post: async (request, h) => {
     // Get upload config object from common code
     const creditsUploadConfig = buildConfig({
       sessionId: request.yar.id,
-      fileExt: constants.metricFileExt,
+      fileExt: creditsConstants.metricFileExt,
       maxFileSize: parseInt(process.env.MAX_METRIC_UPLOAD_MB) * 1024 * 1024,
-      uploadType: constants.uploadTypes.CREDITS_METRIC_UPLOAD_TYPE,
+      uploadType: creditsConstants.uploadTypes.CREDITS_METRIC_UPLOAD_TYPE,
       postProcess: true
     })
 
@@ -99,12 +99,12 @@ const handlers = {
 
 export default [{
   method: 'GET',
-  path: constants.routes.CREDITS_UPLOAD_METRIC,
+  path: creditsConstants.routes.CREDITS_UPLOAD_METRIC,
   handler: handlers.get
 },
 {
   method: 'POST',
-  path: constants.routes.CREDITS_UPLOAD_METRIC,
+  path: creditsConstants.routes.CREDITS_UPLOAD_METRIC,
   config: {
     handler: handlers.post,
     payload: {
@@ -131,6 +131,6 @@ const maximumFileSizeExceeded = h => {
     h,
     href: UPLOAD_CREDIT_METRIC_ID,
     maximumFileSize: process.env.MAX_METRIC_UPLOAD_MB,
-    view: constants.views.CREDITS_UPLOAD_METRIC
+    view: creditsConstants.views.CREDITS_UPLOAD_METRIC
   })
 }
