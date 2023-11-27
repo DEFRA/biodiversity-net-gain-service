41ea41407fd5131b0cbed1a89141319ba6746c73
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-11-27 10:09:27 +0000

    Revert "Use common test file"
    This reverts commit befb20b4e690573639a026289900e9b7e7833ed6.

--- packages/webapp/src/routes/__tests__/land/agent-acting-for-client.spec.js
+++ packages/webapp/src/routes/__tests__/land/agent-acting-for-client.spec.js
@@ -1,23 +1,9 @@
 import { submitGetRequest } from '../helpers/server.js'
 import constants from '../../../utils/constants.js'
-import developerConstants from '../../../utils/developer-constants.js'
 import lojConstants from '../../../utils/loj-constants.js'
-const journeys = [
-  {
-    url: lojConstants.commonRoutes.AGENT_ACTING_FOR_CLIENT,
-    expectedYesResult: constants.routes.CHECK_DEFRA_ACCOUNT_DETAILS,
-    expectedNoResult: constants.routes.APPLICATION_BY_INDIVIDUAL_OR_ORGANISATION,
-    expectedFailureResult: constants.views.AGENT_ACTING_FOR_CLIENT
-  },
-  {
-    url: developerConstants.commonRoutes.DEVELOPER_AGENT_ACTING_FOR_CLIENT,
-    expectedYesResult: constants.routes.CHECK_DEFRA_ACCOUNT_DETAILS,
-    expectedNoResult: constants.routes.APPLICATION_BY_INDIVIDUAL_OR_ORGANISATION,
-    expectedFailureResult: constants.views.AGENT_ACTING_FOR_CLIENT
-  }
-]
+const url = `/${lojConstants.commonRoutes.AGENT_ACTING_FOR_CLIENT}`
 
-describe('agent-acting-for-client', () => {
+describe(url, () => {
   let viewResult
   let h
   let redisMap
@@ -41,51 +27,48 @@ describe('agent-acting-for-client', () => {
     isApplicantAgent = require('../../applicant/agent-acting-for-client.js')
   })
 
-  journeys.forEach(journey => {
-    const url = journey.url
-    describe('GET', () => {
-      it(`should render the ${url.substring(1)} view`, async () => {
-        await submitGetRequest({ url })
-      })
+  describe('GET', () => {
+    it(`should render the ${url.substring(1)} view`, async () => {
+      await submitGetRequest({ url })
+    })
+  })
+
+  describe('POST', () => {
+    it('Should continue journey to applicant-details-confirm if yes is chosen', async () => {
+      const request = {
+        path: url,
+        yar: redisMap,
+        payload: { isApplicantAgent: 'yes' }
+      }
+
+      await isApplicantAgent.default[1].handler(request, h)
+
+      expect(viewResult).toEqual(constants.routes.CHECK_DEFRA_ACCOUNT_DETAILS)
     })
 
-    describe('POST', () => {
-      it('Should continue journey to applicant-details-confirm if yes is chosen', async () => {
-        const request = {
-          path: url,
-          yar: redisMap,
-          payload: { isApplicantAgent: 'yes' }
-        }
+    it('Should continue journey to applying-individual-organisation if no is chosen', async () => {
+      const request = {
+        path: url,
+        yar: redisMap,
+        payload: { isApplicantAgent: 'no' }
+      }
 
-        await isApplicantAgent.default[1].handler(request, h)
+      await isApplicantAgent.default[1].handler(request, h)
 
-        expect(viewResult).toEqual(journey.expectedYesResult)
-      })
+      expect(viewResult).toEqual(constants.routes.APPLICATION_BY_INDIVIDUAL_OR_ORGANISATION)
+    })
 
-      it('Should continue journey to applying-individual-organisation if no is chosen', async () => {
-        const request = {
-          path: url,
-          yar: redisMap,
-          payload: { isApplicantAgent: 'no' }
-        }
+    it('Should fail journey if no answer', async () => {
+      const request = {
+        path: url,
+        yar: redisMap,
+        payload: { }
+      }
 
-        await isApplicantAgent.default[1].handler(request, h)
+      await isApplicantAgent.default[1].handler(request, h)
 
-        expect(viewResult).toEqual(journey.expectedNoResult)
-      })
-
-      it('Should fail journey if no answer', async () => {
-        const request = {
-          path: url,
-          yar: redisMap,
-          payload: { }
-        }
-
-        await isApplicantAgent.default[1].handler(request, h)
-
-        expect(viewResult).toEqual(journey.expectedFailureResult)
-        expect(resultContext.err[0]).toEqual({ text: 'Select yes if you are an agent acting on behalf of a client', href: '#isApplicantAgent' })
-      })
+      expect(viewResult).toEqual(constants.views.AGENT_ACTING_FOR_CLIENT)
+      expect(resultContext.err[0]).toEqual({ text: 'Select yes if you are an agent acting on behalf of a client', href: '#isApplicantAgent' })
     })
   })
 })
