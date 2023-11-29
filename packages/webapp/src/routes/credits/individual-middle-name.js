7a0382adb1ae2643481c3a248a137a717691f788
Tedd Mason <teddmason@gmail.com>
2023-11-29 08:40:44 +0000

    WIP

--- packages/webapp/src/routes/credits/individual-middle-name.js
+++ packages/webapp/src/routes/credits/individual-middle-name.js
@@ -6,4 +6,22 @@ const individualMiddleName = {
   handler: (_request, h) => h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME)
 }
 
-export default individualMiddleName
+export default [{
+  method: 'GET',
+  path: constants.routes.CREDITS_INDIVIDUAL_MIDDLE_NAME,
+  handler: (request, h) => {
+    const middleName = request.yar.get(constants.redisKeys.CREDITS_INDIVIDUAL_MIDDLE_NAME)
+    const middleNameOption = middleName && 'yes'
+    return h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME, { 
+      middleName, 
+      middleNameOption 
+    })
+  }
+}, {
+  method: 'POST',
+  path: constants.routes.CREDITS_INDIVIDUAL_MIDDLE_NAME,
+  handler: (request, h) => {
+    const { middleName, middleNameOption} = request.payload
+    // no copydeck so not sure what edge case and errors are for post route..., moving ticket to blocked.
+  }
+}]
