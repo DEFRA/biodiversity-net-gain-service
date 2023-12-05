d98bd06923423745aa1828b54cfcba2ec380afe7
Tedd Mason <teddmason@gmail.com>
2023-12-05 14:20:23 +0000

    WIP broken

--- packages/webapp/src/routes/credits/individual-dob.js
+++ packages/webapp/src/routes/credits/individual-dob.js
@@ -1,5 +1,9 @@
 import constants from '../../utils/constants.js'
-import { dateClasses } from '../../utils/helpers.js'
+import { 
+  dateClasses,
+  validateDate,
+  validateAndParseISOString
+} from '../../utils/helpers.js'
 
 const individualDob = {
   method: 'GET',
@@ -9,4 +13,25 @@ const individualDob = {
   }
 }
 
-export default individualDob
+export default [{
+  method: 'GET',
+  path: constants.routes.CREDITS_INDIVIDUAL_DOB,
+  handler: (_request, h) => {
+    return h.view(constants.views.CREDITS_INDIVIDUAL_DOB, { dateClasses })
+  }
+}, {
+  method: 'POST',
+  path: constants.routes.CREDITS_INDIVIDUAL_DOB,
+  handler: (request, h) => {
+    const { day, month, year, dateAsISOString, context } = validateDate(
+      request.payload, 
+      'dob', 
+      'date of birth', 
+      'Date of birth',
+      false
+      )
+    console.log('errp')
+
+
+  }
+}]
--- packages/webapp/src/routes/credits/individual-middle-name.js
+++ packages/webapp/src/routes/credits/individual-middle-name.js
@@ -10,18 +10,38 @@ export default [{
   method: 'GET',
   path: constants.routes.CREDITS_INDIVIDUAL_MIDDLE_NAME,
   handler: (request, h) => {
-    const middleName = request.yar.get(constants.redisKeys.CREDITS_INDIVIDUAL_MIDDLE_NAME)
-    const middleNameOption = middleName && 'yes'
-    return h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME, { 
-      middleName, 
-      middleNameOption 
-    })
+    const values = request.yar.get(constants.redisKeys.CREDITS_INDIVIDUAL_MIDDLE_NAME)
+    return h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME, values)
   }
 }, {
   method: 'POST',
   path: constants.routes.CREDITS_INDIVIDUAL_MIDDLE_NAME,
   handler: (request, h) => {
     const { middleName, middleNameOption} = request.payload
-    // no copydeck so not sure what edge case and errors are for post route..., moving ticket to blocked.
+    if (!middleNameOption) {
+      return h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME, {
+        middleName,
+        middleNameOption,
+        err: [{
+          text: 'Select yes and enter your middle name',
+          href: '#middle-name-yes'
+        }]
+      })
+    }
+    if (middleNameOption === 'yes' && middleName.length === 0) {
+      return h.view(constants.views.CREDITS_INDIVIDUAL_MIDDLE_NAME, {
+        middleName,
+        middleNameOption,
+        err: [{
+          text: 'Enter your middle name',
+          href: '#middle-name'
+        }]
+      })
+    }
+    request.yar.set(constants.redisKeys.CREDITS_INDIVIDUAL_MIDDLE_NAME, {
+      middleNameOption,
+      middleName
+    })
+    return h.redirect(request.yar.get(constants.redisKeys.REFERER, true) || constants.routes.CREDITS_INDIVIDUAL_DOB)
   }
 }]
