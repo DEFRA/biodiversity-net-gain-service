8f2cdcb55033af56f5507aa43675756bf1deb5cf
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2023-12-07 18:06:37 +0000

    Add fallback user

--- packages/database-version-control/src/__tests__/migrator-spec.js
+++ packages/database-version-control/src/__tests__/migrator-spec.js
@@ -15,6 +15,18 @@ describe('The database version control migrator', () => {
     await migrator.down({ to: 0 })
     expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
   }, 30000)
+  it('should initialise correctly and be able to run and rollback all pending migrations programmatically when a fallback runtime user is configured', async () => {
+    process.env.POSTGRES_BNG_FALLBACK_USER_PASSWORD = 'mock-password'
+    const migrations = fs.readdirSync(migrationsDirectoryPath)
+    expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
+    // As prepare-data-migrations.js runs during installation and is excluded from test coverage, check that the list of migrations
+    // includes expected data migrations.
+    expect(migrations.includes('2023.02.21T10.42.51.populate-nation-boundary-27700-table.sql')).toBeTruthy()
+    await migrator.up()
+    expect((await migrator.pending()).map(m => m.name)).toEqual([])
+    await migrator.down({ to: 0 })
+    expect((await migrator.pending()).map(m => m.name)).toEqual(migrations.slice(0, migrations.length - 1))
+  }, 30000)
   it('should initialise correctly when POSTGRES_SSL_MODE is set to require', async () => {
     jest.isolateModules(async () => {
       process.env.POSTGRES_SSL_MODE = 'require'
