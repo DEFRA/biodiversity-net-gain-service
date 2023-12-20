b9225c5665a2dc213d55d5f6a221d9a1be1b8ec0
Usman Shaikh <115730841+usman-j-shaikh@users.noreply.github.com>
2023-12-20 20:28:03 +0530

    BNGP-4124: Multiple landownership upload: allows empty strings on removal  (#538)
    * WIP: Fixing landownership proof's bug
    
    * WIP: Fixing task status issues of Add Land Information
    
    * WIP: Improving test coverage
    
    * WIP: Improving test coverage
    
    * WIP: Minor fixes
    
    * WIP: Rebased base changes

--- packages/webapp/src/routes/land/ownership-proof-list.js
+++ packages/webapp/src/routes/land/ownership-proof-list.js
@@ -30,9 +30,26 @@ const handlers = {
     const landOwnershipProofs = request.yar.get(constants.redisKeys.LAND_OWNERSHIP_PROOFS)
     const landOwnershipsList = (landOwnershipProofs || []).map((currElement, index) => getCustomizedHTML(currElement, index))
 
+    // (Ref:BGNP-4124) Redirecting to the register land task list if there is no one file added.
+    // And to avoid looping back navigation from upload ownership proof.
+    const { referer } = request.headers || ''
     if (landOwnershipsList.length === 0) {
-      return h.redirect(constants.routes.UPLOAD_LAND_OWNERSHIP)
+      processRegistrationTask(request, {
+        taskTitle: 'Land information',
+        title: 'Add land ownership details'
+      }, {
+        status: constants.IN_PROGRESS_REGISTRATION_TASK_STATUS,
+        inProgressUrl: constants.routes.LAND_OWNERSHIP_PROOF_LIST,
+        revert: true
+      })
+
+      if (referer && referer.indexOf(constants.routes.LAND_OWNERSHIP_PROOF_LIST) > -1) {
+        return h.redirect(constants.routes.REGISTER_LAND_TASK_LIST)
+      } else {
+        return h.redirect(constants.routes.UPLOAD_LAND_OWNERSHIP)
+      }
     }
+
     return h.view(constants.views.LAND_OWNERSHIP_PROOF_LIST, {
       landOwnershipsList,
       landOwnershipProofs
