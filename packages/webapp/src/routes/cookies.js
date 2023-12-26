ad1919f907a9c51d6037963d9aebbf52481ba058
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2023-12-26 21:25:08 +0530

    Modified route of cookies

--- packages/webapp/src/routes/cookies.js
+++ packages/webapp/src/routes/cookies.js
@@ -3,6 +3,9 @@ import constants from '../utils/constants.js'
 const cookies = {
   method: 'GET',
   path: constants.routes.COOKIES,
+  options: {
+    auth: false
+  },
   handler: async (request, h) => {
     const requestHeadersReferer = request.headers.referer ? encodeURI(request.headers.referer) : ''
 
