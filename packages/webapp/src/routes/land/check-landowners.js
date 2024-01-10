00dccd87dafd761c7ae56c1127d5599bc5d3eabd
Sooraj Vijayan <sooraj.vijayan@defra.gov.uk>
2024-01-10 14:49:03 +0000

    feature/BNGP-4312-legal-agreement-text-wrapping-issue

--- packages/webapp/src/routes/land/check-landowners.js
+++ packages/webapp/src/routes/land/check-landowners.js
@@ -1,26 +1,48 @@
 import constants from '../../utils/constants.js'
 import { processRegistrationTask, getLegalAgreementDocumentType } from '../../utils/helpers.js'
 
-const getTableData = (landOwnerConservationConvenants) => {
-  const rows = landOwnerConservationConvenants.map((item, index) => {
-    const name = item.type === constants.landownerTypes.INDIVIDUAL
-      ? `${item.firstName} ${item.middleNames ? item.middleNames + ' ' : ''}${item.lastName})`
-      : item.organisationName
+const getCustomizedHTML = (item, index) => {
+  if (item.type === constants.landownerTypes.INDIVIDUAL) {
+    const textToDisplay = `${item.firstName} ${item.middleNames ? item.middleNames + ' ' : ''}${item.lastName} (${item.emailAddress})`
+    return {
+      key: {
+        text: textToDisplay,
+        classes: 'govuk-summary-list govuk-!-font-weight-regular hmrc-list-with-actions hmrc-list-with-actions--short'
+      },
+      actions: {
+        items: [{
+          href: `${constants.routes.ADD_LANDOWNER_INDIVIDUAL_CONSERVATION_COVENANT}?id=${index}`,
+          text: 'Change'
+        }, {
+          href: `${constants.routes.REMOVE_LANDOWNER}?id=${index}`,
+          text: 'Remove'
+        }],
+        classes: 'govuk-summary-list__key govuk-!-font-weight-regular hmrc-summary-list__key'
+      },
+      class: 'govuk-summary-list__row'
+    }
+  } else {
+    const textToDisplay = item.organisationName
 
-    const actionsHtml = `
-      <a href="${constants.routes.ADD_LANDOWNER_INDIVIDUAL_CONSERVATION_COVENANT}?id=${index}" class="govuk-link" style="border-right: 1px solid #000; padding-right: 8px; margin-right: 8px;">Change</a>
-      <a href="${constants.routes.REMOVE_LANDOWNER}?id=${index}" class="govuk-link" style="padding-left: 8px;">Remove</a>
-    `
-
-    return [
-      { html: name },
-      { html: actionsHtml }
-    ]
-  })
-
-  return { rows }
+    return {
+      key: {
+        text: textToDisplay,
+        classes: 'govuk-summary-list govuk-!-font-weight-regular hmrc-list-with-actions hmrc-list-with-actions--short'
+      },
+      actions: {
+        items: [{
+          href: `${constants.routes.ADD_LANDOWNER_ORGANISATION_CONSERVATION_COVENANT}?id=${index}`,
+          text: 'Change'
+        }, {
+          href: `${constants.routes.REMOVE_LANDOWNER}?id=${index}`,
+          text: 'Remove'
+        }],
+        classes: 'govuk-summary-list hmrc-list-with-actions hmrc-list-with-actions--short'
+      },
+      class: 'govuk-summary-list__row'
+    }
+  }
 }
-
 const handlers = {
   get: async (request, h) => {
     processRegistrationTask(request, {
@@ -31,30 +53,34 @@ const handlers = {
     })
 
     const landOwnerConservationConvenants = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_LANDOWNER_CONSERVATION_CONVENANTS)
-    const tableData = getTableData(landOwnerConservationConvenants)
+    if (landOwnerConservationConvenants.length === 0) {
+      return h.redirect(constants.routes.NEED_ADD_ALL_LANDOWNERS_CONSERVATION_COVENANT)
+    }
+    const landOwnerConservationConvenantsWithAction = landOwnerConservationConvenants.map((currElement, index) => getCustomizedHTML(currElement, index))
+
+    const { ADD_LANDOWNER_INDIVIDUAL_CONSERVATION_COVENANT, REMOVE_LANDOWNER } = constants.routes
+    const legalAgreementType = getLegalAgreementDocumentType(
+      request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE))?.toLowerCase()
 
     return h.view(constants.views.CHECK_LANDOWNERS, {
-      tableData,
+      landOwnerConservationConvenantsWithAction,
       landOwnerConservationConvenants,
-      legalAgreementType: getLegalAgreementDocumentType(
-        request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
-      )?.toLowerCase(),
-      routes: constants.routes
+      legalAgreementType,
+      routes: { ADD_LANDOWNER_INDIVIDUAL_CONSERVATION_COVENANT, REMOVE_LANDOWNER }
     })
   },
   post: async (request, h) => {
     const { addAnotherLandowner } = request.payload
     const legalAgreementType = getLegalAgreementDocumentType(
-      request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE)
-    )?.toLowerCase()
+      request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_DOCUMENT_TYPE))?.toLowerCase()
     const landOwnerConservationConvenants = request.yar.get(constants.redisKeys.LEGAL_AGREEMENT_LANDOWNER_CONSERVATION_CONVENANTS)
-
     if (!addAnotherLandowner) {
-      const tableData = getTableData(landOwnerConservationConvenants)
+      const landOwnerConservationConvenantsWithAction = landOwnerConservationConvenants.map((currElement, index) => getCustomizedHTML(currElement, index))
+
       return h.view(constants.views.CHECK_LANDOWNERS, {
-        tableData,
-        legalAgreementType,
         landOwnerConservationConvenants,
+        landOwnerConservationConvenantsWithAction,
+        legalAgreementType,
         routes: constants.routes,
         err: [{
           text: 'Select yes if you have added all landowners or leaseholders',
