c07abb9d2ea66220d16889eac50ba39f972c59d2
Sooraj Vijayan <sooraj.vijayan@defra.gov.uk>
2024-01-15 00:47:27 +0000

    feature/BNGP-4369-ITHC-csp-fix

--- packages/webapp/src/server.js
+++ packages/webapp/src/server.js
@@ -1,4 +1,5 @@
 import Hapi from '@hapi/hapi'
+import crypto from 'crypto'
 import Inert from '@hapi/inert'
 import auth from './plugins/auth.js'
 import views from './plugins/views.js'
@@ -46,7 +47,25 @@ const init = async server => {
   await server.register(Blipp)
   await server.register(onPreHandler)
   await server.register(onPostHandler)
+  server.ext('onPreResponse', (request, h) => {
+    // sha refer https://github.com/alphagov/govuk-frontend/issues/1657
+    const scriptHash = '+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='
+    const nonce = crypto.randomBytes(16).toString('base64')
+    if (!request.response.source.context) {
+      request.response.source.context = {}
+    }
+    request.response.source.context.nonce = nonce
+    const response = request.response
+    if (response.header) {
+      const csp = 'default-src \'self\'; ' +
+                    `script-src 'self' 'nonce-${nonce}' 'sha256-${scriptHash}'; ` +
+                    `style-src 'self' 'nonce-${nonce}'; ` +
+                    'img-src \'self\';'
 
+      response.header('Content-Security-Policy', csp)
+    }
+    return h.continue
+  })
   // Override the default keep alive timeout if required.
   // This is important for file uploads in containerised development environments.
   server.listener.keepAliveTimeout = parseInt(KEEP_ALIVE_TIMEOUT_MS) || server.listener.keepAliveTimeout
