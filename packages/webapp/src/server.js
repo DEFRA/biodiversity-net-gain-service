8d2dd74a60f640d5a72938623cefb11fcacccc29
Sooraj Vijayan <sooraj.vijayan@defra.gov.uk>
2024-01-16 23:42:59 +0000

    feature/BNGP-4369-ITHC-csp-fix

--- packages/webapp/src/server.js
+++ packages/webapp/src/server.js
@@ -49,31 +49,29 @@ const init = async server => {
   await server.register(onPostHandler)
   server.ext('onPreResponse', (request, h) => {
     const response = request.response
-
     // Check if the response is a view and source is an object
     if (response.variety === 'view' && response.source && typeof response.source === 'object') {
       const nonce = crypto.randomBytes(16).toString('base64')
-
       // Ensure the context object exists for the view
       if (!response.source.context) {
         response.source.context = {}
       }
-
       // Set the nonce in the context
       response.source.context.nonce = nonce
-
       // Set CSP header if response allows setting headers
       if (typeof response.header === 'function') {
         const scriptHash = '+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='
+        // This is a hash of the inline script in GDS template. It is added to the CSP to except the in-line
         const csp = 'default-src \'self\'; ' +
-                        `script-src 'self' 'nonce-${nonce}' 'sha256-${scriptHash}'; ` +
-                        `style-src 'self' 'nonce-${nonce}'; ` +
-                        'img-src \'self\';'
-
+                  `script-src 'self' 'unsafe-inline' 'nonce-${nonce}' 'sha256-${scriptHash}' 'www.googletagmanager.com'; ` +
+                  `style-src 'self' 'nonce-${nonce}'; ` +
+                  'img-src \'self\'; ' +
+                  'font-src \'self\' \'fonts.gstatic.com\' \'data:\'; ' +
+                  'connect-src \'self\' \'*.google-analytics.com\'; ' +
+                  'frame-ancestors \'none\''
         response.header('Content-Security-Policy', csp)
       }
     }
-
     return h.continue
   })
 
