cf1714b354e0b3a3bacd76c219f49479632933ae
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2024-01-25 20:47:50 +0530

    Minor changes for logger

--- packages/webapp/src/routes/credits/credits-download-metric.js
+++ packages/webapp/src/routes/credits/credits-download-metric.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import { blobStorageConnector } from '@defra/bng-connectors-lib'
 import constants from '../../credits/constants.js'
-import { logger } from 'defra-logging-facade'
+// import { logger } from 'defra-logging-facade'
 
 const downloadMetricFile = async (request, h) => {
   const blobName = request.yar.get(constants.redisKeys.CREDITS_METRIC_LOCATION)
@@ -11,7 +11,7 @@ const downloadMetricFile = async (request, h) => {
     containerName: constants.BLOB_STORAGE_CONTAINER
   }
 
-  const buffer = await blobStorageConnector.downloadToBufferIfExists(logger, config)
+  const buffer = await blobStorageConnector.downloadToBufferIfExists(request.logger, config)
   return h.response(buffer).header('Content-Disposition', 'attachment; filename= ' + path.basename(blobName))
 }
 
