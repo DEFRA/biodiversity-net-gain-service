60a32758ce32e8754a920b76bc058fc2519b0c96
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2024-01-31 20:40:38 +0530

    Lint fixes

--- packages/webapp/src/routes/__tests__/credits-estimation/credits-tasklist.spec.js
+++ packages/webapp/src/routes/__tests__/credits-estimation/credits-tasklist.spec.js
@@ -4,9 +4,9 @@ import { submitGetRequest } from '../helpers/server.js'
 const url = constants.routes.ESTIMATOR_CREDITS_TASKLIST
 
 describe(url, () => {
-    describe('GET', () => {
-      it(`should render the ${constants.views.ESTIMATOR_CREDITS_TASKLIST} view`, async () => {
-        await submitGetRequest({ url })
-      })
+  describe('GET', () => {
+    it(`should render the ${constants.views.ESTIMATOR_CREDITS_TASKLIST} view`, async () => {
+      await submitGetRequest({ url })
     })
+  })
 })
