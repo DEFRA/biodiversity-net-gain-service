d965f9abc1a6d043d1dcd1444b598d1421ea9873
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2024-01-31 20:27:51 +0530

    Added test files

---
+++ packages/webapp/src/routes/__tests__/credits-estimation/credits-tasklist.spec.js
@@ -0,0 +1,12 @@
+import constants from '../../../credits/constants.js'
+import { submitGetRequest } from '../helpers/server.js'
+
+const url = constants.routes.ESTIMATOR_CREDITS_TASKLIST
+
+describe(url, () => {
+    describe('GET', () => {
+      it(`should render the ${constants.views.ESTIMATOR_CREDITS_TASKLIST} view`, async () => {
+        await submitGetRequest({ url })
+      })
+    })
+})
