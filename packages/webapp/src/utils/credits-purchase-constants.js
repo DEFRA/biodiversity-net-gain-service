db480ed11e9ec608f05bfd525fbff7fe43e0d3cf
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2024-02-06 19:33:12 +0530

    Added common folder and file

---
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -0,0 +1,16 @@
+// Routes constants
+
+// ./Routes constants
+
+// RedisKeys constants
+
+// ./RedisKeys constants
+
+// Other constants
+
+// ./Other constants
+
+export default {
+    routes: {},
+    redisKeys: {}
+}
