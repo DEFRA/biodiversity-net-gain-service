a5a3a631abf3ea19add305456b003e7451fdfff5
Usman Shaikh <usman.jilani-shaikh@capgemini.com>
2024-02-06 19:33:12 +0530

    Added common folder and file

--- packages/webapp/src/utils/credits-purchase-constants.js
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -1,15 +1,14 @@
-const routes = {
-  CREDITS_PURCHASE_CHECK_DEFRA_ACCOUNT_DETAILS: '/credits-purchase/check-defra-account-details',
-  CREDITS_PURCHASE_INDIVIDUAL_ORG: '/credits-purchase/applying-individual-organisation',
-  CREDITS_PURCHASE_TERMS_AND_CONDITIONS: '/credits-purchase/confirm-terms-conditions',
-  CREDITS_PURCHASE_CHECK_YOUR_ANSWERS: '/credits-purchase/credits-check-your-answers',
-  CREDITS_PURCHASE_CREDITS_SELECTION: '/credits-purchase/add-statutory-biodiversity-credits',
-  CREDITS_PURCHASE_CREDITS_COST: '/credits-purchase/estimated-cost-statutory-biodiversity-credits'
-}
+// Routes constants
 
-const views = Object.fromEntries(
-  Object.entries(routes).map(([k, v]) => [k, v.substring(1)])
-)
+// ./Routes constants
+
+// RedisKeys constants
+
+// ./RedisKeys constants
+
+// Other constants
+
+// ./Other constants
 
 const redisKeys = {
   CREDITS_PURCHASE_DEFRA_ACCOUNT_DETAILS_CONFIRMED: 'credits-purchase-defra-account-details-confirmed',
@@ -18,7 +17,6 @@ const redisKeys = {
 }
 
 export default {
-  routes,
-  views,
-  redisKeys
+    routes: {},
+    redisKeys: {}
 }
