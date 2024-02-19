4e6ef0c16c4affd3fea433e6499e57a946443f87
KiranVarma02 <varma.ap@gmail.com>
2024-02-19 12:55:57 +0000

    fix: review comments

--- packages/webapp/src/utils/credits-purchase-constants.js
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -1,6 +1,5 @@
 const routes = {
   CREDITS_PURCHASE_CHECK_DEFRA_ACCOUNT_DETAILS: '/credits-purchase/check-defra-account-details'
-  // REGISTER_CREDIT_PURCHASE_TASK_LIST: '/credit-purchase/register-credit-purchase-task-list'
 }
 
 const views = Object.fromEntries(
@@ -8,7 +7,6 @@ const views = Object.fromEntries(
 )
 
 const redisKeys = {
-  // CREDITS_PURCHASE_APPLYING_INDIVIDIAL_ORGANISATION: 'credits-purchase-applying-individual-organisation',
   CREDITS_PURCHASE_DEFRA_ACCOUNT_DETAILS_CONFIRMED: 'credits-purchase-defra-account-details-confirmed'
 }
 
