474d97762ea7cb941cab146d71b271061c2f185a
KiranVarma02 <varma.ap@gmail.com>
2024-02-19 12:27:50 +0000

    fix: remove route/view not related to PR

--- packages/webapp/src/utils/credits-purchase-constants.js
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -1,5 +1,5 @@
 const routes = {
-  CREDITS_PURCHASE_APPLYING_INDIVIDIAL_ORGANISATION: '/credits-purchase/applying-individual-organisation',
+  // CREDITS_PURCHASE_APPLYING_INDIVIDIAL_ORGANISATION: '/credits-purchase/applying-individual-organisation',
   CREDITS_PURCHASE_CHECK_DEFRA_ACCOUNT_DETAILS: '/credits-purchase/check-defra-account-details'
   // REGISTER_CREDIT_PURCHASE_TASK_LIST: '/credit-purchase/register-credit-purchase-task-list'
 }
@@ -9,7 +9,7 @@ const views = Object.fromEntries(
 )
 
 const redisKeys = {
-  CREDITS_PURCHASE_APPLYING_INDIVIDIAL_ORGANISATION: 'credits-purchase-applying-individual-organisation',
+  // CREDITS_PURCHASE_APPLYING_INDIVIDIAL_ORGANISATION: 'credits-purchase-applying-individual-organisation',
   CREDITS_PURCHASE_DEFRA_ACCOUNT_DETAILS_CONFIRMED: 'credits-purchase-defra-account-details-confirmed'
 }
 
