cf6d8b61135bcaa7ff57a8289f6b8df44385c7fe
ajjawalk <ajinkya.jawalkar@capgemini.com>
2024-02-21 14:59:30 +0000

    updated files

--- packages/webapp/src/utils/credits-purchase-constants.js
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -1,7 +1,7 @@
 const routes = {
   CREDITS_PURCHASE_CHECK_DEFRA_ACCOUNT_DETAILS: '/credits-purchase/check-defra-account-details',
   CREDITS_PURCHASE_INDIVIDUAL_ORG: '/credits-purchase/applying-individual-organisation',
-  CREDITS_PURCHASE_TERMS_AND_CONDITIONS: '/credits-purchase/credits-terms-and-conditions',
+  CREDITS_PURCHASE_TERMS_AND_CONDITIONS: '/credits-purchase/confirm-terms-conditions',
   CREDITS_PURCHASE_CHECK_YOUR_ANSWERS: '/credits-purchase/credits-check-your-answers'
 }
 
