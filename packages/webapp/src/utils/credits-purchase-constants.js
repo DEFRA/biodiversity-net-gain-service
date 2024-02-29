bf1680014da02658ba62f31cdce780fb9843550e
Usman Shaikh <115730841+usman-j-shaikh@users.noreply.github.com>
2024-02-29 20:07:41 +0530

    Feature/BNGP-2810: Main Credits Journey: Purchase Order Screen (#437)
    * Added new routes and view for purchase order number
    
    * Fixed code smell
    
    * Fixed indent issue
    
    * WIP: Adding next page
    
    * WIP: reverted infrastructure.yml
    
    * Updated infrastructure.yml
    
    * Modified unit test cases of credits purchase order
    
    * WIP: Folder & file structure changes
    
    * Rebased with base branch
    
    * Modified check purchase order page to redirect on tasklist

--- packages/webapp/src/utils/credits-purchase-constants.js
+++ packages/webapp/src/utils/credits-purchase-constants.js
@@ -9,7 +9,9 @@ const routes = {
   CREDITS_PURCHASE_CHECK_YOUR_ANSWERS: '/credits-purchase/check-and-submit',
   CREDITS_PURCHASE_CREDITS_SELECTION: '/credits-purchase/add-statutory-biodiversity-credits',
   CREDITS_PURCHASE_CREDITS_COST: '/credits-purchase/estimated-cost-statutory-biodiversity-credits',
-  CREDITS_PURCHASE_TASK_LIST: '/credits-purchase/buy-statutory-biodiversity-credits'
+  CREDITS_PURCHASE_TASK_LIST: '/credits-purchase/buy-statutory-biodiversity-credits',
+  CREDITS_PURCHASE_CHECK_ORDER: '/credits-purchase/check-purchase-order',
+  CREDITS_PURCHASE_APP_BY_INDIVIDUAL_OR_ORGANISATION: '/credits-purchase/applying-individual-organisation'
 }
 
 const views = Object.fromEntries(
@@ -23,7 +25,10 @@ const redisKeys = {
   CREDITS_PURCHASE_DATE_OF_BIRTH: 'credits-purchase-date-of-birth',
   CREDITS_PURCHASE_NATIONALITY: 'credits-purchase-nationality-key',
   CREDITS_PURCHASE_TERMS_AND_CONDITIONS_CONFIRMED: 'credits-purchase-consent',
-  CREDITS_PURCHASE_COST_CALCULATION: 'credits-purchase-cost-calculation'
+  CREDITS_PURCHASE_COST_CALCULATION: 'credits-purchase-cost-calculation',
+  CREDITS_PURCHASE_USER_TYPE: 'credits-purchase-user-type',
+  CREDITS_PURCHASE_WILL_PO_IN_USE: 'credits-purchase-will-po-in-use',
+  CREDITS_PURCHASE_ORDER_NUMBER: 'credits-purchase-order-number'
 }
 
 export default {
