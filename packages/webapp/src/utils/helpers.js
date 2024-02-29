adda4d63312945f0d76ca3b094dca0562cfa94bb
Kiran Varma <varma.ap@gmail.com>
2024-02-29 16:17:56 +0000

    feat: BNGP-4140 Remove eligibility routes (#640)
    * feat: remove eligibility from consts
    
    * fix: remove eligiblity and your details
    
    * revert: clients-name.js
    
    * fix: remove unused test
    
    * fix: failing test
    
    * feat: redirect Add your details to agent page
    
    * feat: remove more unused routes
    
    * Revert "fix: remove unused test"
    
    This reverts commit 89dd304f60b6469015991cbc64239561d0eefabe.
    
    * refactor: remove unused helper function
    
    * refactor: remove non-existing constants
    
    * fix: use correct developer route
    
    * test: update route and fix failing test
    
    ---------
    
    Co-authored-by: Adam Kay <90856315+thousand-leaves@users.noreply.github.com>

--- packages/webapp/src/utils/helpers.js
+++ packages/webapp/src/utils/helpers.js
@@ -329,19 +329,6 @@ const getEligibilityResults = session => {
   return eligibilityResults
 }
 
-const getDeveloperEligibilityResults = session => {
-  const developerEligibilityResults = {
-    yes: [],
-    no: [],
-    'not-sure': []
-  }
-  session.get(constants.redisKeys.DEVELOPER_WRITTEN_CONTENT_VALUE) &&
-  developerEligibilityResults[session.get(constants.redisKeys.DEVELOPER_WRITTEN_CONTENT_VALUE)].push(constants.redisKeys.DEVELOPER_WRITTEN_CONTENT_VALUE)
-  session.get(constants.redisKeys.DEVELOPER_ELIGIBILITY_METRIC_VALUE) &&
-  developerEligibilityResults[session.get(constants.redisKeys.DEVELOPER_ELIGIBILITY_METRIC_VALUE)].push(constants.redisKeys.DEVELOPER_ELIGIBILITY_METRIC_VALUE)
-  return developerEligibilityResults
-}
-
 const formatSortCode = sortCode => `${sortCode.substring(0, 2)} ${sortCode.substring(2, 4)} ${sortCode.substring(4, 6)}`
 
 const habitatTypeAndConditionMapper = (sheets, metricData) => {
@@ -839,7 +826,6 @@ export {
   validateFirstLastNameOfLandownerOrLeaseholder,
   emailValidator,
   getDateString,
-  getDeveloperEligibilityResults,
   validateBNGNumber,
   getErrById,
   getMaximumFileSizeExceededView,
