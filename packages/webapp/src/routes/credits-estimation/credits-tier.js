76e6bed0b051642f0aa64e92f642f8ef5b72dac5
John Barnard <John.Barnard@defra.gov.uk>
2024-03-01 16:18:48 +0000

    moved duplicate code into helpers

--- packages/webapp/src/routes/credits-estimation/credits-tier.js
+++ packages/webapp/src/routes/credits-estimation/credits-tier.js
@@ -1,6 +1,7 @@
 import creditsEstimationConstants from '../../utils/credits-estimation-constants.js'
 import calculateCost from '../../credits/calculate.js'
 import Joi from 'joi'
+import { creditsValidationFailAction, creditsValidationSchema } from '../../utils/helpers.js'
 
 const defaultErrorMessage = { text: 'Enter at least one credit from the metric up to 2 decimal places, like 23.75' }
 const charLengthErrorMessage = { text: 'Number of credits must be 10 characters or fewer' }
@@ -20,41 +21,14 @@ const handlers = {
   }
 }
 
-const payloadValidationSchema = Joi.object({
-  a1: inputSchema,
-  a2: inputSchema,
-  a3: inputSchema,
-  a4: inputSchema,
-  a5: inputSchema,
-  h: inputSchema,
-  w: inputSchema
-}).custom((value, helpers) => {
-  if (Object.values(value).every(v => v === '' || Number(v) === 0)) {
-    throw new Error('at least one credit unit input should have a value')
-  }
-})
+const payloadValidationSchema = creditsValidationSchema(inputSchema)
 
 const validationFailAction = (request, h, err) => {
-  const errorMessages = {}
-  const errorList = []
-
-  if (err.details.some(e => e.type === 'any.custom')) {
-    const errorId = 'custom-err'
-    errorMessages[errorId] = defaultErrorMessage
-    errorList.push({
-      ...defaultErrorMessage,
-      href: `#${errorId}`
-    })
-  } else {
-    err.details.forEach(e => {
-      const errorMessage = e.type === 'string.max' ? charLengthErrorMessage : defaultErrorMessage
-      errorMessages[e.context.key] = errorMessage
-      errorList.push({
-        ...errorMessage,
-        href: `#${e.context.key}-units`
-      })
-    })
-  }
+  const { errorMessages, errorList } = creditsValidationFailAction({
+    err,
+    defaultErrorMessage,
+    charLengthErrorMessage
+  })
 
   return h.view(creditsEstimationConstants.views.ESTIMATOR_CREDITS_TIER, {
     errorMessages,
