104ebf871b0162fb29d031b38eb92eefc47881e6
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2024-03-10 23:23:54 +0000

    Check and submit route and view

--- packages/webapp/src/routes/credits-purchase/check-and-submit.js
+++ packages/webapp/src/routes/credits-purchase/check-and-submit.js
@@ -3,9 +3,58 @@ import creditsApplicationValidation from '../../utils/credits-application-valida
 import { postJson } from '../../utils/http.js'
 import creditsPurchaseConstants from '../../utils/credits-purchase-constants.js'
 import constants from '../../utils/constants.js'
+import { getHumanReadableFileSize, dateToString } from '../../utils/helpers.js'
+
+const getApplicationDetails = session => {
+  const metricData = session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_DATA)
+  const metricFileSize = getHumanReadableFileSize(session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_FILE_SIZE), 1)
+  const credits = session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_COST_CALCULATION)
+  const creditsAmounts = Object.fromEntries(credits.tierCosts.map(element => [element.tier, element.unitAmount]))
+  const usingPurchaseOrder = session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_PURCHASE_ORDER_USED)
+  const nationality = session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_NATIONALITY)
+  const nationalityHtml = Object.values(nationality).filter(n => n !== '').join('<br/>')
+
+  return {
+    metric: {
+      fileName: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_FILE_NAME),
+      fileNameUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_UPLOAD_METRIC,
+      fileSize: metricFileSize,
+      detailsConfirmed: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_DETAILS_CONFIRMED),
+      detailsConfirmedUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_CONFIRM_DEV_DETAILS,
+      projectName: metricData.startPage.projectName,
+      localAuthority: metricData.startPage.planningAuthority,
+      planningRef: metricData.startPage.planningApplicationReference
+    },
+    credits: {
+      amounts: creditsAmounts,
+      total: credits.total.toLocaleString('en-gb', { style: 'currency', currency: 'GBP', minimumFractionDigits: 0 }),
+      changeUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_CREDITS_SELECTION
+    },
+    purchaseOrder: {
+      usingPurchaseOrder,
+      number: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_PURCHASE_ORDER_NUMBER),
+      changeUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_CHECK_PURCHASE_ORDER
+    },
+    dueDiligence: {
+      individualOrOrg: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_USER_TYPE),
+      individualOrOrgUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_INDIVIDUAL_OR_ORG,
+      middleName: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_MIDDLE_NAME)?.middleName,
+      middleNameUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_MIDDLE_NAME,
+      dateOfBirth: dateToString(session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_DATE_OF_BIRTH)),
+      dateOfBirthUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_DATE_OF_BIRTH,
+      nationality: nationalityHtml,
+      nationalityUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_NATIONALITY
+    }
+  }
+}
 
 const handlers = {
-  get: (request, h) => h.view(creditsPurchaseConstants.views.CREDITS_PURCHASE_CHECK_YOUR_ANSWERS),
+  get: (request, h) => {
+    return h.view(creditsPurchaseConstants.views.CREDITS_PURCHASE_CHECK_YOUR_ANSWERS, {
+      ...getApplicationDetails(request.yar),
+      backLink: creditsPurchaseConstants.routes.CREDITS_PURCHASE_TASK_LIST
+    })
+  },
   post: async (request, h) => {
     const { value, error } = creditsApplicationValidation.validate(creditsApplication(request.yar, request.auth.credentials.account))
     if (error) {
