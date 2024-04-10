661626d0fe7853372b76d7d4b458aab346b902d7
KiranVarma02 <varma.ap@gmail.com>
2024-04-10 09:19:49 +0000

    fix: remove yes/no from route

--- packages/webapp/src/routes/developer/confirm-off-site-gain.js
+++ packages/webapp/src/routes/developer/confirm-off-site-gain.js
@@ -1,8 +1,6 @@
 import constants from '../../utils/constants.js'
-import { deleteBlobFromContainers } from '../../utils/azure-storage.js'
 import { processDeveloperTask } from '../../utils/helpers.js'
 
-const href = '#offsite-details-checked-yes'
 const handlers = {
   get: (request, h) => {
     const context = getContext(request)
@@ -10,31 +8,14 @@ const handlers = {
   },
   post: async (request, h) => {
     const confirmOffsiteGain = request.payload.confirmOffsiteGain
-    const metricUploadLocation = request.yar.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION)
     request.yar.set(constants.redisKeys.CONFIRM_OFFSITE_GAIN_CHECKED, confirmOffsiteGain)
-    if (confirmOffsiteGain === constants.DEVELOPER_CONFIRM_OFF_SITE_GAIN.NO) {
-      await deleteBlobFromContainers(metricUploadLocation)
-      request.yar.clear(constants.redisKeys.DEVELOPER_METRIC_LOCATION)
-      return h.redirect(constants.routes.DEVELOPER_UPLOAD_METRIC)
-    } else if (confirmOffsiteGain === constants.DEVELOPER_CONFIRM_OFF_SITE_GAIN.YES) {
-      processDeveloperTask(request,
-        {
-          taskTitle: 'Biodiversity 4.1 Metric calculations',
-          title: 'Confirm off-site gain'
-        }, { status: constants.COMPLETE_DEVELOPER_TASK_STATUS })
-      return h.redirect(request.yar.get(constants.redisKeys.REFERER, true) || constants.routes.DEVELOPER_TASKLIST)
-    } else {
-      const context = getContext(request)
-      return h.view(constants.views.DEVELOPER_CONFIRM_OFF_SITE_GAIN, {
-        context,
-        err: [
-          {
-            text: 'Select yes if this is the correct file',
-            href
-          }
-        ]
-      })
-    }
+
+    processDeveloperTask(request,
+      {
+        taskTitle: 'Biodiversity 4.1 Metric calculations',
+        title: 'Confirm off-site gain'
+      }, { status: constants.COMPLETE_DEVELOPER_TASK_STATUS })
+    return h.redirect(request.yar.get(constants.redisKeys.REFERER, true) || constants.routes.DEVELOPER_TASKLIST)
   }
 }
 
