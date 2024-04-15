a9fdd5e87d47c9d195d938c41f0b1f7e5274eab1
Adam Kay <thousand_leaves@hotmail.com>
2024-04-15 16:06:16 +0100

    Refactor generatePayloadOptions function to remove unused import and console.log statements

--- packages/webapp/src/utils/generate-payload-options.js
+++ packages/webapp/src/utils/generate-payload-options.js
@@ -1,4 +1,4 @@
-import { processErrorUpload, maximumFileSizeExceeded } from './upload-error-handler.js'
+import { maximumFileSizeExceeded } from './upload-error-handler.js'
 
 function generatePayloadOptions (href, maximumFileSize, view) {
   // TODO: delete console.log when you have tested the new function
@@ -13,18 +13,10 @@ function generatePayloadOptions (href, maximumFileSize, view) {
       allow: 'multipart/form-data',
       failAction: (request, h, err) => {
         console.log('Hello from failAction')
-        // TODO: can i eventually get this first if statement moved into the upload-error-handler.js file?
-        // For some reason it doesn't return the correct error when i try to move it
         if (err.output.statusCode === 413) { // Request entity too large
           console.log('Hello from failAction: statusCode 413 Request entity too large')
           return maximumFileSizeExceeded(h, href, maximumFileSize, view).takeover()
-        } else if (err) {
-          // will this ever be hit? I don't think so... can we ensure that we forward to processErrorUpload
-          // in the upload-error-handler.js file instead and handle the file size error there?
-          console.log('Hello from failAction: err')
-          return processErrorUpload(err, h, href, maximumFileSize, view).takeover()
         } else {
-          console.log('Hello from failAction: else')
           throw err
         }
       }
