10253a6d27f910673ff6f1adcade5d384328e7e2
KiranVarma02 <varma.ap@gmail.com>
2024-04-18 18:39:07 +0000

    fix: remove confirm development details from check and submit

--- packages/webapp/src/routes/credits-purchase/check-and-submit.js
+++ packages/webapp/src/routes/credits-purchase/check-and-submit.js
@@ -23,7 +23,6 @@ const getApplicationDetails = (session, currentOrganisation) => {
       fileName: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_FILE_NAME),
       fileNameUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_UPLOAD_METRIC,
       fileSize: metricFileSize,
-      detailsConfirmed: session.get(creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_METRIC_DETAILS_CONFIRMED),
       detailsConfirmedUrl: creditsPurchaseConstants.routes.CREDITS_PURCHASE_CONFIRM_DEV_DETAILS,
       projectName: metricData.startPage.projectName ?? '',
       localAuthority: metricData.startPage.planningAuthority ?? '',
