ad9e14a1d953444e5ac1891ffd079229ac541611
KiranVarma02 <varma.ap@gmail.com>
2024-04-30 12:13:11 +0000

    fix: failing tests

--- packages/webapp/src/routes/__tests__/land/add-planning-authority.spec.js
+++ packages/webapp/src/routes/__tests__/land/add-planning-authority.spec.js
@@ -105,7 +105,7 @@ describe(url, () => {
 
       await addPlanningAuthority.default[1].handler(request, h)
       expect(viewResult).toEqual(constants.views.ADD_PLANNING_AUTHORITY)
-      expect(resultContext.err).toEqual([{ href: '#localPlanningAuthority', text: 'This local planning authority has already been added - enter a different local planning authority, if there is one' }])
+      expect(resultContext.err).toEqual([{ href: '#localPlanningAuthorityErr', text: 'This local planning authority has already been added - enter a different local planning authority, if there is one' }])
     })
     it('should fail to edit planning authority  to legal agreement with duplicate planning authority name', async () => {
       const request = {
@@ -116,7 +116,7 @@ describe(url, () => {
 
       await addPlanningAuthority.default[1].handler(request, h)
       expect(viewResult).toEqual(constants.views.ADD_PLANNING_AUTHORITY)
-      expect(resultContext.err).toEqual([{ href: '#localPlanningAuthority', text: 'This local planning authority has already been added - enter a different local planning authority, if there is one' }])
+      expect(resultContext.err).toEqual([{ href: '#localPlanningAuthorityErr', text: 'This local planning authority has already been added - enter a different local planning authority, if there is one' }])
     })
 
     it('Should show error message if invalid lpa is provided', async () => {
@@ -129,7 +129,7 @@ describe(url, () => {
       await addPlanningAuthority.default[1].handler(request, h)
 
       expect(viewResult).toEqual(constants.views.ADD_PLANNING_AUTHORITY)
-      expect(resultContext.localPlanningAuthorityNameErr[0].text).toEqual('Enter a valid local planning authority')
+      expect(resultContext.err[0].text).toEqual('Enter a valid local planning authority')
     })
   })
 })
