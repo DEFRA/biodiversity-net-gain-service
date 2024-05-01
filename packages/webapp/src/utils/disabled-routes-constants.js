112dc8b154c0d123f74a3e812c96f93a096affc5
John Barnard <John.Barnard@defra.gov.uk>
2024-05-01 15:48:59 +0100

    Disable land payment routes unless env var set to Y

--- packages/webapp/src/utils/disabled-routes-constants.js
+++ packages/webapp/src/utils/disabled-routes-constants.js
@@ -6,7 +6,8 @@ const {
   ENABLE_ROUTE_SUPPORT_FOR_ADDITIONAL_EMAIL,
   ENABLE_ROUTE_SUPPORT_FOR_DEV_JOURNEY,
   ENABLE_ROUTE_SUPPORT_FOR_CREDIT_ESTIMATION_JOURNEY,
-  ENABLE_ROUTE_SUPPORT_FOR_CREDIT_PURCHASE_JOURNEY
+  ENABLE_ROUTE_SUPPORT_FOR_CREDIT_PURCHASE_JOURNEY,
+  ENABLE_ROUTE_LAND_PAYMENT_JOURNEY
 } = process.env
 let disabledRoutes = {}
 
@@ -17,6 +18,10 @@ const GEOSPATIAL_LAND_BOUNDARY = 'land/geospatial-land-boundary'
 const CHOOSE_LAND_BOUNDARY_UPLOAD = 'land/choose-land-boundary-upload'
 const DEVELOPER_EMAIL_ENTRY = 'developer/email-entry'
 const OS_API_TOKEN = 'land/os-api-token'
+const LAND_PAYMENT = 'land/payment'
+const LAND_PAYMENT_RETURN = 'land/payment-return'
+const LAND_PAYMENT_SUCCESS = 'land/payment-success'
+const LAND_PAYMENT_FAILURE = 'land/payment-failure'
 
 const geospatialRoutes = {
   CHECK_GEOSPATIAL_FILE,
@@ -28,6 +33,13 @@ const geospatialRoutes = {
 
 const additionalEmailRoutes = {
   DEVELOPER_EMAIL_ENTRY
+}
+
+const landPaymentRoutes = {
+  LAND_PAYMENT,
+  LAND_PAYMENT_SUCCESS,
+  LAND_PAYMENT_FAILURE,
+  LAND_PAYMENT_RETURN
 }
 
 if (ENABLE_ROUTE_SUPPORT_FOR_GEOSPATIAL === 'Y') {
@@ -48,6 +60,10 @@ if (ENABLE_ROUTE_SUPPORT_FOR_CREDIT_ESTIMATION_JOURNEY === 'Y') {
 
 if (ENABLE_ROUTE_SUPPORT_FOR_CREDIT_PURCHASE_JOURNEY === 'Y') {
   disabledRoutes = { ...disabledRoutes, ...creditsPurchaseConstants.routes }
+}
+
+if (ENABLE_ROUTE_LAND_PAYMENT_JOURNEY === 'Y') {
+  disabledRoutes = { ...disabledRoutes, ...landPaymentRoutes }
 }
 
 export default disabledRoutes
