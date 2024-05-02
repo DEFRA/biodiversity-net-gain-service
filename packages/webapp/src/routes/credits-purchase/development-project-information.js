f9b9e62a09b03537f7e68cf9824f3a07e5f1b8d4
Sooraj Vijayan <sooraj.vijayan@defra.gov.uk>
2024-05-02 18:58:01 +0100

    feature/BNGP-4733-credit-purchase-implement-referrer-logic

--- packages/webapp/src/routes/credits-purchase/development-project-information.js
+++ packages/webapp/src/routes/credits-purchase/development-project-information.js
@@ -63,7 +63,7 @@ const handlers = {
       request.yar.set(creditsConstants.redisKeys.CREDITS_PURCHASE_PLANNING_AUTHORITY_LIST, selectedLpa)
       request.yar.set(creditsConstants.redisKeys.CREDITS_PURCHASE_PLANNING_APPLICATION_REF, planningApplicationRef)
       request.yar.set(creditsConstants.redisKeys.CREDITS_PURCHASE_DEVELOPMENT_NAME, developmentName)
-      return h.redirect(creditsConstants.routes.CREDITS_PURCHASE_TASK_LIST)
+      return h.redirect(request.yar.get(creditsConstants.redisKeys.REFERER, true) || creditsConstants.routes.CREDITS_PURCHASE_TASK_LIST)
     }
   }
 }
