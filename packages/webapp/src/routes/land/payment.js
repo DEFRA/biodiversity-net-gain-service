cfd310d72bcde0079e6e7514c118d2f796653008
John Barnard <John.Barnard@defra.gov.uk>
2024-05-03 14:37:50 +0100

    Only allow use to go to payment is ready to submit and show reference on new line

--- packages/webapp/src/routes/land/payment.js
+++ packages/webapp/src/routes/land/payment.js
@@ -5,9 +5,15 @@ import applicationValidation from '../../utils/application-validation.js'
 import application from '../../utils/application.js'
 import fees from '../../payment/fees.js'
 import { postJson } from '../../utils/http.js'
+import { getTaskList } from '../../journey-validation/task-list-generator.js'
 
 const handlers = {
   get: async (request, h) => {
+    const { canSubmit } = getTaskList(constants.applicationTypes.REGISTRATION, request.yar)
+
+    if (!canSubmit) {
+      return h.redirect(constants.routes.REGISTER_LAND_TASK_LIST)
+    }
     const { value, error } = applicationValidation.validate(application(request.yar, request.auth.credentials.account))
     if (error) {
       throw new Error(error)
