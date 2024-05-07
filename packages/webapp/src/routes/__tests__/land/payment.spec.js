965d938a03416e6bb211f2757de05896345d1760
John Barnard <John.Barnard@defra.gov.uk>
2024-05-07 10:59:15 +0100

    Test payment route failures

--- packages/webapp/src/routes/__tests__/land/payment.spec.js
+++ packages/webapp/src/routes/__tests__/land/payment.spec.js
@@ -2,7 +2,14 @@ import createPayment from '../../../payment/gov-pay-api/payment-create.js'
 import { submitGetRequest } from '../helpers/server.js'
 import constants from '../../../utils/constants.js'
 
+import applicationValidation from '../../../utils/application-validation.js'
+import { getTaskList } from '../../../journey-validation/task-list-generator.js'
+
 jest.mock('../../../payment/gov-pay-api/payment-create.js')
+jest.mock('../../../journey-validation/task-list-generator.js')
+jest.mock('../../../utils/application-validation.js')
+jest.mock('../../../payment/gov-pay-api/payment-create.js')
+jest.mock('../../../utils/http.js')
 
 const url = constants.routes.LAND_PAYMENT
 
@@ -10,6 +17,17 @@ describe(url, () => {
   describe('GET', () => {
     it(`should render the ${url.substring(1)} view`, async () => {
       const testUrl = 'testUrl'
+      getTaskList.mockReturnValueOnce({ canSubmit: true })
+      applicationValidation.validate.mockReturnValueOnce({
+        value: {
+          landownerGainSiteRegistration: {
+            payment: {
+              reference: 'TESTREF'
+            }
+          }
+        },
+        error: null
+      })
       createPayment.mockResolvedValue({
         _links: {
           next_url: {
@@ -19,6 +37,17 @@ describe(url, () => {
       })
       const response = await submitGetRequest({ url }, 302)
       expect(response.headers.location).toEqual(testUrl)
+    })
+    it('should redirect to register land task list when canSubmit is false', async () => {
+      getTaskList.mockReturnValueOnce({ canSubmit: false })
+      const response = await submitGetRequest({ url }, 302)
+      expect(response.headers.location).toEqual(constants.routes.REGISTER_LAND_TASK_LIST)
+    })
+    it('should throw an error when application validation fails', async () => {
+      getTaskList.mockReturnValueOnce({ canSubmit: true })
+      const validationError = new Error('Validation error')
+      applicationValidation.validate.mockReturnValueOnce({ value: null, error: validationError })
+      await submitGetRequest({ url }, 500)
     })
   })
 })
