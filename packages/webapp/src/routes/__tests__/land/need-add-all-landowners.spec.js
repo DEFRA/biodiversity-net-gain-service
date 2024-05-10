888cd90ce80c9e142611117bd088e89bbb62c482
soorajvdefra <135850940+soorajvdefra@users.noreply.github.com>
2024-05-10 16:18:21 +0100

    feature/BNGP-4402-s106-extra-urls-needtobe-removed (#726)
    * feature/BNGP-4402-s106-extra-urls-needtobe-removed
    
    * feature/BNGP-4402-s106-extra-urls-needtobe-removed
    
    * feature/BNGP-4402-s106-extra-urls-needtobe-removed
    
    * feature/BNGP-4402-s106-extra-urls-needtobe-removed

--- packages/webapp/src/routes/__tests__/land/need-add-all-landowners.spec.js
+++ packages/webapp/src/routes/__tests__/land/need-add-all-landowners.spec.js
@@ -1,13 +1,13 @@
 import { submitGetRequest, submitPostRequest } from '../helpers/server.js'
 import constants from '../../../utils/constants.js'
-const url = constants.routes.NEED_ADD_ALL_LANDOWNERS_CONSERVATION_COVENANT
+const url = constants.routes.NEED_ADD_ALL_LANDOWNERS
 
 describe(url, () => {
   describe('GET', () => {
     it(`should render the ${url.substring(1)} view`, async () => {
       await submitGetRequest({
         headers: {
-          referer: constants.routes.NEED_ADD_ALL_LANDOWNERS_CONSERVATION_COVENANT
+          referer: constants.routes.NEED_ADD_ALL_LANDOWNERS
         },
         url
       })
@@ -23,7 +23,7 @@ describe(url, () => {
     })
     it('Should continue journey if Continue button clicked', async () => {
       const res = await submitPostRequest(postOptions)
-      expect(res.headers.location).toEqual('/land/landowner-conservation-covenant-individual-organisation')
+      expect(res.headers.location).toEqual('/land/landowner-individual-organisation')
     })
   })
 })
