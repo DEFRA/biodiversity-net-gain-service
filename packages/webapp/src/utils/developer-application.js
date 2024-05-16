74c157b0acf91e8927485dbb53e1f0ac910651f2
Adam Kay <thousand_leaves@hotmail.com>
2024-05-16 10:34:37 +0100

    Restructures the developer-application.js file to be more similar to other journeys

--- packages/webapp/src/utils/developer-application.js
+++ packages/webapp/src/utils/developer-application.js
@@ -3,52 +3,30 @@ import paymentConstants from '../payment/constants.js'
 import savePayment from '../payment/save-payment.js'
 import path from 'path'
 
+// Developer Application object schema must match the expected payload format for the Operator application
+const getApplicant = (account, session) => ({
+  id: account.idTokenClaims.contactId,
+  role: getApplicantRole(session)
+})
+
+const getApplicantRole = session => {
+  const applicantIsAgent = session.get(constants.redisKeys.IS_AGENT)
+  const organisationId = session.get(constants.redisKeys.ORGANISATION_ID)
+  let applicantRole
+
+  if (applicantIsAgent === constants.APPLICANT_IS_AGENT.YES) {
+    applicantRole = constants.applicantTypes.AGENT
+  } else if (organisationId) {
+    applicantRole = constants.applicantTypes.REPRESENTATIVE
+  } else {
+    applicantRole = constants.applicantTypes.LANDOWNER
+  }
+
+  return applicantRole
+}
+
 const getDeveloperApplicationReference = session => session.get(constants.redisKeys.DEVELOPER_APP_REFERENCE) || ''
 
-// Developer Application object schema must match the expected payload format for the Operator application
-export default (session, account) => {
-  return {
-    developerAllocation: {
-      applicant: {
-        firstName: account.idTokenClaims.firstName,
-        lastName: account.idTokenClaims.lastName,
-        emailAddress: account.idTokenClaims.email,
-        role: session.get(constants.redisKeys.DEVELOPER_ROLE_KEY),
-        contactId: account.idTokenClaims.contactId
-      },
-      developmentDetails: {
-        projectName: session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)?.startPage.projectName,
-        localAuthority: session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)?.startPage.planningAuthority,
-        planningReference: session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)?.startPage.planningApplicationReference
-      },
-      additionalEmailAddresses: session.get(constants.redisKeys.DEVELOPER_ADDITIONAL_EMAILS) || [],
-      biodiversityGainSiteNumber: session.get(constants.redisKeys.BIODIVERSITY_NET_GAIN_NUMBER),
-      confirmDevelopmentDetails: session.get(constants.redisKeys.METRIC_FILE_CHECKED),
-      confirmOffsiteGainDetails: session.get(constants.redisKeys.CONFIRM_OFFSITE_GAIN_CHECKED),
-      gainSite: getGainSite(session),
-      habitats: getHabitats(session),
-      gainSiteReference: getDeveloperApplicationReference(session), // Need to get one after submitting application
-      submittedOn: new Date().toISOString(),
-      files: [
-        {
-          contentMediaType: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_TYPE),
-          fileType: 'developer-upload-metric',
-          fileSize: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION),
-          fileName: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_NAME) && path.basename(session.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION))
-        },
-        {
-          contentMediaType: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_TYPE),
-          fileType: 'developer-upload-consent',
-          fileSize: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_SIZE),
-          fileLocation: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_LOCATION),
-          fileName: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_NAME) && path.basename(session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_LOCATION))
-        }
-      ],
-      payment: savePayment(session, paymentConstants.ALLOCATION, getDeveloperApplicationReference(session))
-    }
-  }
-}
 const getHabitats = session => {
   const metricData = session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)
   const allocatedIdentifiers = ['d2', 'e2', 'f2', 'd3', 'e3', 'f3']
@@ -98,3 +76,54 @@ const getGainSite = session => {
     }
   }
 }
+
+const getPayment = session => {
+  const payment = savePayment(session, paymentConstants.REGISTRATION, getDeveloperApplicationReference(session))
+  return {
+    reference: payment.reference,
+    method: payment.type
+  }
+}
+
+const application = (session, account) => {
+  const applicationDetails = {
+    developerRegistration: {
+      applicant: getApplicant(account, session),
+      isLandownerLeaseholder: session.get(constants.redisKeys.IS_LANDOWNER_LEASEHOLDER),
+      // confirmDevelopmentDetails: session.get(constants.redisKeys.METRIC_FILE_CHECKED),
+      // confirmOffsiteGainDetails: session.get(constants.redisKeys.CONFIRM_OFFSITE_GAIN_CHECKED),
+      gainSite: getGainSite(session),
+      habitats: getHabitats(session),
+      files: [
+        {
+          contentMediaType: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_TYPE),
+          fileType: 'developer-upload-metric',
+          fileSize: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_SIZE),
+          fileLocation: session.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION),
+          fileName: session.get(constants.redisKeys.DEVELOPER_METRIC_FILE_NAME) && path.basename(session.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION))
+        },
+        {
+          contentMediaType: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_TYPE),
+          fileType: 'developer-upload-consent',
+          fileSize: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_SIZE),
+          fileLocation: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_LOCATION),
+          fileName: session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_NAME) && path.basename(session.get(constants.redisKeys.DEVELOPER_CONSENT_FILE_LOCATION))
+        }
+      ],
+      development: {
+        localPlanningAuthority: {
+          code: session.get(constants.redisKeys.DEVELOPER_LOCAL_AUTHORITY_CODE),
+          name: session.get(constants.redisKeys.DEVELOPER_LOCAL_AUTHORITY_NAME)
+        },
+        planningReference: session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)?.startPage.planningApplicationReference,
+        name: session.get(constants.redisKeys.DEVELOPER_METRIC_DATA)?.startPage.projectName
+      },
+      payment: getPayment(session),
+      allocationReference: getDeveloperApplicationReference(session), // Need to get one after submitting application
+      submittedOn: new Date().toISOString()
+    }
+  }
+  return applicationDetails
+}
+
+export default application
