8cfa4e0ce349e6fed5a51396e988351f6d610a52
John Barnard <John.Barnard@defra.gov.uk>
2024-05-31 14:21:22 +0100

    Removed duplicate helper

--- packages/webapp/src/utils/helpers.js
+++ packages/webapp/src/utils/helpers.js
@@ -783,26 +783,6 @@ const isAgentAndNotLandowner = session => {
   return isAgent && clientIsNotLandownerOrLeaseholder
 }
 
-const extractAllocationHabitatsByGainSiteNumber = (metricData, gainSiteNumber) => {
-  const filteredMetricData = {}
-  const sheetLabels = ['d2', 'd3', 'e2', 'e3', 'f2', 'f3']
-
-  sheetLabels.forEach(label => {
-    filteredMetricData[label] = metricData[label].filter(habitat => String(habitat['Off-site reference']) === gainSiteNumber)
-
-    // calculate the area based on the filtered out habitats and add to the habitat array
-    // as the last entry, this is then used by habitatTypeAndConditionMapper later
-    const unitKey = habitatTypeMap[label].unitKey
-    const measurementTotal = filteredMetricData[label].reduce((acc, cur) => acc + cur[unitKey], 0)
-    filteredMetricData[label].push({
-      [unitKey]: measurementTotal
-    })
-  })
-
-  const habitats = habitatTypeAndConditionMapper(['d2', 'd3', 'e2', 'e3', 'f2', 'f3'], filteredMetricData)
-  return combineHabitats(habitats)
-}
-
 export {
   validateDate,
   dateClasses,
@@ -859,6 +839,5 @@ export {
   getAuthenticatedUserRedirectUrl,
   creditsValidationSchema,
   creditsValidationFailAction,
-  isAgentAndNotLandowner,
-  extractAllocationHabitatsByGainSiteNumber
+  isAgentAndNotLandowner
 }
