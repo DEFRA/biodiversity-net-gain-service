c0d54e1515cbe5467390940f341d19e3eaf5e978
John Barnard <John.Barnard@defra.gov.uk>
2024-06-10 12:01:46 +0100

    updated test secret

--- packages/webapp/src/plugins/test-utils/setup.js
+++ packages/webapp/src/plugins/test-utils/setup.js
@@ -11,7 +11,7 @@ export const getServer = async () => {
       options: {
         storeBlank: false,
         cookieOptions: {
-          password: 'q9w7r5u3i1e0w9o7p5a3s1d9f7g5h3j1k9l7z5x3c1v9b7n5m3q1w0e8r6t4y2u0i8o6p4a2s0d8f6g4h2j0k8l6z4x2c0v8b6n4m2',
+          password: 'only-for-test-server-cookie-secret',
           isSecure: false
         }
       }
