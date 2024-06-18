13a3bbf5796d90cbf131c270161e7265d6dea447
Stewart Jumbe <stewart.jumbe@defra.gov.uk>
2024-06-18 14:01:32 +0100

    update error-handler

--- packages/webapp/src/utils/upload-error-handler.js
+++ packages/webapp/src/utils/upload-error-handler.js
@@ -2,54 +2,56 @@ import constants from './constants.js'
 import { getMaximumFileSizeExceededView } from './helpers.js'
 import { ThreatScreeningError, MalwareDetectedError } from '@defra/bng-errors-lib'
 
-function processErrorUpload ({ err, h, href, noFileErrorMessage, unsupportedFileExtErrorMessage, optionalErrorMessage = 'default', maximumFileSize }) {
+function processErrorUpload ({ err, h, route, elementID, noFileErrorMessage, unsupportedFileExtErrorMessage, optionalErrorMessage = 'default', maximumFileSize }) {
   switch (err.message) {
     case constants.uploadErrors.maximumFileSizeExceeded:
-      return buildErrorResponse(h, `The selected file must not be larger than ${maximumFileSize}MB`, href)
+      return buildErrorResponse(h, `The selected file must not be larger than ${maximumFileSize}MB`, route, elementID)
     case constants.uploadErrors.emptyFile:
-      return buildErrorResponse(h, 'The selected file is empty', href)
+      return buildErrorResponse(h, 'The selected file is empty', route, elementID)
     case constants.uploadErrors.noFile:
-      return buildErrorResponse(h, noFileErrorMessage, href)
+      return buildErrorResponse(h, noFileErrorMessage, route, elementID)
     case constants.uploadErrors.unsupportedFileExt:
-      return buildErrorResponseWithTwoMessages(h, unsupportedFileExtErrorMessage || 'The selected file must be a DOC, DOCX or PDF', optionalErrorMessage, href)
+      return buildErrorResponseWithTwoMessages(h, unsupportedFileExtErrorMessage || 'The selected file must be a DOC, DOCX or PDF', optionalErrorMessage, route, elementID)
     case constants.uploadErrors.notValidMetric:
-      return buildErrorResponse(h, 'The selected file is not a valid Metric', href)
+      return buildErrorResponse(h, 'The selected file is not a valid Metric', route, elementID)
     default:
       if (err instanceof ThreatScreeningError) {
-        return buildErrorResponse(h, constants.uploadErrors.malwareScanFailed, href)
+        return buildErrorResponse(h, constants.uploadErrors.malwareScanFailed, route, elementID)
       } else if (err instanceof MalwareDetectedError) {
-        return buildErrorResponse(h, constants.uploadErrors.threatDetected, href)
+        return buildErrorResponse(h, constants.uploadErrors.threatDetected, route, elementID)
       } else {
-        return buildErrorResponseWithTwoMessages(h, constants.uploadErrors.uploadFailure, optionalErrorMessage, href)
+        return buildErrorResponseWithTwoMessages(h, constants.uploadErrors.uploadFailure, optionalErrorMessage, route, elementID)
       }
   }
 }
 
-function buildErrorResponse (h, message, href) {
-  return h.view(href, {
+function buildErrorResponse (h, message, route, elementID) {
+  return h.view(route, {
     err: [{
       text: message,
-      href
+      href: elementID
     }]
   })
 }
 
-function buildErrorResponseWithTwoMessages (h, message, message2 = 'default', href) {
+function buildErrorResponseWithTwoMessages (h, message, message2 = 'default', route, elementID) {
   if (message2 === 'default') {
-    return h.view(href, {
+    return h.view(route, {
       err: [{
         text: message,
-        href
+        href: elementID
+
       }]
     })
   } else {
-    return h.view(href, {
+    return h.view(route, {
       err: [{
         text: message2,
-        href
+        href: elementID
+
       }, {
         text: message,
-        href
+        href: elementID
       }]
     })
   }
