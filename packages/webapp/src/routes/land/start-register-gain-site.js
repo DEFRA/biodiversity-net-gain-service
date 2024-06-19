4d28c471c04c40003586eca15518a5797b6af98c
Adam Kay <thousand_leaves@hotmail.com>
2024-06-19 22:02:52 +0100

    WIP: radio buttons and save and continue button

--- packages/webapp/src/routes/land/start-register-gain-site.js
+++ packages/webapp/src/routes/land/start-register-gain-site.js
@@ -1,10 +1,37 @@
 import constants from '../../utils/constants.js'
 
-export default {
-  method: 'GET',
-  path: constants.routes.START_REGISTER_GAIN_SITE,
-  handler: async (request, h) => {
+const handlers = {
+  get: async (request, h) => {
     return h.view(constants.views.START_REGISTER_GAIN_SITE, {
     })
+  },
+  post: async (request, h) => {
+    // const applicationType = request.payload.applicantType
+    console.log('POST request received')
+    const applicationType = 'registration'
+    console.log('applicationType:', applicationType)
+    if (applicationType === 'registration') {
+      console.log('Redirecting to:', constants.routes.BIODIVERSITY_GAIN_SITES)
+      return h.redirect(constants.routes.BIODIVERSITY_GAIN_SITES)
+    } else {
+      console.log('Rendering view with error')
+      return h.view(constants.views.START_REGISTER_GAIN_SITE, {
+        err: [{
+          text: 'Select an option to continue',
+          href: '#applicant-type'
+        }]
+      })
+    }
   }
 }
+
+export default [{
+  method: 'GET',
+  path: constants.routes.START_REGISTER_GAIN_SITE,
+  handler: handlers.get
+},
+{
+  method: 'POST',
+  path: constants.routes.START_REGISTER_GAIN_SITE,
+  handler: handlers.post
+}]
