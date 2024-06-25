613db86f40ace6f9dad35544aeb0f33b5014a9fe
Adam Kay <thousand_leaves@hotmail.com>
2024-06-25 09:09:20 +0100

    removed comments

--- packages/webapp/src/routes/land/start-register-gain-site.js
+++ packages/webapp/src/routes/land/start-register-gain-site.js
@@ -6,18 +6,12 @@ const handlers = {
     })
   },
   post: async (request, h) => {
-    console.log('PAYLOAD::::')
-    console.log(JSON.stringify(request.payload, null, 2))
-    console.log('POST request received')
     const applicationType = request.payload.applicationType
     if (applicationType === 'registration') {
-      console.log('Redirecting to:', constants.routes.BIODIVERSITY_GAIN_SITES)
       return h.redirect(constants.routes.BIODIVERSITY_GAIN_SITES)
     } else if (applicationType === 'combined-case') {
-      console.log('Redirecting to:', constants.routes.COMBINED_CASE_PROJECTS)
       return h.redirect(constants.routes.COMBINED_CASE_PROJECTS)
     } else {
-      console.log('Rendering view with error')
       return h.view(constants.views.START_REGISTER_GAIN_SITE, {
         err: [{
           text: 'Select an option to continue',
