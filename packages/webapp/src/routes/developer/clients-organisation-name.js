6a15df8af739e6d3ce5b57d1a406287fbe63d28a
Stewart Jumbe <stewartjumbe@gmail.com>
2024-06-28 16:45:18 +0100

    add # for href to elementID

--- packages/webapp/src/routes/developer/clients-organisation-name.js
+++ packages/webapp/src/routes/developer/clients-organisation-name.js
@@ -14,14 +14,14 @@ const handlers = {
     if (!organisationName) {
       organisationNameErr = [{
         text: "Enter your client's organisation name",
-        href: 'organisationName'
+        href: '#organisationName'
       }]
     }
 
     if (organisationName && organisationName.length > 50) {
       organisationNameErr = [{
         text: 'Organisation name must be 50 characters or fewer',
-        href: 'organisationName'
+        href: '#organisationName'
       }]
     }
 
