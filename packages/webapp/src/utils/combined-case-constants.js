b0041a9629af1af8e1199aa337ce176b5bb30027
John Barnard <John.Barnard@defra.gov.uk>
2024-07-05 10:53:28 +0100

    Changed URL path

--- packages/webapp/src/utils/combined-case-constants.js
+++ packages/webapp/src/utils/combined-case-constants.js
@@ -4,7 +4,7 @@ const routes = {
   COMBINED_CASE_TASK_LIST: 'combined-case/tasklist',
   COMBINED_CASE_CHECK_AND_SUBMIT: 'combined-case/check-and-submit',
   COMBINED_CASE_UPLOAD_ALLOCATION_METRIC: 'combined-case/upload-allocation-metric',
-  COMBINED_CASE_CHECK_UPLOAD_ALLOCATION_METRIC: 'combined-case/check-allocation-metric-file'
+  COMBINED_CASE_CHECK_UPLOAD_ALLOCATION_METRIC: 'combined-case/check-allocation-metric'
 }
 const views = Object.fromEntries(
   Object.entries(routes).map(([k, v]) => [k, v.substring(1)])
