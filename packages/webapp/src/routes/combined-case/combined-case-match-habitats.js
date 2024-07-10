70e146375a6799f8ae3ccdd735b16e51fd69d6ca
Adam Kay <thousand_leaves@hotmail.com>
2024-07-10 13:13:53 +0100

    adds pagination logic and correct parameterisation for some of the values

--- packages/webapp/src/routes/combined-case/combined-case-match-habitats.js
+++ packages/webapp/src/routes/combined-case/combined-case-match-habitats.js
@@ -1,53 +1,11 @@
 import constants from '../../utils/constants.js'
 
-// Function to get the number of objects in d1 array from metric data
 const getNumberOfPages = (metricData) => {
   const numberOfPages = metricData.d1.length - 1
   console.log('Number of objects in d1 array:', numberOfPages)
   return numberOfPages
 }
 
-// Function to get the required habitat info from metric data
-// const getHabitats = (metricData) => {
-//   const habitats = metricData.d1.slice(0, -1).map((item) => ({
-//     broadHabitat: item['Broad habitat'],
-//     habitatType: item['Habitat type'],
-//     area: item['Area (hectares)'],
-//     condition: item.Condition
-//   }))
-//   console.log('Habitats:', habitats)
-//   return habitats
-// }
-
-// // Function to get the broad habitat info from metric data
-// const getBroadHabitat = (metricData) => {
-//   const broadHabitat = metricData.d1.slice(0, -1).map((item) => item['Broad habitat'])
-//   console.log('Broad habitat:', broadHabitat)
-//   return broadHabitat
-// }
-
-// // Function to get the habitat type info from metric data
-// const getHabitatType = (metricData) => {
-//   const habitatType = metricData.d1.slice(0, -1).map((item) => item['Habitat type'])
-//   console.log('Habitat type:', habitatType)
-//   return habitatType
-// }
-
-// // Function to get the area from metric data
-// const getArea = (metricData) => {
-//   const area = metricData.d1.slice(0, -1).map((item) => item['Area (hectares)'])
-//   console.log('Area:', area)
-//   return area
-// }
-
-// // Function to get the condition from metric data
-// const getCondition = (metricData) => {
-//   const condition = metricData.d1.slice(0, -1).map((item) => item.Condition)
-//   console.log('Condition:', condition)
-//   return condition
-// }
-
-// Function to get detailed habitat information from metric data
 const getHabitatDetails = (metricData) => {
   const details = metricData.d1.slice(0, -1).map((item) => ({
     broadHabitat: item['Broad habitat'],
@@ -67,13 +25,27 @@ const getHabitatDetails = (metricData) => {
 const handlers = {
   get: async (request, h) => {
     const metricData = request.yar.get(constants.redisKeys.METRIC_DATA)
-    console.log('Metric data:', metricData)
+    const currentPage = parseInt(request.query.page || '1', 10)
     const numberOfPages = getNumberOfPages(metricData)
+    console.log('Metric data:', metricData)
     const { broadHabitat, habitatType, area, condition } = getHabitatDetails(metricData)
-    return h.view(constants.views.COMBINED_CASE_MATCH_HABITATS, { numberOfPages, broadHabitat, habitatType, area, condition })
+
+    const safeCurrentPage = Math.max(1, Math.min(currentPage, numberOfPages))
+
+    return h.view(constants.views.COMBINED_CASE_MATCH_HABITATS, {
+      numberOfPages,
+      currentPage: safeCurrentPage,
+      broadHabitat: broadHabitat[safeCurrentPage - 1],
+      habitatType: habitatType[safeCurrentPage - 1],
+      area: area[safeCurrentPage - 1],
+      condition: condition[safeCurrentPage - 1]
+    })
   },
   post: async (request, h) => {
-    return h.redirect(constants.routes.COMBINED_CASE_MATCH_HABITATS)
+    // Logic to handle form submission, potentially updating session state or database
+    // Redirect to the next page based on the user's selection
+    const nextPage = parseInt(request.payload.currentPage, 10) + 1
+    return h.redirect(`${constants.routes.COMBINED_CASE_MATCH_HABITATS}?page=${nextPage}`)
   }
 }
 
