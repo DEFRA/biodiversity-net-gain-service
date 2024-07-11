3ae77badd26c8b2b51947fae4b67409b02293c72
John Barnard <John.Barnard@defra.gov.uk>
2024-07-11 10:53:26 +0100

    Shared habitat info journey validation

--- packages/webapp/src/journey-validation/registration/habitat-info.js
+++ packages/webapp/src/journey-validation/registration/habitat-info.js
@@ -1,71 +1,41 @@
 import constants from '../../utils/constants.js'
 import {
   ANY,
-  routeDefinition,
   journeyStep,
   journeyStepFromRoute
 } from '../utils.js'
-import { getValidReferrerUrl } from '../../utils/helpers.js'
-import { FormError } from '../../utils/form-error.js'
+import {
+  createUploadMetricRoute,
+  createCheckUploadMetricRoute,
+  createCheckHabitatBaselineRoute,
+  createCheckHabitatCreatedRoute,
+  createCheckMetricDetailsRoute
+} from '../shared/habitat-info.js'
 
-const UPLOAD_METRIC = routeDefinition(
+const UPLOAD_METRIC = createUploadMetricRoute(
   constants.routes.UPLOAD_METRIC,
-  [
-    constants.redisKeys.METRIC_LOCATION,
-    constants.redisKeys.METRIC_FILE_SIZE,
-    constants.redisKeys.METRIC_FILE_TYPE,
-    constants.redisKeys.METRIC_DATA
-  ],
-  () => {
-    return constants.routes.CHECK_UPLOAD_METRIC
-  }
+  constants.routes.CHECK_UPLOAD_METRIC
 )
 
-const CHECK_UPLOAD_METRIC = routeDefinition(
+const CHECK_UPLOAD_METRIC = createCheckUploadMetricRoute(
   constants.routes.CHECK_UPLOAD_METRIC,
-  [constants.redisKeys.METRIC_FILE_CHECKED],
-  (session) => {
-    const checkUploadMetric = session.get(constants.redisKeys.METRIC_FILE_CHECKED)
-    if (checkUploadMetric === 'no') {
-      return constants.routes.UPLOAD_METRIC
-    } else if (checkUploadMetric === 'yes') {
-      session.set(constants.redisKeys.METRIC_UPLOADED_ANSWER, true)
-      const referrerUrl = getValidReferrerUrl(session, constants.LAND_METRIC_VALID_REFERRERS)
-      return (referrerUrl || constants.routes.CHECK_HABITAT_BASELINE)
-    } else {
-      const message = 'Select yes if this is the correct file'
-      throw new FormError(message, {
-        text: message,
-        href: '#check-upload-correct-yes'
-      })
-    }
-  }
+  constants.routes.UPLOAD_METRIC,
+  constants.routes.CHECK_HABITAT_BASELINE
 )
 
-const CHECK_HABITAT_BASELINE = routeDefinition(
+const CHECK_HABITAT_BASELINE = createCheckHabitatBaselineRoute(
   constants.routes.CHECK_HABITAT_BASELINE,
-  [constants.redisKeys.METRIC_HABITAT_BASELINE_CHECKED],
-  (session) => {
-    const referrerUrl = getValidReferrerUrl(session, constants.LAND_METRIC_VALID_REFERRERS)
-    return (referrerUrl || constants.routes.CHECK_HABITAT_CREATED)
-  }
+  constants.routes.CHECK_HABITAT_CREATED
 )
 
-const CHECK_HABITAT_CREATED = routeDefinition(
+const CHECK_HABITAT_CREATED = createCheckHabitatCreatedRoute(
   constants.routes.CHECK_HABITAT_CREATED,
-  [constants.redisKeys.METRIC_HABITAT_CREATED_CHECKED],
-  (session) => {
-    const referrerUrl = getValidReferrerUrl(session, constants.LAND_METRIC_VALID_REFERRERS)
-    return (referrerUrl || constants.routes.CHECK_METRIC_DETAILS)
-  }
+  constants.routes.CHECK_METRIC_DETAILS
 )
 
-const CHECK_METRIC_DETAILS = routeDefinition(
+const CHECK_METRIC_DETAILS = createCheckMetricDetailsRoute(
   constants.routes.CHECK_METRIC_DETAILS,
-  [],
-  () => {
-    return constants.routes.REGISTER_LAND_TASK_LIST
-  }
+  constants.routes.REGISTER_LAND_TASK_LIST
 )
 
 const habitatInfoJourneys = [
