62853abf391c974cca667291ed81bf39f960f8cb
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2024-07-15 21:21:24 +0100

    Add new and continued application routes

--- packages/webapp/src/utils/combined-case-constants.js
+++ packages/webapp/src/utils/combined-case-constants.js
@@ -2,7 +2,9 @@
 const routes = {
   COMBINED_CASE_TASK_LIST: 'combined-case/tasklist',
   COMBINED_CASE_CHECK_AND_SUBMIT: 'combined-case/check-and-submit',
-  COMBINED_CASE_PROJECTS: 'combined-case/combined-case-projects'
+  COMBINED_CASE_PROJECTS: 'combined-case/combined-case-projects',
+  COMBINED_CASE_CONTINUE_PROJECT: 'combined-case/continue-combined-case-project',
+  COMBINED_CASE_NEW_PROJECT: 'combined-case/new-combined-case-project'
 }
 
 const views = Object.fromEntries(
