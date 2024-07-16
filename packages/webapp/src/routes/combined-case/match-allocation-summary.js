0377ff1d5c5823c85f06ae85cc4a93188d54a5a8
John Barnard <John.Barnard@defra.gov.uk>
2024-07-16 16:24:38 +0100

    Habitat data

--- packages/webapp/src/routes/combined-case/match-allocation-summary.js
+++ packages/webapp/src/routes/combined-case/match-allocation-summary.js
@@ -2,12 +2,40 @@ import constants from '../../utils/constants.js'
 
 const handlers = {
   get: (request, h) => {
-    return h.view(constants.views.COMBINED_CASE_MATCH_ALLOCATION_SUMMARY,
-      {
-        habitatA: '',
-        habitatB: '',
-        habitatC: '5 hectares / Moderate Condition'
-      })
+    // todo get from session and structure might change
+    const habitats = {
+      proposed: [
+        {
+          habitatType: 'Grassland - Other neutral grassland',
+          area: '2 hectares',
+          condition: 'Moderate Condition'
+        },
+        {
+          habitatType: 'Heathland and shrub - Mixed scrub',
+          area: '0.05 hectares',
+          condition: 'Good Condition'
+        },
+        {
+          habitatType: 'Woodland and forest - Lowland mixed deciduous woodland',
+          area: '20 hectares',
+          condition: 'Poor Condition'
+        }
+      ]
+    }
+
+    const matchedHabitats = habitats?.proposed.map((habitat, index) => {
+      return {
+        text: `${habitat.habitatType} (${habitat.area} / ${habitat.condition})`,
+        value: '', // TODO not sure where this comes from
+        valueDataTestId: `habitat-${index + 1}`,
+        href: `todo?page=${index + 1}`, // todo
+        visuallyHiddenText: habitat.habitatType,
+        classes: '',
+        show: true
+      }
+    })
+
+    return h.view(constants.views.COMBINED_CASE_MATCH_ALLOCATION_SUMMARY, { matchedHabitats })
   },
   post: async (request, h) => {
     return h.redirect(constants.routes.COMBINED_CASE_MATCH_ALLOCATION_SUMMARY)
