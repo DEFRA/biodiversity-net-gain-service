f4c38a20c3d3d10bba0705375a8f8ddfbd5d012c
Adam Kay <thousand_leaves@hotmail.com>
2024-07-30 10:58:39 +0100

    completed unit test

--- packages/webapp/src/routes/__tests__/combined-case/application-submitted.spec.js
+++ packages/webapp/src/routes/__tests__/combined-case/application-submitted.spec.js
@@ -19,6 +19,22 @@ describe(url, () => {
       }
       await getHandler({ headers: { referer: 'http://localhost/combined-case/check-and-submit' }, yar: session }, h)
       expect(viewArgs[0]).toEqual(constants.views.APPLICATION_SUBMITTED)
+      expect(viewArgs[1].applicationReference).toEqual(combinedCaseReference)
+    })
+
+    it(`should render the ${url.substring(1)} view with formatted sort code`, async () => {
+      const getHandler = applicationSubmitted[0].handler
+      const session = new Session()
+      session.set(constants.redisKeys.COMBINED_CASE_APP_REFERENCE, combinedCaseReference)
+      let viewArgs = ''
+      const h = {
+        view: (...args) => {
+          viewArgs = args
+        }
+      }
+      await getHandler({ headers: { referer: 'http://localhost/combined-case/check-and-submit' }, yar: session }, h)
+      expect(viewArgs[0]).toEqual(constants.views.APPLICATION_SUBMITTED)
+      expect(viewArgs[1].bacs.sortCode).toEqual('12 34 56')
     })
 
     it('should render payment fee details', async () => {
