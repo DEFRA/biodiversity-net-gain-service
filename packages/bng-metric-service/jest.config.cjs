cf6a902b0cc89039dc757fb55b7890d2a9bcc1ea
Stewart Jumbe <stewartjumbe@gmail.com>
2024-07-31 09:49:47 +0100

    correct config module mapper paths

--- packages/bng-metric-service/jest.config.cjs
+++ packages/bng-metric-service/jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    '@defra/bng-connectors-lib/azure-storage': '<rootDir>/node_modules/@defra/bng-azure-storage-test-utils/node_modules/@defra/bng-connectors-lib/src/helpers/azure-storage.js'
+    '@defra/bng-connectors-lib/azure-storage': '<rootDir>/../../packages/bng-connectors-lib/src/helpers/azure-storage.js'
   },
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   coveragePathIgnorePatterns: ['<rootDir>/src/azure-service-bus-connector.js']
