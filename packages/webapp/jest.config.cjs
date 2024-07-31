d267a19cb4e0268896814e068354f39487e57f3e
Stewart Jumbe <stewartjumbe@gmail.com>
2024-07-31 12:00:18 +0100

    update path for azure-storage.js to node modules

--- packages/webapp/jest.config.cjs
+++ packages/webapp/jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   coveragePathIgnorePatterns: ['__tests__/helpers'],
   moduleNameMapper: {
-    '@defra/bng-connectors-lib/azure-storage': '<rootDir>/../../packages/bng-connectors-lib/src/helpers/azure-storage.js',
+    '@defra/bng-connectors-lib/azure-storage': '<rootDir>/../../node_modules/@defra/bng-connectors-lib/src/helpers/azure-storage.js',
     '@defra/bng-utils-lib': '<rootDir>/../../node_modules/@defra/bng-utils-lib/src/utils.js'
   },
   setupFiles: ['<rootDir>/.jest/test.env.js'],
