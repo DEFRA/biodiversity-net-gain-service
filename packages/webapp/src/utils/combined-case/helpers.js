6b347625f2e6f9b61cf9d4593afa7e147176c8fd
jd-barnard <160484642+jd-barnard@users.noreply.github.com>
2024-08-02 11:16:36 +0100

    BNGP-5300 Create helper function to match habitats (#820)
    * Combined case habitat match helper
    
    * Isolate modules per test
    
    * Test getHabitatType, getState and getModule helper functions
    
    * Removed unused function

---
+++ packages/webapp/src/utils/combined-case/helpers.js
@@ -0,0 +1,128 @@
+import combinedCaseConstants from '../combined-case-constants.js'
+
+let habitatReferenceCounter = 0
+let ownReferenceCounter = 0
+
+const generateHabitatReference = () => `HAB-00000000-${habitatReferenceCounter++}`
+
+const generateOwnReference = () => `${ownReferenceCounter++}`
+
+const getHabitatType = (identifier, details) => {
+  switch (identifier) {
+    case 'd2':
+      if (details['Broad habitat'] && details['Proposed habitat']) {
+        return `${details['Broad habitat']} - ${details['Proposed habitat']}`
+      }
+      return undefined
+    case 'd3':
+      if (details['Proposed Broad Habitat'] && details['Proposed habitat']) {
+        return `${details['Proposed Broad Habitat']} - ${details['Proposed habitat']}`
+      }
+      return undefined
+    case 'e2':
+      return details['Habitat type']
+    case 'e3':
+      return details['Proposed habitat']
+    case 'f2':
+      return details['Watercourse type']
+    case 'f3':
+      return details['Proposed habitat']
+  }
+}
+
+const getState = identifier => {
+  switch (identifier.charAt(0)) {
+    case 'd':
+      return 'Habitat'
+    case 'e':
+      return 'Hedge'
+    case 'f':
+      return 'Watercourse'
+  }
+}
+
+const getModule = identifier => {
+  switch (identifier.charAt(identifier.length - 1)) {
+    case '1':
+      return 'Baseline'
+    case '2':
+      return 'Created'
+    case '3':
+      return 'Enhanced'
+  }
+}
+
+const processMetricData = session => {
+  const registrationMetricData = session.get(combinedCaseConstants.redisKeys.COMBINED_CASE_REGISTRATION_METRIC_DATA)
+  const allocationMetricData = session.get(combinedCaseConstants.redisKeys.COMBINED_CASE_ALLOCATION_METRIC_DATA)
+  const registrationHabitats = []
+  const allocationHabitats = []
+  const sheets = ['d2', 'd3', 'e2', 'e3', 'f2', 'f3']
+
+  const extractHabitats = (metricData, habitats, isAllocation) => {
+    sheets.forEach(sheet => {
+      if (metricData[sheet]) {
+        metricData[sheet].forEach(habitat => {
+          const habitatType = getHabitatType(sheet, habitat)
+          const condition = habitat.Condition
+
+          if (habitatType && condition) {
+            habitats.push({
+              habitatType,
+              condition,
+              module: getModule(sheet),
+              state: getState(sheet),
+              id: isAllocation ? generateOwnReference() : generateHabitatReference(),
+              size: habitat['Length (km)'] ?? habitat['Area (hectares)'],
+              measurementUnits: 'Length (km)' in habitat ? 'kilometres' : 'hectares',
+              processed: false
+            })
+          }
+        })
+      }
+    })
+  }
+
+  extractHabitats(registrationMetricData, registrationHabitats, false)
+  extractHabitats(allocationMetricData, allocationHabitats, true)
+
+  session.set(combinedCaseConstants.redisKeys.COMBINED_CASE_REGISTRATION_HABITATS, registrationHabitats)
+  session.set(combinedCaseConstants.redisKeys.COMBINED_CASE_ALLOCATION_HABITATS, allocationHabitats)
+}
+
+const habitatDescription = habitat =>
+  `${habitat.habitatType} || ${habitat.condition} || ${habitat.size} ${habitat.measurementUnits} || ${habitat.module} || ${habitat.state}`
+
+const getMatchingHabitats = (habitat, habitatList) => habitatList.filter(h =>
+  h.state === habitat.state &&
+  h.module === habitat.module &&
+  h.habitatType === habitat.habitatType &&
+  h.condition === habitat.condition
+)
+
+const summariseHabitatMatches = (registrationHabitats, allocationHabitats) => {
+  const matches = {}
+
+  registrationHabitats.forEach(habitat => {
+    const allocationHabitat = allocationHabitats.find(h => h.id === habitat.id)
+
+    matches[habitat.id] = {
+      registration: habitatDescription(habitat),
+      allocation: allocationHabitat && habitatDescription(allocationHabitat)
+    }
+  })
+
+  return matches
+}
+
+export {
+  generateOwnReference,
+  generateHabitatReference,
+  processMetricData,
+  habitatDescription,
+  getMatchingHabitats,
+  summariseHabitatMatches,
+  getHabitatType,
+  getState,
+  getModule
+}
