413d06e26958a11203086d3126b792a781801ed9
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-08-14 15:03:12 +0100

    Remove `geospatial-land-boundary` upload type support from Azure function `ProcessTrustedFile`

--- packages/azure-functions/ProcessTrustedFile/__tests__/index.spec.js
+++ packages/azure-functions/ProcessTrustedFile/__tests__/index.spec.js
@@ -1,14 +1,10 @@
 import processTrustedFile from '../index.mjs'
 import { getContext } from '../../.jest/setup.js'
-import { CoordinateSystemValidationError, ValidationError } from '@defra/bng-errors-lib'
 import { Readable } from 'stream'
 import { blobStorageConnector } from '@defra/bng-connectors-lib'
 import fs from 'fs'
 jest.mock('@defra/bng-connectors-lib')
 
-const GEOJSON_FILE_EXTENSION = '.geojson'
-const GEOPACKAGE_FILE_EXTENSION = '.gpkg'
-const ZIP_FILE_EXTENSION = '.zip'
 const METRIC_FILE_EXTENSION = '.xlsx'
 const DEVELOPER_METRIC_UPLOAD_TYPE = 'developer-metric-upload'
 const CREDITS_PURCHASE_METRIC_UPLOAD_TYPE = 'credits-metric-upload'
@@ -16,30 +12,6 @@ const LOJ_METRIC_UPLOAD_TYPE = 'metric-upload'
 const REPROJECTED_TO_OSGB36 = 'reprojectedToOsgb36'
 
 describe('Trusted file processing', () => {
-  it('should process a known WGS84 geospatial upload type in a compressed file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(ZIP_FILE_EXTENSION, '4326', done)
-  })
-
-  it('should process a known OSGB36 geospatial upload type in a compressed file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(ZIP_FILE_EXTENSION, '27700', done)
-  })
-
-  it('should process a WGS84 GeoJSON file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(GEOJSON_FILE_EXTENSION, '4326', done)
-  })
-
-  it('should process an OSGB36 GeoJSON file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(GEOJSON_FILE_EXTENSION, '27700', done)
-  })
-
-  it('should process a WGS84 Geopackage file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(GEOPACKAGE_FILE_EXTENSION, '4326', done)
-  })
-
-  it('should process an OSGB36 Geopackage file.', done => {
-    performValidGeospatialLandBoundaryProcessingTest(GEOPACKAGE_FILE_EXTENSION, '27700', done)
-  })
-
   it('should process a known metric file.', done => {
     performValidMetricFileProcessingTest(METRIC_FILE_EXTENSION, done)
   })
@@ -50,40 +22,6 @@ describe('Trusted file processing', () => {
 
   it('should process a known credits metric file. ', done => {
     performCreditsValidMetricFileProcessingTest(METRIC_FILE_EXTENSION, done)
-  })
-
-  it('should respond to a coordinate reference system validation error. ', done => {
-    const config = {
-      expectedRes: {
-        authorityKey: 'mock authority key',
-        errorCode: 'mock error code'
-      },
-      fileExtension: GEOPACKAGE_FILE_EXTENSION,
-      processLandBoundaryMockFunction: throwCoordinateSystemValidationError
-    }
-    performInvalidGeospatialLandBoundaryProcessingTest(config, done)
-  })
-
-  it('should respond to a validation error. ', done => {
-    const config = {
-      expectedRes: {
-        errorCode: 'mock error code'
-      },
-      fileExtension: GEOPACKAGE_FILE_EXTENSION,
-      processLandBoundaryMockFunction: throwValidationError
-    }
-    performInvalidGeospatialLandBoundaryProcessingTest(config, done)
-  })
-
-  it('should respond to an error. ', done => {
-    const config = {
-      expectedRes: {
-        errorMessage: 'mock error message'
-      },
-      fileExtension: GEOPACKAGE_FILE_EXTENSION,
-      processLandBoundaryMockFunction: throwError
-    }
-    performInvalidGeospatialLandBoundaryProcessingTest(config, done)
   })
 
   it('should not process an unsupported file type.', done => {
@@ -261,81 +199,6 @@ const buildConfig = (fileExtension, uploadType, epsg) => {
   return config
 }
 
-const performValidGeospatialLandBoundaryProcessingTest = (fileExtension, epsg, done) => {
-  jest.isolateModules(async () => {
-    try {
-      const context = getContext()
-      jest.mock('@defra/bng-geoprocessing-service')
-      jest.mock('@defra/bng-connectors-lib')
-      const testConfig = buildConfig(fileExtension, 'geospatial-land-boundary', epsg)
-      const geoprocessingService = await import('@defra/bng-geoprocessing-service')
-      const { blobStorageConnector } = await import('@defra/bng-connectors-lib')
-      geoprocessingService.processLandBoundary = jest.fn().mockImplementation(async (logger, config) => {
-        return testConfig.mapConfig
-      })
-
-      blobStorageConnector.getBlobSizeInBytes = jest.fn().mockImplementation(async config => {
-        return 500
-      })
-
-      const spy = jest.spyOn(blobStorageConnector, 'moveBlob')
-      let expectedNumberOfMoveBlobCalls
-
-      if (epsg === '27700') {
-        if (fileExtension === GEOJSON_FILE_EXTENSION) {
-          // OSGB36 GeoJSON uploads do not require converting to GeoJSON format. As such moveBlob should not be called in this scenario.
-          expectedNumberOfMoveBlobCalls = 0
-        } else {
-          // OSGB36 non-GeoJSON uploads need converting to GeoJSON. As such moveBlob should be called once in this scenario.
-          expectedNumberOfMoveBlobCalls = 1
-        }
-      } else if (fileExtension === GEOJSON_FILE_EXTENSION) {
-        // WGS84 GeoJSON uploads require reprojection to OSGB36. As such moveBlob should be called once in this scenario.
-        expectedNumberOfMoveBlobCalls = 1
-      } else {
-        // WGS84 non-GeoJSON uploads require conversion to GeoJSON and reprojection to OSGB36. As such moveBlob should be called twice in this scenario.
-        expectedNumberOfMoveBlobCalls = 2
-      }
-
-      await processTrustedFile(context, testConfig.req)
-      expect(context.res.status).toEqual(200)
-      expect(JSON.parse(context.res.body)).toEqual(testConfig.expectedRes.body)
-      expect(spy).toHaveBeenCalledTimes(expectedNumberOfMoveBlobCalls)
-      done()
-    } catch (e) {
-      done(e)
-    }
-  })
-}
-
-const performInvalidGeospatialLandBoundaryProcessingTest = (config, done) => {
-  jest.isolateModules(async () => {
-    try {
-      jest.mock('@defra/bng-geoprocessing-service')
-      jest.mock('@defra/bng-connectors-lib')
-      const testConfig = buildConfig(config.fileExtension, 'geospatial-land-boundary')
-      testConfig.expectedRes.body = config.expectedRes
-
-      const geoprocessingService = (await import('@defra/bng-geoprocessing-service'))
-
-      geoprocessingService.processLandBoundary = jest.fn().mockImplementation(async (logger, processLandBundaryConfig) => {
-        config.processLandBoundaryMockFunction(testConfig)
-      })
-
-      const spy = jest.spyOn(blobStorageConnector, 'deleteBlobIfExists')
-
-      await processTrustedFile(getContext(), testConfig.req)
-      const context = getContext()
-      expect(context.res.status).toEqual(200)
-      expect(JSON.parse(context.res.body)).toEqual(testConfig.expectedRes.body)
-      expect(spy).toHaveBeenCalledTimes(1)
-      done()
-    } catch (e) {
-      done(e)
-    }
-  })
-}
-
 const performValidMetricFileProcessingTest = (fileExtension, done) => {
   jest.isolateModules(async () => {
     try {
@@ -356,22 +219,6 @@ const performValidMetricFileProcessingTest = (fileExtension, done) => {
       done(e)
     }
   })
-}
-
-const throwCoordinateSystemValidationError = testConfig => {
-  const authorityKey = testConfig.expectedRes.body.authorityKey
-  const errorCode = testConfig.expectedRes.body.errorCode
-  throw new CoordinateSystemValidationError(authorityKey, errorCode)
-}
-
-const throwValidationError = testConfig => {
-  const errorCode = testConfig.expectedRes.body.errorCode
-  throw new ValidationError(errorCode)
-}
-
-const throwError = testConfig => {
-  const errorMessage = testConfig.expectedRes.body.errorMessage
-  throw new Error(errorMessage)
 }
 
 const performDeveloperValidMetricFileProcessingTest = (fileExtension, done) => {
