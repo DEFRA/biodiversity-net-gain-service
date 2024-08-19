b2727e8284155175713c00bf4cb19959be8adfc1
Adam Kay <thousand_leaves@hotmail.com>
2024-08-19 12:36:11 +0100

    add cc application-submitted route

--- packages/webapp/src/routes/combined-case/check-and-submit.js
+++ packages/webapp/src/routes/combined-case/check-and-submit.js
@@ -22,7 +22,6 @@ const handlers = {
     }
 
     const applicationDetails = application(request.yar, request.auth.credentials.account).combinedCase
-    console.log(JSON.stringify(applicationDetails, null, 2))
     const claims = request.auth.credentials.account.idTokenClaims
     const { currentOrganisation } = getOrganisationDetails(claims)
     return h.view(
@@ -59,7 +58,7 @@ const handlers = {
 
     const result = await postJson(`${constants.AZURE_FUNCTION_APP_URL}/processcombinedcaseapplication`, value)
     request.yar.set(constants.redisKeys.COMBINED_CASE_APPLICATION_REFERENCE, result.applicationReference)
-    return h.redirect(constants.routes.APPLICATION_SUBMITTED)
+    return h.redirect(constants.routes.COMBINED_CASE_CONFIRMATION)
   }
 }
 
