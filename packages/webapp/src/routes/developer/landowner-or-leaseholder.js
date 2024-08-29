9bb71f5b49aa122fc14770092795d93903a2c4ff
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-08-29 12:58:33 +0100

    Fix "landowner or leaseholder" no change issue

--- packages/webapp/src/routes/developer/landowner-or-leaseholder.js
+++ packages/webapp/src/routes/developer/landowner-or-leaseholder.js
@@ -12,12 +12,8 @@ const handlers = {
   post: async (request, h) => {
     const landownerOrLeaseholder = request.payload.landownerOrLeaseholder
     const isApplicantAgent = request.yar.get(constants.redisKeys.DEVELOPER_IS_AGENT)
-    request.yar.set(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER, landownerOrLeaseholder)
-    if (landownerOrLeaseholder && isApplicantAgent === constants.APPLICANT_IS_AGENT.YES) {
-      return h.redirect(constants.routes.DEVELOPER_CLIENT_INDIVIDUAL_ORGANISATION)
-    } else if (landownerOrLeaseholder && isApplicantAgent === constants.APPLICANT_IS_AGENT.NO) {
-      return h.redirect(constants.routes.DEVELOPER_APPLICATION_BY_INDIVIDUAL_OR_ORGANISATION)
-    } else {
+
+    if (!landownerOrLeaseholder) {
       return h.view(constants.views.DEVELOPER_LANDOWNER_OR_LEASEHOLDER, {
         err: [{
           text: `Select yes if ${isApplicantAgent === constants.APPLICANT_IS_AGENT.YES ? 'your client is' : 'you are'} the landowner or leaseholder`,
@@ -27,6 +23,20 @@ const handlers = {
         isApplicantAgent
       })
     }
+
+    // If there's already a response and it matches the new response then we've likely entered from Check And Submit so
+    // don't want to go any further (otherwise we're just confirming additional questions that we don't need to). We
+    // likely started at Check And Submit so we'll go back there if the key is set, but use the task list as a fallback.
+    if (request.yar.get(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER) === landownerOrLeaseholder) {
+      const returnRoute = request.yar.get(constants.redisKeys.CHECK_AND_SUBMIT_JOURNEY_ROUTE) || constants.routes.DEVELOPER_TASKLIST
+      return h.redirect(returnRoute)
+    }
+
+    request.yar.set(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER, landownerOrLeaseholder)
+
+    return isApplicantAgent === constants.APPLICANT_IS_AGENT.YES
+      ? h.redirect(constants.routes.DEVELOPER_CLIENT_INDIVIDUAL_ORGANISATION)
+      : h.redirect(constants.routes.DEVELOPER_APPLICATION_BY_INDIVIDUAL_OR_ORGANISATION)
   }
 }
 
