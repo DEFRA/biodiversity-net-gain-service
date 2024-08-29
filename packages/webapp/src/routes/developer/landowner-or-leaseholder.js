0d3a3c3331e49032689ce766835a5cd879b6ca79
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-08-29 17:59:22 +0100

    Fix "landowner or leaseholder" change issue

--- packages/webapp/src/routes/developer/landowner-or-leaseholder.js
+++ packages/webapp/src/routes/developer/landowner-or-leaseholder.js
@@ -24,15 +24,30 @@ const handlers = {
       })
     }
 
-    // If there's already a response and it matches the new response then we've likely entered from Check And Submit so
+    const previousResponse = request.yar.get(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER)
+
+    // Force replay of full journey if switching between landowner and non-landowner application
+    if (previousResponse && previousResponse !== landownerOrLeaseholder) {
+      request.yar.clear(constants.redisKeys.REFERER)
+    }
+
+    // If there's already a response which matches the new response then we've likely entered from Check And Submit so
     // don't want to go any further (otherwise we're just confirming additional questions that we don't need to). We
-    // likely started at Check And Submit so we'll go back there if the key is set, but use the task list as a fallback.
-    if (request.yar.get(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER) === landownerOrLeaseholder) {
+    // go back there if the key is set, but use the task list as a fallback.
+    if (previousResponse === landownerOrLeaseholder) {
       const returnRoute = request.yar.get(constants.redisKeys.CHECK_AND_SUBMIT_JOURNEY_ROUTE) || constants.routes.DEVELOPER_TASKLIST
       return h.redirect(returnRoute)
     }
 
     request.yar.set(constants.redisKeys.DEVELOPER_LANDOWNER_OR_LEASEHOLDER, landownerOrLeaseholder)
+
+    // If there was a previous response and the response is now "yes" then we don't need any further info so we return
+    // to Check And Submit (since that's likely where they came from if they're confirming info), or the task list as a
+    // fallback.
+    if (previousResponse && landownerOrLeaseholder === constants.DEVELOPER_IS_LANDOWNER_OR_LEASEHOLDER.YES) {
+      const returnRoute = request.yar.get(constants.redisKeys.CHECK_AND_SUBMIT_JOURNEY_ROUTE) || constants.routes.DEVELOPER_TASKLIST
+      return h.redirect(returnRoute)
+    }
 
     return isApplicantAgent === constants.APPLICANT_IS_AGENT.YES
       ? h.redirect(constants.routes.DEVELOPER_CLIENT_INDIVIDUAL_ORGANISATION)
