f6ae183e29b71952e3e51af1e1696aaf15e39339
Adam Kay <thousand_leaves@hotmail.com>
2024-09-06 18:02:25 +0100

    Add navigation logic to the `check-metric-details.js` and `check-metric-details.html` file

--- packages/webapp/src/routes/land/check-metric-details.js
+++ packages/webapp/src/routes/land/check-metric-details.js
@@ -2,6 +2,7 @@ import path from 'path'
 import constants from '../../utils/constants.js'
 import { REGISTRATIONCONSTANTS } from '../../journey-validation/registration/task-sections.js'
 import { getIndividualTaskStatus, getNextStep } from '../../journey-validation/task-list-generator.js'
+
 const handlers = {
   get: async (request, h) => {
     const registrationTaskStatus = getIndividualTaskStatus(request.yar, REGISTRATIONCONSTANTS.HABITAT_INFO)
@@ -10,9 +11,17 @@ const handlers = {
     }
     const metricUploadLocation = request.yar.get(constants.redisKeys.METRIC_LOCATION)
     const isCombinedCase = (request?._route?.path || '').startsWith('/combined-case')
+
+    const registrationMetricUploaded = request.yar.get(constants.redisKeys.METRIC_LOCATION)
+    const developerMetricUploaded = request.yar.get(constants.redisKeys.DEVELOPER_METRIC_LOCATION)
+
+    // Determine if both metrics have been uploaded
+    const bothMetricsUploaded = Boolean(registrationMetricUploaded) && Boolean(developerMetricUploaded)
+
     return h.view(constants.views.CHECK_METRIC_DETAILS, {
       filename: path.basename(metricUploadLocation),
-      urlPath: isCombinedCase ? '/combined-case' : '/land'
+      urlPath: isCombinedCase ? '/combined-case' : '/land',
+      bothMetricsUploaded
     })
   },
   post: async (request, h) => {
