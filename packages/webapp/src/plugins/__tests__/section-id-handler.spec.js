5b95772a5453f26747e7801457b82a8e66072f28
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-09-10 09:34:18 +0100

    Tweak tests slightly

--- packages/webapp/src/plugins/__tests__/section-id-handler.spec.js
+++ packages/webapp/src/plugins/__tests__/section-id-handler.spec.js
@@ -9,17 +9,18 @@ describe('section-id-handler', () => {
     return { pathname, hash }
   }
 
-  it('Should not redirect if path does not include /public/', async () => {
+  it('Should not redirect if path includes /public/', async () => {
     const application = JSON.parse(testApplication.dataString)
     const response = await submitGetRequest({ url: '/public/robots.txt' }, 200, application)
     expect(forwardedUrl(response).pathname).toBeUndefined()
   })
 
-  it('Should store journey-start-section-id query param and remove it from url', async () => {
+  it('Should store journey-start-section-id query param, remove it from url, and forward to the resulting page with no hash defined', async () => {
     const application = JSON.parse(testApplication.dataString)
     const response = await submitGetRequest({ url: `${constants.reusedRoutes.COMBINED_CASE_CHECK_PLANNING_DECISION_NOTICE_FILE}?journey-start-section-id=test-section-id` }, 302, application)
     expect(application[constants.redisKeys.JOURNEY_START_SECTION_ID]).toEqual('test-section-id')
     expect(forwardedUrl(response).pathname).toEqual(constants.reusedRoutes.COMBINED_CASE_CHECK_PLANNING_DECISION_NOTICE_FILE)
+    expect(forwardedUrl(response).hash).toBeFalsy()
   })
 
   it('Should not redirect if journey-start-section-id is not present', async () => {
@@ -28,7 +29,7 @@ describe('section-id-handler', () => {
     expect(forwardedUrl(response).pathname).toBeUndefined()
   })
 
-  it('Should redirect to url with hash and clear value of JOURNEY_START_SECTION_ID if path is combined case check and submit and journey-start-section-id is present', async () => {
+  it('Should redirect to url with hash and clear the value of JOURNEY_START_SECTION_ID if the path is combined case check and submit, and journey-start-section-id is present', async () => {
     const application = JSON.parse(testApplication.dataString)
     application[constants.redisKeys.JOURNEY_START_SECTION_ID] = 'test-section-id'
     const response = await submitGetRequest({ url: constants.routes.COMBINED_CASE_CHECK_AND_SUBMIT }, 302, application)
@@ -37,7 +38,7 @@ describe('section-id-handler', () => {
     expect(application[constants.redisKeys.JOURNEY_START_SECTION_ID]).toBeUndefined()
   })
 
-  it('Should only redirect if path is combined case check and submit', async () => {
+  it('Should not redirect if path is not combined case check and submit', async () => {
     const application = JSON.parse(testApplication.dataString)
     application[constants.redisKeys.JOURNEY_START_SECTION_ID] = 'test-section-id'
     const response = await submitGetRequest({ url: constants.reusedRoutes.COMBINED_CASE_CHECK_PLANNING_DECISION_NOTICE_FILE }, 200, application)
