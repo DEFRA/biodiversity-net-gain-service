77265511131a1fbc1817d92a192bc7964db238d0
Adam Kay <thousand_leaves@hotmail.com>
2024-09-13 11:17:35 +0100

    Add referer logic to `change-registration-metric.js` to handle back button behavior

--- packages/webapp/src/routes/combined-case/change-registration-metric.js
+++ packages/webapp/src/routes/combined-case/change-registration-metric.js
@@ -1,15 +1,25 @@
 import constants from '../../utils/constants.js'
 
 const handlers = {
-  get: async (_, h) => {
+  get: async (request, h) => {
+    const referer = request.headers.referer
+    if (referer) {
+      request.yar.set(constants.redisKeys.REFERER, referer)
+    }
     return h.view(constants.views.COMBINED_CASE_CHANGE_REGISTRATION_METRIC)
   },
   post: async (request, h) => {
     const changeRegistrationMetric = request.payload.changeRegistrationMetric
+    const referer = request.yar.get(constants.redisKeys.REFERER)
 
     if (changeRegistrationMetric === 'yes') {
       return h.redirect(constants.reusedRoutes.COMBINED_CASE_UPLOAD_METRIC)
     }
+
+    if (referer === constants.routes.COMBINED_CASE_CHECK_AND_SUBMIT) {
+      return h.redirect(constants.routes.COMBINED_CASE_CHECK_AND_SUBMIT)
+    }
+
     return h.redirect(constants.routes.COMBINED_CASE_TASK_LIST)
   }
 }
