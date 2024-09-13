ce7a34b09543fba02fb1d642e08579d2fa41302a
Adam Kay <thousand_leaves@hotmail.com>
2024-09-13 12:20:51 +0100

    fix tests

--- packages/webapp/src/routes/__tests__/combined-case/change-registration-metric.spec.js
+++ packages/webapp/src/routes/__tests__/combined-case/change-registration-metric.spec.js
@@ -1,28 +1,10 @@
-import { submitGetRequest } from '../helpers/server.js'
 import constants from '../../../utils/constants.js'
-
+import changeRegistrationMetric from '../../combined-case/change-registration-metric.js'
+import applicationSession from '../../../__mocks__/application-session.js'
+import { submitGetRequest } from '../helpers/server.js'
 const url = constants.routes.COMBINED_CASE_CHANGE_REGISTRATION_METRIC
 
 describe(url, () => {
-  let viewResult
-  let h
-  let redisMap
-  let changeRegistrationMetric
-
-  beforeEach(() => {
-    h = {
-      view: (view) => {
-        viewResult = view
-      },
-      redirect: (view) => {
-        viewResult = view
-      }
-    }
-
-    redisMap = new Map()
-    changeRegistrationMetric = require('../../combined-case/change-registration-metric.js')
-  })
-
   describe('GET', () => {
     it(`should render the ${url.substring(1)} view`, async () => {
       await submitGetRequest({ url })
@@ -30,28 +12,53 @@ describe(url, () => {
   })
 
   describe('POST', () => {
-    it('should continue journey to upload registration metric page is user confirms to changing the metric', async () => {
-      const request = {
-        yar: redisMap,
-        payload: { changeRegistrationMetric: 'yes' },
-        path: changeRegistrationMetric.default[1].path
-      }
-
-      await changeRegistrationMetric.default[1].handler(request, h)
-
-      expect(viewResult).toEqual(constants.reusedRoutes.COMBINED_CASE_UPLOAD_METRIC)
-    })
-  })
-  describe('POST', () => {
     it('should continue journey to tasklist if user does not want to change the registration metric', async () => {
-      const request = {
-        payload: { changeRegistrationMetric: 'no' },
-        path: changeRegistrationMetric.default[1].path
+      const session = applicationSession()
+      const postHandler = changeRegistrationMetric[1].handler
+      let redirectArgs = ''
+      const h = {
+        redirect: (...args) => {
+          redirectArgs = args
+        }
       }
 
-      await changeRegistrationMetric.default[1].handler(request, h)
+      await postHandler({ yar: session, payload: { changeRegistrationMetric: 'no' } }, h)
+      expect(redirectArgs[0]).toEqual(constants.routes.COMBINED_CASE_TASK_LIST)
+    })
 
-      expect(viewResult).toEqual(constants.routes.COMBINED_CASE_TASK_LIST)
+    it('should redirect to upload metric if user wants to change the registration metric', async () => {
+      const session = applicationSession()
+      const postHandler = changeRegistrationMetric[1].handler
+      let redirectArgs = ''
+      const h = {
+        redirect: (...args) => {
+          redirectArgs = args
+        }
+      }
+
+      await postHandler({ yar: session, payload: { changeRegistrationMetric: 'yes' } }, h)
+      expect(redirectArgs[0]).toEqual(constants.reusedRoutes.COMBINED_CASE_UPLOAD_METRIC)
+    })
+
+    it('should return to check-and-submit if user does not want to change the registration metric', async () => {
+      const session = applicationSession()
+      session.set(constants.redisKeys.REFERER, constants.routes.COMBINED_CASE_CHECK_AND_SUBMIT)
+      const postHandler = changeRegistrationMetric[1].handler
+      let redirectArgs = ''
+      const h = {
+        redirect: (...args) => {
+          redirectArgs = args
+        }
+      }
+
+      const request = {
+        yar: session,
+        payload: { changeRegistrationMetric: 'no' },
+        path: changeRegistrationMetric[1].path
+      }
+
+      await postHandler(request, h)
+      expect(redirectArgs[0]).toEqual(constants.routes.COMBINED_CASE_CHECK_AND_SUBMIT)
     })
   })
 })
