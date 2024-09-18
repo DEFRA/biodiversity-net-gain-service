99f5c7aeb69140489b987b77d74f2d37db2afb88
Adam Kay <thousand_leaves@hotmail.com>
2024-09-18 15:52:54 +0100

    Handle unauthorised 500 errors

--- packages/webapp/src/plugins/error-pages.js
+++ packages/webapp/src/plugins/error-pages.js
@@ -3,17 +3,11 @@ import getApplicantContext from '../utils/get-applicant-context.js'
 const errorPages = {
   name: 'error-pages',
   register: server => {
-    server.ext('onPreResponse', (request, h) => {
+    server.ext('onPreResponse', async (request, h) => {
       const response = request.response
 
       if (response.isBoom) {
-        // An error was raised during
-        // processing the request
         const statusCode = response.output.statusCode
-
-        if (statusCode === 404) {
-          return h.view('404').code(statusCode)
-        }
 
         // Log the error
         request.log('error', {
@@ -22,23 +16,37 @@ const errorPages = {
           stack: response.data ? response.data.stack : response.stack
         })
 
-        // The return the `500` view
-        const { representing, organisation } = getApplicantContext(request.auth.credentials.account, request.yar)
-        const accountInfo = request.auth.credentials.account.idTokenClaims
-
         const context = {
           auth: {
-            firstName: accountInfo.firstName,
-            lastName: accountInfo.lastName
+            firstName: '',
+            lastName: ''
           }
         }
 
-        if (organisation) {
-          context.auth.representing = representing
+        // Handle 404 errors
+        if (statusCode === 404) {
+          return h.view('404', context).code(statusCode)
         }
 
+        // Handle other errors and add additional context if authenticated
+        if (request.auth?.credentials) {
+          const { representing, organisation } = getApplicantContext(request.auth.credentials.account, request.yar)
+          const accountInfo = request.auth.credentials.account.idTokenClaims
+
+          context.auth = {
+            firstName: accountInfo.firstName,
+            lastName: accountInfo.lastName
+          }
+
+          if (organisation) {
+            context.auth.representing = representing
+          }
+        }
+
+        // Return the 500 view with context
         return h.view('500', context).code(statusCode)
       }
+
       return h.continue
     })
   }
