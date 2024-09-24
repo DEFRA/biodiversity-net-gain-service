6b2ff7b7b3d9a3cc57555438724000810a1539be
Adam Kay <90856315+thousand-leaves@users.noreply.github.com>
2024-09-24 14:27:32 +0100

    BNGP-5395: Add resubmission alert for registration metric change (#853)
    * BNGP-5395: Add resubmission alert for registration metric change
    
    https://eaflood.atlassian.net/browse/BNGP-5395
    
    This update adds a confirmation prompt when users change the registration metric after uploading the development metric. A warning alerts the user that they must re-upload the allocation metric and re-enter habitat matching details.
    
    * Add route, view and constant for change registration metric
    
    * Add logic to `check-metric-file.js` to check if both metric files have been uploaded
    
    * Add navigation logic to the `check-metric-details.js` and `check-metric-details.html` file
    
    * Hook up logic to payload in `change-registration-metric.js`
    
    * Clear developer metric and matching data
    
    * Resolve merge conflict
    
    * Fix test
    
    * Update hardcoded redirect values to constants
    
    * add `change-registration-metric.spec.js` test file
    
    * Add solution if coming from check-and-submit page and refactor
    
    * fix test
    
    * Add more tests for coverage
    
    * Add referer logic to `change-registration-metric.js` to handle back button behavior
    
    * fix tests
    
    ---------
    
    Co-authored-by: Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
    Co-authored-by: Stuart Adair <43574728+StuAA78@users.noreply.github.com>

--- packages/webapp/src/routes/__tests__/land/check-metric-details.spec.js
+++ packages/webapp/src/routes/__tests__/land/check-metric-details.spec.js
@@ -15,7 +15,7 @@ describe(url, () => {
       await submitGetRequest({ url: combinedCaseUrl })
     })
 
-    it(`should render the ${url.substring(1)} view`, async () => {
+    it(`should render the ${url.substring(1)} view with correct context`, async () => {
       const session = applicationSession()
       const getHandler = checkMetricDetails[0].handler
       let viewArgs = ''
@@ -27,8 +27,11 @@ describe(url, () => {
 
       await getHandler({ yar: session }, h)
       expect(viewArgs[0]).toEqual(constants.views.CHECK_METRIC_DETAILS)
-      expect(viewArgs[1]).toEqual({ filename: 'new-metric-4.0.xlsm', urlPath: '/land' })
+      expect(viewArgs[1].filename).toEqual('new-metric-4.0.xlsm')
+      expect(viewArgs[1].urlPath).toEqual('/land')
+      expect(viewArgs[1].changeMetricUrl).toEqual('/check-metric-file')
     })
+
     it('should redirect to REGISTER_LAND_TASK_LIST view if mandatory data missing', done => {
       jest.isolateModules(async () => {
         try {
@@ -52,6 +55,7 @@ describe(url, () => {
         }
       })
     })
+
     it('should redirect to COMBINED_CASE_TASK_LIST view if a combined case application and mandatory data is missing', done => {
       jest.isolateModules(async () => {
         try {
@@ -78,7 +82,42 @@ describe(url, () => {
         }
       })
     })
+
+    it('should set changeMetricUrl to /change-registration-metric if both metrics are uploaded', async () => {
+      const session = applicationSession()
+      session.set(constants.redisKeys.METRIC_LOCATION, 'metric-location')
+      session.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, 'developer-metric-location')
+      const getHandler = checkMetricDetails[0].handler
+      let viewArgs = ''
+      const h = {
+        view: (...args) => {
+          viewArgs = args
+        }
+      }
+
+      await getHandler({ yar: session }, h)
+      expect(viewArgs[0]).toEqual(constants.views.CHECK_METRIC_DETAILS)
+      expect(viewArgs[1].changeMetricUrl).toEqual('/change-registration-metric')
+    })
+
+    it('should set changeMetricUrl to /check-metric-file if both metrics are not uploaded', async () => {
+      const session = applicationSession()
+      session.set(constants.redisKeys.METRIC_LOCATION, 'metric-location')
+      session.set(constants.redisKeys.DEVELOPER_METRIC_LOCATION, null)
+      const getHandler = checkMetricDetails[0].handler
+      let viewArgs = ''
+      const h = {
+        view: (...args) => {
+          viewArgs = args
+        }
+      }
+
+      await getHandler({ yar: session }, h)
+      expect(viewArgs[0]).toEqual(constants.views.CHECK_METRIC_DETAILS)
+      expect(viewArgs[1].changeMetricUrl).toEqual('/check-metric-file')
+    })
   })
+
   describe('POST', () => {
     it('Should flow to register task list', async () => {
       const session = applicationSession()
