6f67b16135a97c7176eb4a522ac162184b2aabc3
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-10-09 11:28:51 +0100

    BNGP-5461: Fix incorrect signed-out page text when no application started (#872)
    * Refactor signed-out page and update with new content
    
    * Rename unit tests to `signed-out.spec.js`
    
    * Display appropriate sign-out if no application in progress
    
    This covers pages such as `/land/biodiversity-gain-sites` where a user does not have an application in progress but we still want to display the specific sign-out page for the journey
    
    * Amend default text and reformat page
    
    To avoid link text being too close to the panel text when no heading is given, we removing the top padding from the heading and apply a bottom margin to the panel
    
    * Update test coverage
    
    * Further test coverage
    
    * Update combined case text as per BNGP-5285
    
    * Remove `Combined cases` heading
    
    * Fix test coverage
    
    ---------
    
    Co-authored-by: Shaun Ricketts <89984496+shaundefra@users.noreply.github.com>

--- packages/webapp/src/routes/__tests__/signed-out.spec.js
+++ packages/webapp/src/routes/__tests__/signed-out.spec.js
@@ -4,8 +4,37 @@ const url = constants.routes.SIGNED_OUT
 
 describe(url, () => {
   describe('GET', () => {
-    it(`should render the ${url.substring(1)} view`, async () => {
-      await submitGetRequest({ url })
+    it('should render the signed-out view with default content when no app query is provided', async () => {
+      const response = await submitGetRequest({ url })
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain('Sign in again to resume an existing application or start a new one')
+    })
+
+    it('should render the signed-out view with registration content when app query is registration', async () => {
+      const response = await submitGetRequest({ url: `${url}?app=${constants.applicationTypes.REGISTRATION.toLowerCase()}` })
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain('Your registrations')
+      expect(response.payload).toContain('Sign in to continue with a registration')
+    })
+
+    it('should render the signed-out view with allocation content when app query is allocation', async () => {
+      const response = await submitGetRequest({ url: `${url}?app=${constants.applicationTypes.ALLOCATION.toLowerCase()}` })
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain('Development projects')
+      expect(response.payload).toContain('Sign in to continue with a development project')
+    })
+
+    it('should render the signed-out view with credits purchase content when app query is credits purchase', async () => {
+      const response = await submitGetRequest({ url: `${url}?app=${constants.applicationTypes.CREDITS_PURCHASE.toLowerCase()}` })
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain('Your statutory biodiversity credits')
+      expect(response.payload).toContain('Sign in to continue with a statutory biodiversity credits purchase')
+    })
+
+    it('should render the signed-out view with combined case content when app query is combined case', async () => {
+      const response = await submitGetRequest({ url: `${url}?app=${constants.applicationTypes.COMBINED_CASE.toLowerCase()}` })
+      expect(response.statusCode).toBe(200)
+      expect(response.payload).toContain('Sign in to continue with site registration and record off-site gains for a development')
     })
   })
 })
