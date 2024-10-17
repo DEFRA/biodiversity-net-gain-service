e240f2c92cd67d92c7699f2b07f1df545f4548d1
Adam Kay <thousand_leaves@hotmail.com>
2024-10-17 17:02:26 +0100

    Fix more tests

--- packages/webapp/src/routes/__tests__/credits-purchase/add-statutory-biodiversity-credits.spec.js
+++ packages/webapp/src/routes/__tests__/credits-purchase/add-statutory-biodiversity-credits.spec.js
@@ -19,40 +19,64 @@ describe(url, () => {
         errorList: null
       }
       const res = await submitGetRequest({ url }, 200, sessionData)
-      expect(res.payload).toContain('50') // Check for the unitAmount rendered
+      expect(res.payload).toContain('50')
     })
 
-    it('should render the view with the correct error messages when validation fails', async () => {
+    it('should render the view with the correct error message when no input is provided', async () => {
       const sessionData = {
-        errorMessages: [{ text: 'Enter at least one credit from the metric up to 2 decimal places, like 23.75' }],
-        errorList: [{ text: 'Number of credits must be 10 characters or fewer', href: '#inputField' }]
+        errorList: [{ text: 'Enter at least one credit from the metric up to 2 decimal places, like 23.75' }]
       }
       const res = await submitGetRequest({ url }, 200, sessionData)
       expect(res.payload).toContain('There is a problem')
       expect(res.payload).toContain('Enter at least one credit from the metric up to 2 decimal places, like 23.75')
     })
+
+    it('should render the view with the correct error messages when validation fails', async () => {
+      const sessionData = {
+        [creditsPurchaseConstants.redisKeys.CREDITS_PURCHASE_COST_CALCULATION]: {
+          tierCosts: [{ tier: 'exampleTier', unitAmount: 5012345678901 }]
+        },
+        errorList: [{ text: 'Number of credits must be 10 characters or fewer' }]
+      }
+
+      const res = await submitGetRequest({ url }, 200, sessionData)
+      expect(res.payload).toContain('There is a problem')
+      expect(res.payload).toContain('Number of credits must be 10 characters or fewer')
+    })
+
+    it
+
   })
 
   describe('POST', () => {
     let postOptions
-
+  
     beforeEach(() => {
       postOptions = {
         url,
         payload: {}
       }
     })
-
+  
     it('should continue to the cost page when valid input is provided', async () => {
-      postOptions.payload = { inputField: '25.00' }
-      const res = await submitPostRequest(postOptions)
+      postOptions.payload = { a1: '1', a2: '', a3: '', a4: '', a5: '', h: '', w: '' }
+      const res = await submitPostRequest(postOptions, 302)
       expect(res.headers.location).toEqual(creditsPurchaseConstants.routes.CREDITS_PURCHASE_CREDITS_COST)
     })
-
-    it('should fail and redirect back if validation fails', async () => {
-      postOptions.payload = { inputField: 'invalidInput' }
-      const res = await submitPostRequest(postOptions, 302)
+  
+    it('should not continue journey if validation fails due to long input', async () => {
+      postOptions.payload = { a1: '12345678910', a2: '', a3: '', a4: '', a5: '', h: '', w: '' }
+      const res = await submitPostRequest(postOptions, 302, null, { expectedNumberOfPostJsonCalls: 0 })
+      expect(res.headers.location).toEqual(creditsPurchaseConstants.routes.CREDITS_PURCHASE_CREDITS_SELECTION)
+    })
+  
+    it('should not continue journey if no input is provided', async () => {
+      postOptions.payload = { a1: '', a2: '', a3: '', a4: '', a5: '', h: '', w: '' }
+      const res = await submitPostRequest(postOptions, 302, null, { expectedNumberOfPostJsonCalls: 0 })
       expect(res.headers.location).toEqual(creditsPurchaseConstants.routes.CREDITS_PURCHASE_CREDITS_SELECTION)
     })
   })
+  
+  
+
 })
