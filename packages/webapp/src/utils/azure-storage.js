ab4dbb8ff9c544bc259d6844885692c67b2e3bce
Adam Kay <thousand_leaves@hotmail.com>
2024-10-29 14:05:41 +0000

    Add `deleteBlobFromContainersWithCheck` function to `azure-storage.js`

--- packages/webapp/src/utils/azure-storage.js
+++ packages/webapp/src/utils/azure-storage.js
@@ -47,10 +47,20 @@ const uploadStreamAndAwaitScan = async (logger, config, stream, maxAttempts = 20
 }
 
 const deleteBlobFromContainers = async blobName => {
+  console.log(`${new Date().toUTCString()} ${blobName} is about to be deleted`)
   await blobStorageConnector.deleteBlobIfExists({
     containerName: constants.BLOB_STORAGE_CONTAINER,
     blobName
   })
+  console.log(`${new Date().toUTCString()} ${blobName} has been deleted`)
 }
 
-export { uploadStreamAndAwaitScan, deleteBlobFromContainers }
+const deleteBlobFromContainersWithCheck = async (blobPath, previousBlobPath) => {
+  console.log(`${new Date().toUTCString()} ${blobPath} is about to be checked for deletion`)
+  if (blobPath !== previousBlobPath) {
+    console.log(`${new Date().toUTCString()} ${blobPath} has been passed to deleteBlobFromContainers`)
+    await deleteBlobFromContainers(previousBlobPath)
+  }
+}
+
+export { uploadStreamAndAwaitScan, deleteBlobFromContainers, deleteBlobFromContainersWithCheck }
