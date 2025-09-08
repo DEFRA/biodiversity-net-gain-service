a83d170b77375cf71b92a70aa0f25ca86716a612
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2025-09-08 22:06:45 +0100

    Test sql command

--- packages/database-version-control/src/migrations/2025.09.08T14.14.02.create-principal.js
+++ packages/database-version-control/src/migrations/2025.09.08T14.14.02.create-principal.js
@@ -1,6 +1,7 @@
 /** @type {import('@slonik/migrator').Migration} */
 exports.up = async ({ context: { connection, sql } }) => {
-  const result = await connection.query(sql`select * from pgaadauth_create_principal('TSTBNGWEBFA4401', false, false);`)
+  // const result = await connection.query(sql`select * from pgaadauth_create_principal('TSTBNGWEBFA4401', false, false);`)
+  const result = await connection.query(sql`SELECT current_user;`)
   console.log(result)
 }
 
